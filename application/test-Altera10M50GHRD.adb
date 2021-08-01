76ac8aa3636a6fc4586c9f0189efd397615f3adf
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2021-08-01 23:59:22 +0200

    added a volatile dummy to make the loop in the QEMU machine rendered at a human rate

--- application/test-Altera10M50GHRD.adb
+++ application/test-Altera10M50GHRD.adb
@@ -29,12 +29,13 @@ package body Application is
       if True then
          declare
             Delay_Count : constant := 100_000_000;
+            Dummy       : Unsigned_32 with Volatile => True;
          begin
             IOEMU_IO1 := 0;
             loop
                -- IOEMU GPIO test
                IOEMU_IO1 := IOEMU_IO1 + 1;
-               for Delay_Loop_Count in 1 .. Delay_Count loop null; end loop;
+               for Delay_Loop_Count in 1 .. Delay_Count loop Dummy := Dummy + 1; end loop;
             end loop;
          end;
       end if;
