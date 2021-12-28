fd2d99a7536d2ea1111ec3a7879dfcd96885cbb8
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2021-12-29 00:17:05 +0100

    implemented MIPS.NOP

--- cpus/MIPS/mips.adb
+++ cpus/MIPS/mips.adb
@@ -2,7 +2,7 @@
 --                                                     SweetAda                                                      --
 -----------------------------------------------------------------------------------------------------------------------
 -- __HDS__                                                                                                           --
--- __FLN__ cpu.ads                                                                                                   --
+-- __FLN__ mips.adb                                                                                                  --
 -- __DSC__                                                                                                           --
 -- __HSH__ e69de29bb2d1d6434b8b29ae775ad8c2e48c5391                                                                  --
 -- __HDE__                                                                                                           --
@@ -15,45 +15,43 @@
 -- Please consult the LICENSE.txt file located in the top-level directory.                                           --
 -----------------------------------------------------------------------------------------------------------------------
 
-with MIPS;
-with MIPS32;
+with System.Machine_Code;
+with Definitions;
 
-package CPU is
+package body MIPS is
 
    --========================================================================--
    --                                                                        --
    --                                                                        --
-   --                               Public part                              --
+   --                           Local declarations                           --
+   --                                                                        --
+   --                                                                        --
+   --========================================================================--
+
+   use System.Machine_Code;
+
+   CRLF : String renames Definitions.CRLF;
+
+   --========================================================================--
+   --                                                                        --
+   --                                                                        --
+   --                           Package subprograms                          --
    --                                                                        --
    --                                                                        --
    --========================================================================--
 
    ----------------------------------------------------------------------------
-   -- CPU helper subprograms
+   -- NOP
    ----------------------------------------------------------------------------
+   procedure NOP is
+   begin
+      Asm (
+           Template => "        nop",
+           Outputs  => No_Output_Operands,
+           Inputs   => No_Input_Operands,
+           Clobber  => "",
+           Volatile => True
+          );
+   end NOP;
 
-   procedure NOP renames MIPS.NOP;
-
-   ----------------------------------------------------------------------------
-   -- Exceptions and interrupts
-   ----------------------------------------------------------------------------
-
-   subtype Irq_State_Type is MIPS32.Irq_State_Type;
-   subtype Irq_Id_Type    is MIPS32.Irq_Id_Type;
-
-   procedure Irq_Enable                                    renames MIPS32.Irq_Enable;
-   procedure Irq_Disable                                   renames MIPS32.Irq_Disable;
-   function Irq_State_Get return Irq_State_Type            renames MIPS32.Irq_State_Get;
-   procedure Irq_State_Set (Irq_State : in Irq_State_Type) renames MIPS32.Irq_State_Set;
-
-   ----------------------------------------------------------------------------
-   -- Locking
-   ----------------------------------------------------------------------------
-
-   subtype Lock_Type is MIPS.Lock_Type;
-
-   procedure Lock_Try (Lock_Object : in out MIPS.Lock_Type; Success : out Boolean) renames MIPS32.Lock_Try;
-   procedure Lock (Lock_Object : in out MIPS.Lock_Type)                            renames MIPS32.Lock;
-   procedure Unlock (Lock_Object : out MIPS.Lock_Type)                             renames MIPS32.Unlock;
-
-end CPU;
+end MIPS;
