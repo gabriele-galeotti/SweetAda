f2778ba7759f3ca08e2650a6e5bf2e7be3f2d930
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2021-12-30 17:18:43 +0100

    use the CPU unit rather than the explicit one

--- application/test-QEMU-RISC-V-32.adb
+++ application/test-QEMU-RISC-V-32.adb
@@ -1,8 +1,8 @@
 
 with System.Storage_Elements;
 with Interfaces;
-
-with RISCV;
+with CPU;
+with IOEMU;
 
 package body Application is
 
@@ -17,28 +17,6 @@ package body Application is
    use System.Storage_Elements;
    use Interfaces;
 
-   ----------------------------------------------------------------------------
-   -- IOEMU
-   ----------------------------------------------------------------------------
-
-   IOEMU_BASEADDRESS : constant := 16#1000_0100#;
-
-   IOEMU_IO0 : aliased Unsigned_8 with
-      Address    => To_Address (IOEMU_BASEADDRESS + 0),
-      Volatile   => True,
-      Import     => True,
-      Convention => Ada;
-   IOEMU_IO1 : aliased Unsigned_8 with
-      Address    => To_Address (IOEMU_BASEADDRESS + 1),
-      Volatile   => True,
-      Import     => True,
-      Convention => Ada;
-   IOEMU_IO2 : aliased Unsigned_8 with
-      Address    => To_Address (IOEMU_BASEADDRESS + 2),
-      Volatile   => True,
-      Import     => True,
-      Convention => Ada;
-
    --========================================================================--
    --                                                                        --
    --                                                                        --
@@ -51,22 +29,22 @@ package body Application is
    begin
       -------------------------------------------------------------------------
       if False then
-         RISCV.Asm_Call (System.Storage_Elements.To_Address (16#2000#));
+         CPU.Asm_Call (System.Storage_Elements.To_Address (16#2000#));
       end if;
       -------------------------------------------------------------------------
       if True then
          declare
             Delay_Count : constant := 300_000_000;
          begin
-            IOEMU_IO1 := 0;
-            IOEMU_IO2 := 0;
+            IOEMU.IOEMU_IO1 := 0;
+            IOEMU.IOEMU_IO2 := 0;
             loop
                -- IOEMU GPIO test
-               IOEMU_IO0 := 16#FF#;
-               IOEMU_IO0 := 16#00#;
-               IOEMU_IO1 := IOEMU_IO1 + 1;
-               IOEMU_IO2 := IOEMU_IO2 + 1;
-               for Delay_Loop_Count in 1 .. Delay_Count loop RISCV.NOP; end loop;
+               IOEMU.IOEMU_IO0 := 16#FF#;
+               IOEMU.IOEMU_IO0 := 16#00#;
+               IOEMU.IOEMU_IO1 := @ + 1;
+               IOEMU.IOEMU_IO2 := @ + 1;
+               for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
             end loop;
          end;
       end if;
