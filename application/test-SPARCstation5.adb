73c90d567dac24159ccab4845e6958c3b0bd8661
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2021-12-30 16:31:30 +0100

    segregate IOEMU module

--- application/test-SPARCstation5.adb
+++ application/test-SPARCstation5.adb
@@ -5,8 +5,10 @@ with Interfaces;
 with Configure;
 with Bits;
 with BSP;
+with CPU;
 with SCC;
 with Sun4m;
+with IOEMU;
 with Console;
 
 package body Application is
@@ -23,28 +25,6 @@ package body Application is
    use Interfaces;
    use Bits;
    use Sun4m;
-
-   -- IOEMU GPIO 0x72000000..0x72000003
-   IOEMU_IO0 : Unsigned_8 with
-      Address    => To_Address (16#7200_0000#),
-      Volatile   => True,
-      Import     => True,
-      Convention => Ada;
-   IOEMU_IO1 : Unsigned_8 with
-      Address    => To_Address (16#7200_0001#),
-      Volatile   => True,
-      Import     => True,
-      Convention => Ada;
-   IOEMU_IO2 : Unsigned_8 with
-      Address    => To_Address (16#7200_0002#),
-      Volatile   => True,
-      Import     => True,
-      Convention => Ada;
-   IOEMU_IO3 : Unsigned_8 with
-      Address    => To_Address (16#7200_0003#),
-      Volatile   => True,
-      Import     => True,
-      Convention => Ada;
 
    --========================================================================--
    --                                                                        --
@@ -75,24 +55,24 @@ package body Application is
                   Delay_Count := 50_000;
                end if;
             end if;
-            IOEMU_IO1 := 0;
+            IOEMU.IOEMU_IO1 := 0;
             loop
                Console.Print ("hello, SweetAda", NL => True);
                CHANNELB_Putchar ('O');
                CHANNELB_Putchar ('K');
                CHANNELB_Putchar (Ada.Characters.Latin_1.CR);
                CHANNELB_Putchar (Ada.Characters.Latin_1.LF);
-               for Delay_Loop_Count in 1 .. Delay_Count loop null; end loop;
+               for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
                -- IOEMU GPIO test
-               IOEMU_IO1 := IOEMU_IO1 + 1;
+               IOEMU.IOEMU_IO1 := @ + 1;
                if System_Timer.Counter.LR = 1 then
                   declare
                      -- Unused : Slavio_Timer_Limit_Type with Unreferenced => True;
                   begin
                      -- Unused := System_Timer.Limit;
                      System_Timer_ClearLR;
-                     IOEMU_IO0 := 1;
-                     IOEMU_IO0 := 0;
+                     IOEMU.IOEMU_IO0 := 1;
+                     IOEMU.IOEMU_IO0 := 0;
                   end;
                end if;
             end loop;
