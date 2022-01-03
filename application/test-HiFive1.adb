5b7fceb120c304a13e026e59d4d691e4fdce3b88
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2022-01-03 15:32:25 +0100

    HiFive1: use CPU.NOP rather than a null

--- application/test-HiFive1.adb
+++ application/test-HiFive1.adb
@@ -1,5 +1,6 @@
 
 with Interfaces;
+with CPU;
 with HiFive1;
 
 package body Application is
@@ -33,20 +34,20 @@ package body Application is
       begin
          while True loop
             -- turn on RED
-            HiFive1.LEDs_PORT := HiFive1.LEDs_PORT and 16#FFBF_FFFF#;
-            for Delay_Loop_Count in 1 .. Delay_Count loop null; end loop;
-            HiFive1.LEDs_PORT := HiFive1.LEDs_PORT or 16#0068_0000#;
-            for Delay_Loop_Count in 1 .. Delay_Count loop null; end loop;
+            HiFive1.LEDs_PORT := @ and 16#FFBF_FFFF#;
+            for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
+            HiFive1.LEDs_PORT := @ or 16#0068_0000#;
+            for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
             -- turn on GREEN
-            HiFive1.LEDs_PORT := HiFive1.LEDs_PORT and 16#FFF7_FFFF#;
-            for Delay_Loop_Count in 1 .. Delay_Count loop null; end loop;
-            HiFive1.LEDs_PORT := HiFive1.LEDs_PORT or 16#0068_0000#;
-            for Delay_Loop_Count in 1 .. Delay_Count loop null; end loop;
+            HiFive1.LEDs_PORT := @ and 16#FFF7_FFFF#;
+            for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
+            HiFive1.LEDs_PORT := @ or 16#0068_0000#;
+            for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
             -- turn on BLUE
-            HiFive1.LEDs_PORT := HiFive1.LEDs_PORT and 16#FFDF_FFFF#;
-            for Delay_Loop_Count in 1 .. Delay_Count loop null; end loop;
-            HiFive1.LEDs_PORT := HiFive1.LEDs_PORT or 16#0068_0000#;
-            for Delay_Loop_Count in 1 .. Delay_Count loop null; end loop;
+            HiFive1.LEDs_PORT := @ and 16#FFDF_FFFF#;
+            for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
+            HiFive1.LEDs_PORT := @ or 16#0068_0000#;
+            for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
          end loop;
       end;
       -------------------------------------------------------------------------
