6e9fe43744ece5386ab75b7b55331d4a30bcd923
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2022-02-16 17:01:35 +0100

    remove generic ugliness

--- drivers/uartioemu.adb
+++ drivers/uartioemu.adb
@@ -36,19 +36,19 @@ package body UARTIOEMU is
    -- Register types
    ----------------------------------------------------------------------------
 
-   type UartIOEMU_Register_Type is (RHR, THR, CONTROL, STATUS);
+   type UartIOEMU_Register_Type is (RHR, THR, CR, SR);
 
    UartIOEMU_Register_Offset : constant array (UartIOEMU_Register_Type) of Storage_Offset :=
       (
-       RHR     => 0,
-       THR     => 0,
-       CONTROL => 1,
-       STATUS  => 2
+       RHR => 0,
+       THR => 0,
+       CR  => 1,
+       SR  => 2
       );
 
    -- Control Register
 
-   type UartIOEMU_Control_Register_Type is
+   type CR_Type is
    record
       RXIRQENABLE : Boolean;
       TXIRQENABLE : Boolean;
@@ -56,16 +56,19 @@ package body UARTIOEMU is
    end record with
       Bit_Order => Low_Order_First,
       Size      => 8;
-   for UartIOEMU_Control_Register_Type use
+   for CR_Type use
    record
       RXIRQENABLE at 0 range 0 .. 0;
       TXIRQENABLE at 0 range 1 .. 1;
       Unused      at 0 range 2 .. 7;
    end record;
 
+   function To_U8 is new Ada.Unchecked_Conversion (CR_Type, Unsigned_8);
+   function To_CR is new Ada.Unchecked_Conversion (Unsigned_8, CR_Type);
+
    -- Status Register
 
-   type UartIOEMU_Status_Register_Type is
+   type SR_Type is
    record
       RXREADY : Boolean;
       TXEMPTY : Boolean;
@@ -73,25 +76,30 @@ package body UARTIOEMU is
    end record with
       Bit_Order => Low_Order_First,
       Size      => 8;
-   for UartIOEMU_Status_Register_Type use
+   for SR_Type use
    record
       RXREADY at 0 range 0 .. 0;
       TXEMPTY at 0 range 1 .. 1;
       Unused  at 0 range 2 .. 7;
    end record;
 
+   function To_U8 is new Ada.Unchecked_Conversion (SR_Type, Unsigned_8);
+   function To_SR is new Ada.Unchecked_Conversion (Unsigned_8, SR_Type);
+
+   -- Local subprograms
+
    function Register_Read (
                            Descriptor : UartIOEMU_Descriptor_Type;
                            Register   : UartIOEMU_Register_Type
-                          ) return Unsigned_8;
-   -- not inlined to avoid code bloating
+                          ) return Unsigned_8 with
+      Inline => True;
 
    procedure Register_Write (
                              Descriptor : in UartIOEMU_Descriptor_Type;
                              Register   : in UartIOEMU_Register_Type;
                              Value      : in Unsigned_8
-                            );
-   -- not inlined to avoid code bloating
+                            ) with
+      Inline => True;
 
    --========================================================================--
    --                                                                        --
@@ -101,101 +109,36 @@ package body UARTIOEMU is
    --                                                                        --
    --========================================================================--
 
+   ----------------------------------------------------------------------------
+   -- Register_Read
+   ----------------------------------------------------------------------------
    function Register_Read (
                            Descriptor : UartIOEMU_Descriptor_Type;
                            Register   : UartIOEMU_Register_Type
                           ) return Unsigned_8 is
    begin
-      return Descriptor.Read (
-                              Build_Address (
+      return Descriptor.Read (Build_Address (
                                              Descriptor.Base_Address,
                                              UartIOEMU_Register_Offset (Register),
                                              Descriptor.Scale_Address
-                                            )
-                             );
+                                            ));
    end Register_Read;
 
+   ----------------------------------------------------------------------------
+   -- Register_Write
+   ----------------------------------------------------------------------------
    procedure Register_Write (
                              Descriptor : in UartIOEMU_Descriptor_Type;
                              Register   : in UartIOEMU_Register_Type;
                              Value      : in Unsigned_8
                             ) is
    begin
-      Descriptor.Write (
-                        Build_Address (
+      Descriptor.Write (Build_Address (
                                        Descriptor.Base_Address,
                                        UartIOEMU_Register_Offset (Register),
                                        Descriptor.Scale_Address
-                                      ),
-                        Value
-                       );
+                                      ), Value);
    end Register_Write;
-
-   ----------------------------------------------------------------------------
-   -- Local Subprograms (generic)
-   ----------------------------------------------------------------------------
-
-   generic
-      Register_Type : in UartIOEMU_Register_Type;
-      type Output_Register_Type is private;
-   function Typed_Register_Read (
-                                 Descriptor : UartIOEMU_Descriptor_Type
-                                ) return Output_Register_Type;
-   pragma Inline (Typed_Register_Read);
-   function Typed_Register_Read (
-                                 Descriptor : UartIOEMU_Descriptor_Type
-                                ) return Output_Register_Type is
-      function Convert is new Ada.Unchecked_Conversion (Unsigned_8, Output_Register_Type);
-   begin
-      return Convert (Register_Read (Descriptor, Register_Type));
-   end Typed_Register_Read;
-
-   generic
-      Register_Type : in UartIOEMU_Register_Type;
-      type Input_Register_Type is private;
-   procedure Typed_Register_Write (
-                                   Descriptor : in UartIOEMU_Descriptor_Type;
-                                   Value      : in Input_Register_Type
-                                  );
-   pragma Inline (Typed_Register_Write);
-   procedure Typed_Register_Write (
-                                   Descriptor : in UartIOEMU_Descriptor_Type;
-                                   Value      : in Input_Register_Type
-                                  ) is
-      function Convert is new Ada.Unchecked_Conversion (Input_Register_Type, Unsigned_8);
-   begin
-      Register_Write (Descriptor, Register_Type, Convert (Value));
-   end Typed_Register_Write;
-
-   ----------------------------------------------------------------------------
-   -- Registers Read/Write subprograms
-   ----------------------------------------------------------------------------
-
-   function RHR_Read (Descriptor : UartIOEMU_Descriptor_Type) return Unsigned_8;
-   pragma Inline (RHR_Read);
-   function RHR_Read (Descriptor : UartIOEMU_Descriptor_Type) return Unsigned_8 is
-   begin
-      return Register_Read (Descriptor, RHR);
-   end RHR_Read;
-
-   procedure THR_Write (Descriptor : in UartIOEMU_Descriptor_Type; Value : in Unsigned_8);
-   pragma Inline (THR_Write);
-   procedure THR_Write (Descriptor : in UartIOEMU_Descriptor_Type; Value : in Unsigned_8) is
-   begin
-      Register_Write (Descriptor, THR, Value);
-   end THR_Write;
-
-   function CONTROL_Read is new Typed_Register_Read (CONTROL, UartIOEMU_Control_Register_Type);
-   pragma Inline (CONTROL_Read);
-
-   procedure CONTROL_Write is new Typed_Register_Write (CONTROL, UartIOEMU_Control_Register_Type);
-   pragma Inline (CONTROL_Write);
-
-   function STATUS_Read is new Typed_Register_Read (STATUS, UartIOEMU_Status_Register_Type);
-   pragma Inline (STATUS_Read);
-
-   procedure STATUS_Write is new Typed_Register_Write (STATUS, UartIOEMU_Status_Register_Type);
-   pragma Inline (STATUS_Write);
 
    ----------------------------------------------------------------------------
    -- Init
@@ -208,8 +151,8 @@ package body UARTIOEMU is
          RX_Irqenable := True;
          -- TX_Irqenable := True;  __FIX__
       end if;
-      CONTROL_Write (Descriptor, (RXIRQENABLE => RX_Irqenable, TXIRQENABLE => TX_Irqenable, others => 0));
-      STATUS_Write (Descriptor, (RXREADY => True, TXEMPTY => True, others => 0));
+      Register_Write (Descriptor, CR, To_U8 (CR_Type'((RXIRQENABLE => RX_Irqenable, TXIRQENABLE => TX_Irqenable, others => 0))));
+      Register_Write (Descriptor, SR, To_U8 (SR_Type'((RXREADY => True, TXEMPTY => True, others => 0))));
    end Init;
 
    ----------------------------------------------------------------------------
@@ -217,7 +160,7 @@ package body UARTIOEMU is
    ----------------------------------------------------------------------------
    procedure TX (Descriptor : in UartIOEMU_Descriptor_Type; Data : in Unsigned_8) is
    begin
-      THR_Write (Descriptor, Data);
+      Register_Write (Descriptor, THR, Data);
    end TX;
 
    ----------------------------------------------------------------------------
@@ -225,7 +168,7 @@ package body UARTIOEMU is
    ----------------------------------------------------------------------------
    procedure RX (Descriptor : UartIOEMU_Descriptor_Type; Data : out Unsigned_8) is
    begin
-      Data := RHR_Read (Descriptor);
+      Data := Register_Read (Descriptor, RHR);
    end RX;
 
    ----------------------------------------------------------------------------
@@ -234,8 +177,8 @@ package body UARTIOEMU is
    function RXIrqActive (Descriptor : UartIOEMU_Descriptor_Type) return Boolean is
    begin
       if
-         CONTROL_Read (Descriptor).RXIRQENABLE and then
-         STATUS_Read (Descriptor).RXREADY
+         To_CR (Register_Read (Descriptor, CR)).RXIRQENABLE and then
+         To_SR (Register_Read (Descriptor, SR)).RXREADY
       then
          return True;
       else
@@ -247,10 +190,10 @@ package body UARTIOEMU is
    -- RXClearIrq
    ----------------------------------------------------------------------------
    procedure RXClearIrq (Descriptor : in UartIOEMU_Descriptor_Type) is
-      Status_Value : UartIOEMU_Status_Register_Type;
+      Status_Value : SR_Type;
    begin
       Status_Value.RXREADY := True;
-      STATUS_Write (Descriptor, Status_Value);
+      Register_Write (Descriptor, SR, To_U8 (Status_Value));
    end RXClearIrq;
 
 end UARTIOEMU;
