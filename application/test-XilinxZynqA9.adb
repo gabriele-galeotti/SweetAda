950903641f623e60cb00fc307151ac3f9c1671dd
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2022-02-22 11:22:12 +0100

    added IOEMU unit

--- application/test-XilinxZynqA9.adb
+++ application/test-XilinxZynqA9.adb
@@ -1,8 +1,20 @@
 
+with Interfaces;
 with CPU;
+with IOEMU;
 with Console;
 
 package body Application is
+
+   --========================================================================--
+   --                                                                        --
+   --                                                                        --
+   --                           Local declarations                           --
+   --                                                                        --
+   --                                                                        --
+   --========================================================================--
+
+   use Interfaces;
 
    --========================================================================--
    --                                                                        --
@@ -20,10 +32,17 @@ package body Application is
       -------------------------------------------------------------------------
       if True then
          declare
-            Delay_Count : constant := 50_000_000;
+            Delay_Count : constant := 100_000_000;
+            Value       : Unsigned_8;
          begin
+            Value := 0;
+            IOEMU.IOEMU_IO1 := 0;
             loop
-               Console.Print ("hello, SweetAda", NL => True);
+               if (Value mod 10) = 0 then
+                  Console.Print ("hello, SweetAda", NL => True);
+               end if;
+               Value := @ + 1;
+               IOEMU.IOEMU_IO1 := @ + 1;
                for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
             end loop;
          end;
