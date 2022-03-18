2740ce55c289f6c69f88d2a6e68e3bed0b3361cc
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2022-03-18 21:22:17 +0100

    Amiga, PC-x86: use aspects rather than pragmas

--- platforms/Amiga-FS-UAE/exceptions.ads
+++ platforms/Amiga-FS-UAE/exceptions.ads
@@ -48,45 +48,71 @@ package Exceptions is
    -- 000 stack pointer
    -- 001 reset vector
    -- 002
-   Buserr_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Buserr_Handler, "buserr_handler");
+   Buserr_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "buserr_handler";
    -- 003
-   Addrerr_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Addrerr_Handler, "addrerr_handler");
+   Addrerr_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "addrerr_handler";
    -- 004
-   Illinstr_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Illinstr_Handler, "illinstr_handler");
+   Illinstr_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "illinstr_handler";
    -- 005
-   Div0_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Div0_Handler, "div0_handler");
+   Div0_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "div0_handler";
    -- 006
-   Chkinstr_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Chkinstr_Handler, "chkinstr_handler");
+   Chkinstr_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "chkinstr_handler";
    -- 007
-   Trapc_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Trapc_Handler, "trapc_handler");
+   Trapc_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "trapc_handler";
    -- 008
-   PrivilegeV_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, PrivilegeV_Handler, "privilegev_handler");
+   PrivilegeV_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "privilegev_handler";
    -- 009
-   Trace_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Trace_Handler, "trace_handler");
+   Trace_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "trace_handler";
    -- 010
-   Line1010_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Line1010_Handler, "line1010_handler");
+   Line1010_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "line1010_handler";
    -- 011
-   Line1111_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Line1111_Handler, "line1111_handler");
+   Line1111_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "line1111_handler";
    -- 012 is reserved
    -- 013
-   CProtocolV_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, CProtocolV_Handler, "cprotocolv_handler");
+   CProtocolV_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "cprotocolv_handler";
    -- 014
-   Formaterr_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Formaterr_Handler, "formaterr_handler");
+   Formaterr_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "formaterr_handler";
    -- 015
-   Uninitint_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Uninitint_Handler, "uninitint_handler");
+   Uninitint_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "uninitint_handler";
    -- 016 is reserved
    -- 017 is reserved
    -- 018 is reserved
@@ -96,48 +122,59 @@ package Exceptions is
    -- 022 is reserved
    -- 023 is reserved
    -- 024
-   Spurious_Interrupt_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Spurious_Interrupt_Handler, "spurious_handler");
+   Spurious_Interrupt_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "spurious_handler";
    -- 025
-   Level_1_Interrupt_Autovector_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Level_1_Interrupt_Autovector_Handler, "l1autovector_handler");
+   Level_1_Interrupt_Autovector_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "l1autovector_handler";
    -- 026
-   Level_2_Interrupt_Autovector_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Level_2_Interrupt_Autovector_Handler, "l2autovector_handler");
+   Level_2_Interrupt_Autovector_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "l2autovector_handler";
    -- 027
-   Level_3_Interrupt_Autovector_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Level_3_Interrupt_Autovector_Handler, "l3autovector_handler");
+   Level_3_Interrupt_Autovector_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "l3autovector_handler";
    -- 028
-   Level_4_Interrupt_Autovector_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Level_4_Interrupt_Autovector_Handler, "l4autovector_handler");
+   Level_4_Interrupt_Autovector_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "l4autovector_handler";
    -- 029
-   Level_5_Interrupt_Autovector_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Level_5_Interrupt_Autovector_Handler, "l5autovector_handler");
+   Level_5_Interrupt_Autovector_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "l5autovector_handler";
    -- 030
-   Level_6_Interrupt_Autovector_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Level_6_Interrupt_Autovector_Handler, "l6autovector_handler");
+   Level_6_Interrupt_Autovector_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "l6autovector_handler";
    -- 031
-   Level_7_Interrupt_Autovector_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Level_7_Interrupt_Autovector_Handler, "l7autovector_handler");
+   Level_7_Interrupt_Autovector_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "l7autovector_handler";
    -- 047
-   Trap_15_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Trap_15_Handler, "trap15_handler");
+   Trap_15_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "trap15_handler";
 
-   procedure Process (Exception_Number : in Unsigned_32; Frame_Address : in Address);
-   procedure Irq_Process (Irq_Identifier : in Exception_Vector_Id_Type);
+   procedure Process (Exception_Number : in Unsigned_32; Frame_Address : in Address) with
+      Export        => True,
+      Convention    => Asm,
+      External_Name => "exception_process";
+   procedure Irq_Process (Irq_Identifier : in Exception_Vector_Id_Type) with
+      Export        => True,
+      Convention    => Asm,
+      External_Name => "irq_process";
    procedure Init;
-
-private
-
-   --========================================================================--
-   --                                                                        --
-   --                                                                        --
-   --                              Private part                              --
-   --                                                                        --
-   --                                                                        --
-   --========================================================================--
-
-   pragma Export (Asm, Process, "exception_process");
-   pragma Export (Asm, Irq_Process, "irq_process");
 
 end Exceptions;
--- platforms/PC-x86/exceptions.ads
+++ platforms/PC-x86/exceptions.ads
@@ -33,70 +33,134 @@ package Exceptions is
    use Bits;
    use CPU.x86;
 
-   Div_By_0_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Div_By_0_Handler, "div_by_0_handler");
-   Debug_Exception_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Debug_Exception_Handler, "debug_exception_handler");
-   Nmi_Interrupt_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Nmi_Interrupt_Handler, "nmi_interrupt_handler");
-   One_Byte_Int_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, One_Byte_Int_Handler, "one_byte_int_handler");
-   Int_On_Overflow_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Int_On_Overflow_Handler, "int_on_overflow_handler");
-   Array_Bounds_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Array_Bounds_Handler, "array_bounds_handler");
-   Invalid_Opcode_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Invalid_Opcode_Handler, "invalid_opcode_handler");
-   Device_Not_Avl_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Device_Not_Avl_Handler, "device_not_avl_handler");
-   Double_Fault_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Double_Fault_Handler, "double_fault_handler");
-   Cp_Seg_Ovr_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Cp_Seg_Ovr_Handler, "cp_seg_ovr_handler");
-   Invalid_Tss_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Invalid_Tss_Handler, "invalid_tss_handler");
-   Seg_Not_Prsnt_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Seg_Not_Prsnt_Handler, "seg_not_prsnt_handler");
-   Stack_Fault_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Stack_Fault_Handler, "stack_fault_handler");
-   Gen_Prot_Fault_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Gen_Prot_Fault_Handler, "gen_prot_fault_handler");
-   Page_Fault_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Page_Fault_Handler, "page_fault_handler");
-   Coproc_Error_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Coproc_Error_Handler, "coproc_error_handler");
-   Irq0_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Irq0_Handler, "irq0_handler");
-   Irq1_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Irq1_Handler, "irq1_handler");
-   Irq2_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Irq2_Handler, "irq2_handler");
-   Irq3_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Irq3_Handler, "irq3_handler");
-   Irq4_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Irq4_Handler, "irq4_handler");
-   Irq5_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Irq5_Handler, "irq5_handler");
-   Irq6_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Irq6_Handler, "irq6_handler");
-   Irq7_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Irq7_Handler, "irq7_handler");
-   Irq8_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Irq8_Handler, "irq8_handler");
-   Irq9_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Irq9_Handler, "irq9_handler");
-   Irq10_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Irq10_Handler, "irq10_handler");
-   Irq11_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Irq11_Handler, "irq11_handler");
-   Irq12_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Irq12_Handler, "irq12_handler");
-   Irq13_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Irq13_Handler, "irq13_handler");
-   Irq14_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Irq14_Handler, "irq14_handler");
-   Irq15_Handler : aliased Asm_Entry_Point;
-   pragma Import (Asm, Irq15_Handler, "irq15_handler");
+   Div_By_0_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "div_by_0_handler";
+   Debug_Exception_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "debug_exception_handler";
+   Nmi_Interrupt_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "nmi_interrupt_handler";
+   One_Byte_Int_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "one_byte_int_handler";
+   Int_On_Overflow_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "int_on_overflow_handler";
+   Array_Bounds_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "array_bounds_handler";
+   Invalid_Opcode_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "invalid_opcode_handler";
+   Device_Not_Avl_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "device_not_avl_handler";
+   Double_Fault_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "double_fault_handler";
+   Cp_Seg_Ovr_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "cp_seg_ovr_handler";
+   Invalid_Tss_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "invalid_tss_handler";
+   Seg_Not_Prsnt_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "seg_not_prsnt_handler";
+   Stack_Fault_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "stack_fault_handler";
+   Gen_Prot_Fault_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "gen_prot_fault_handler";
+   Page_Fault_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "page_fault_handler";
+   Coproc_Error_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "coproc_error_handler";
+   Irq0_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "irq0_handler";
+   Irq1_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "irq1_handler";
+   Irq2_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "irq2_handler";
+   Irq3_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "irq3_handler";
+   Irq4_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "irq4_handler";
+   Irq5_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "irq5_handler";
+   Irq6_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "irq6_handler";
+   Irq7_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "irq7_handler";
+   Irq8_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "irq8_handler";
+   Irq9_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "irq9_handler";
+   Irq10_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "irq10_handler";
+   Irq11_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "irq11_handler";
+   Irq12_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "irq12_handler";
+   Irq13_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "irq13_handler";
+   Irq14_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "irq14_handler";
+   Irq15_Handler : aliased Asm_Entry_Point with
+      Import        => True,
+      Convention    => Asm,
+      External_Name => "irq15_handler";
 
    procedure Process (
                       Exception_Identifier          : in Exception_Id_Type;
