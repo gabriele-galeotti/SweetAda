12d916bf692ec75e1ca7f72ce01532003e0b01bd
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2022-03-21 23:37:57 +0100

    PC-x86: C library test enable/disable

--- platforms/PC-x86/ctest.c
+++ platforms/PC-x86/ctest.c
@@ -1,24 +1,10 @@
 
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 /* imported as procedure "Ctest" */
 void
 ctest(void)
 {
-        const char *number = "5687";
-        long        n;
-
-        n = strtol(number, NULL, 10);
-        if (n == 5687)
-        {
-                printf("strtol() test: OK\n");
-        }
-
-        printf("%s: %ld\n", "ld 80000000", 0x80000000L);
-        printf("%s: %lu\n", "lu 80000000", 0x80000000UL);
-        printf("%s: %hd\n", "hd 8000", (unsigned int)0x8000);
-        printf("%s: %hu\n", "hu 8000", (unsigned int)0x8000);
+        printf("C test OK.\n");
 }
 
