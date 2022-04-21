1cd66a51cd17999a50b8c893663b94a93b181dc3
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2022-04-21 19:46:31 +0200

    ZOOM: use appropriate definitions for U7 latch and test LEDs

--- application/test-ZOOM.adb
+++ application/test-ZOOM.adb
@@ -20,14 +20,43 @@ package body Application is
    begin
       -------------------------------------------------------------------------
       if True then
-         MCF5373.PAR_TIMER  := 16#3F#;
-         MCF5373.PDDR_TIMER := 16#08#;
-         MCF5373.PODR_TIMER := 16#00#;
+         MCF5373.PAR_TIMER  := (
+                                PAR_T0IN => MCF5373.PAR_T0IN_T0IN,
+                                PAR_T1IN => MCF5373.PAR_T1IN_T1IN,
+                                PAR_T2IN => MCF5373.PAR_T2IN_T2IN,
+                                PAR_T3IN => MCF5373.PAR_T3IN_GPIO  -- MFP9 - TIN3/TOUT3 mapped on LATCH U7 /2OE
+                               );
+         MCF5373.PDDR_TIMER := (
+                                PDDR_0 => False,
+                                PDDR_1 => False,
+                                PDDR_2 => False,
+                                PDDR_3 => True,  -- drive LATCH U7 2/OE
+                                others => 0
+                               );
+         MCF5373.PODR_TIMER := (
+                                PODR_0 => False,
+                                PODR_1 => False,
+                                PODR_2 => False,
+                                PODR_3 => False, -- LATCH U7 /2OE active low
+                                others => 0
+                               );
          loop
-            for Delay_Loop_Count in 1 .. 500_000 loop CPU.NOP; end loop;
-            ZOOM.LED := 16#FFFF#;
-            for Delay_Loop_Count in 1 .. 500_000 loop CPU.NOP; end loop;
-            ZOOM.LED := 0;
+            for Delay_Loop_Count in 1 .. 1_000_000 loop CPU.NOP; end loop;
+            ZOOM.LATCH_U7.WRITE := (
+                                    USB1_PWR_EN => True,
+                                    USB2_PWR_EN => True,
+                                    STATUS_1    => True,
+                                    STATUS_2    => False,
+                                    others      => False
+                                   );
+            for Delay_Loop_Count in 1 .. 1_000_000 loop CPU.NOP; end loop;
+            ZOOM.LATCH_U7.WRITE := (
+                                    USB1_PWR_EN => True,
+                                    USB2_PWR_EN => True,
+                                    STATUS_1    => False,
+                                    STATUS_2    => True,
+                                    others      => False
+                                   );
          end loop;
       end if;
       -------------------------------------------------------------------------
--- platforms/ZOOM/zoom.ads
+++ platforms/ZOOM/zoom.ads
@@ -40,7 +40,70 @@ package ZOOM is
    -- low) by lowering the MFP9 - TIN3/TOUT3 pin on the GPIO module, which is
    -- normally assigned to the DMA timers.
    ----------------------------------------------------------------------------
-   LED : aliased Unsigned_16 with
+
+   type LATCH_U7_ACCESS_Type is (READ, WRITE);
+
+   type LATCH_U7_READ_Type is
+   record
+      MODE2       : Boolean;
+      nIRQD       : Boolean; -- negated
+      WRLAN_nINT  : Boolean; -- negated
+      LATCH_GPO_2 : Boolean;
+      LCD_VEEEN   : Boolean;
+      LATCH_GPO_1 : Boolean;
+      nSUSPEND    : Boolean; -- negated
+      nSTANDBY    : Boolean; -- negated
+   end record with
+      Bit_Order => Low_Order_First,
+      Size      => 8;
+   for LATCH_U7_READ_Type use
+   record
+      MODE2       at 0 range 0 .. 0;
+      nIRQD       at 0 range 1 .. 1;
+      WRLAN_nINT  at 0 range 2 .. 2;
+      LATCH_GPO_2 at 0 range 3 .. 3;
+      LCD_VEEEN   at 0 range 4 .. 4;
+      LATCH_GPO_1 at 0 range 5 .. 5;
+      nSUSPEND    at 0 range 6 .. 6;
+      nSTANDBY    at 0 range 7 .. 7;
+   end record;
+
+   type LATCH_U7_WRITE_Type is
+   record
+      USB1_PWR_EN : Boolean;
+      USB2_PWR_EN : Boolean;
+      NAND_nGPIO  : Boolean; -- negated
+      LATCH_GPO_2 : Boolean;
+      LCD_VEEEN   : Boolean;
+      LATCH_GPO_1 : Boolean;
+      STATUS_2    : Boolean;
+      STATUS_1    : Boolean;
+   end record with
+      Bit_Order => Low_Order_First,
+      Size      => 8;
+   for LATCH_U7_WRITE_Type use
+   record
+      USB1_PWR_EN at 0 range 0 .. 0;
+      USB2_PWR_EN at 0 range 1 .. 1;
+      NAND_nGPIO  at 0 range 2 .. 2;
+      LATCH_GPO_2 at 0 range 3 .. 3;
+      LCD_VEEEN   at 0 range 4 .. 4;
+      LATCH_GPO_1 at 0 range 5 .. 5;
+      STATUS_2    at 0 range 6 .. 6;
+      STATUS_1    at 0 range 7 .. 7;
+   end record;
+
+   type LATCH_U7_Type (RW : LATCH_U7_ACCESS_Type := READ) is
+   record
+      case RW is
+         when READ  => READ  : LATCH_U7_READ_Type;
+         when WRITE => WRITE : LATCH_U7_WRITE_Type;
+      end case;
+   end record with
+      Pack            => True,
+      Unchecked_Union => True;
+
+   LATCH_U7 : aliased LATCH_U7_Type with
       Address    => To_Address (16#1008_0000#),
       Volatile   => True,
       Import     => True,
