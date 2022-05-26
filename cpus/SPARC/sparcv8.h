9afa31fe7d1b39bd47a8877d0387dfd32c44c9f5
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2022-05-27 00:14:02 +0200

    SPARC: reworked header file for asm startup

--- cpus/SPARC/sparcv8.h
+++ cpus/SPARC/sparcv8.h
@@ -15,14 +15,21 @@
 #define NOP3  nop ; nop ; nop
 #define NOP10 NOP3 ; NOP3 ; NOP3 ; nop
 
-#define PSR_EF     (1 << 12)
-#define PSR_PIL(x) (((x) & 0xF) << 8)
-#define PSR_S      (1 << 7)
-#define PSR_PS     (1 << 6)
 #define PSR_ET     (1 << 5)
+#define PSR_PS     (1 << 6)
+#define PSR_S      (1 << 7)
+#define PSR_PIL(x) (((x) & 0xF) << 8)
+#define PSR_EF     (1 << 12)
 #define PSR_CWP(x) ((x) & 0x1F)
+#define PSR_ver    (0xF << 24)
+#define PSR_impl   (0xF << 28)
 
-#define BOOTMODE (1 << 14)
+#define MMU_CR_E  (1 << 0)
+#define MMU_CR_BM (1 << 14)
+
+#define ASI_IMPLICIT   0x00
+#define ASI_N          0x04     /* MMU registers RW single-size */
+#define ASI_SCRATCHPAD 0x20     /* reference MMU bypass RW */
 
 #endif /* _SPARCV8_H */
 
