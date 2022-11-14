2d73d3f5f239348c5fdbe76e7f465b9c796d848f
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2022-11-14 22:13:54 +0100

    new platform REF405EP; this is because QEMU no longer provides the Taihu platform

---
+++ platforms/REF405EP/bsp.adb
@@ -0,0 +1,77 @@
+-----------------------------------------------------------------------------------------------------------------------
+--                                                     SweetAda                                                      --
+-----------------------------------------------------------------------------------------------------------------------
+-- __HDS__                                                                                                           --
+-- __FLN__ bsp.adb                                                                                                   --
+-- __DSC__                                                                                                           --
+-- __HSH__ e69de29bb2d1d6434b8b29ae775ad8c2e48c5391                                                                  --
+-- __HDE__                                                                                                           --
+-----------------------------------------------------------------------------------------------------------------------
+-- Copyright (C) 2020, 2021, 2022 Gabriele Galeotti                                                                  --
+--                                                                                                                   --
+-- SweetAda web page: http://sweetada.org                                                                            --
+-- contact address: gabriele.galeotti@sweetada.org                                                                   --
+-- This work is licensed under the terms of the MIT License.                                                         --
+-- Please consult the LICENSE.txt file located in the top-level directory.                                           --
+-----------------------------------------------------------------------------------------------------------------------
+
+with System.Storage_Elements;
+with Interfaces;
+with Definitions;
+with Configure;
+with Core;
+with Bits;
+with PowerPC;
+with PPC405;
+with Exceptions;
+with MMIO;
+with REF405EP;
+
+package body BSP is
+
+   --========================================================================--
+   --                                                                        --
+   --                                                                        --
+   --                           Local declarations                           --
+   --                                                                        --
+   --                                                                        --
+   --========================================================================--
+
+   use System.Storage_Elements;
+   use Interfaces;
+   use Definitions;
+   use Core;
+   use Bits;
+   use PowerPC;
+   use PPC405;
+   use REF405EP;
+
+   --========================================================================--
+   --                                                                        --
+   --                                                                        --
+   --                           Package subprograms                          --
+   --                                                                        --
+   --                                                                        --
+   --========================================================================--
+
+   ----------------------------------------------------------------------------
+   -- Console wrappers
+   ----------------------------------------------------------------------------
+
+   -- procedure Console_Putchar (C : in Character) is null;
+
+   -- procedure Console_Getchar (C : out Character) is null;
+
+   ----------------------------------------------------------------------------
+   -- BSP_Setup
+   ----------------------------------------------------------------------------
+   procedure BSP_Setup is
+   begin
+      -------------------------------------------------------------------------
+      Exceptions.Init;
+      Tclk_Init;
+      MSREE_Set;
+      -------------------------------------------------------------------------
+   end BSP_Setup;
+
+end BSP;
---
+++ platforms/REF405EP/bsp.ads
@@ -0,0 +1,32 @@
+-----------------------------------------------------------------------------------------------------------------------
+--                                                     SweetAda                                                      --
+-----------------------------------------------------------------------------------------------------------------------
+-- __HDS__                                                                                                           --
+-- __FLN__ bsp.ads                                                                                                   --
+-- __DSC__                                                                                                           --
+-- __HSH__ e69de29bb2d1d6434b8b29ae775ad8c2e48c5391                                                                  --
+-- __HDE__                                                                                                           --
+-----------------------------------------------------------------------------------------------------------------------
+-- Copyright (C) 2020, 2021, 2022 Gabriele Galeotti                                                                  --
+--                                                                                                                   --
+-- SweetAda web page: http://sweetada.org                                                                            --
+-- contact address: gabriele.galeotti@sweetada.org                                                                   --
+-- This work is licensed under the terms of the MIT License.                                                         --
+-- Please consult the LICENSE.txt file located in the top-level directory.                                           --
+-----------------------------------------------------------------------------------------------------------------------
+
+package BSP is
+
+   --========================================================================--
+   --                                                                        --
+   --                                                                        --
+   --                               Public part                              --
+   --                                                                        --
+   --                                                                        --
+   --========================================================================--
+
+   -- procedure Console_Putchar (C : in Character);
+   -- procedure Console_Getchar (C : out Character);
+   procedure BSP_Setup;
+
+end BSP;
