d219dd13894d830d6b78c0b52d0e47bd9b11ae02
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2022-12-02 21:59:30 +0100

    STM32F769I: use application per previous commit

--- platforms/STM32F769I/bsp.adb
+++ platforms/STM32F769I/bsp.adb
@@ -15,10 +15,6 @@
 -- Please consult the LICENSE.txt file located in the top-level directory.                                           --
 -----------------------------------------------------------------------------------------------------------------------
 
-with Interfaces;
-with STM32F769I;
-with Console;
-
 package body BSP is
 
    --========================================================================--
@@ -28,9 +24,6 @@ package body BSP is
    --                                                                        --
    --                                                                        --
    --========================================================================--
-
-   use Interfaces;
-   use STM32F769I;
 
    --========================================================================--
    --                                                                        --
@@ -58,22 +51,8 @@ package body BSP is
    -- BSP_Setup
    ----------------------------------------------------------------------------
    procedure BSP_Setup is
-      Delay_Count : constant := 50_000_000;
-      dummy       : Unsigned_32;
    begin
-      --                           15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
-      PORT_GPIOJ.MODER.Value  := 2#00_00_01_00_00_00_00_00_00_00_01_00_00_00_00_00#;
-      --                           10 98 76 54 32 10 98 76 54 32 10 98 76 54 32 10
-      PORT_GPIOJ.OTYPER.Value := 2#00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00#;
-      -- PORT_GPIOJ.OTYPER.Value := 2#00_00_00_00_00_00_00_00_00_10_00_00_00_10_00_00#;
-      PORT_GPIOJ.PUPDR := 0;
-      while True loop
-         dummy := PORT_GPIOJ.ODR.Value;
-         PORT_GPIOJ.ODR.Value := dummy or 16#0000_2020#;
-         for Delay_Loop_Count in 1 .. Delay_Count loop null; end loop;
-         PORT_GPIOJ.ODR.Value := dummy and 16#0000_DFDF#;
-         for Delay_Loop_Count in 1 .. Delay_Count loop null; end loop;
-      end loop;
+      null;
    end BSP_Setup;
 
 end BSP;
