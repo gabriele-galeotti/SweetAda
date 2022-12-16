381ce34615292e31e2fd3bbe48e024a6e423e24b
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2022-12-17 00:40:54 +0100

    Tcl libopenocd: - inherit all OpenOCD functions from library.tcl - prefix openocd to version_numeric

--- libutils/libopenocd.tcl
+++ libutils/libopenocd.tcl
@@ -1,6 +1,6 @@
 
 #
-# SweetAda OpenOCD library.
+# SweetAda OpenOCD Tcl library.
 #
 # Copyright (C) 2020, 2021, 2022 Gabriele Galeotti
 #
@@ -17,11 +17,11 @@
 #
 
 ################################################################################
-# version_numeric                                                              #
+# openocd_version_numeric                                                      #
 #                                                                              #
 # "Open On-Chip Debugger 0.11.0-rc1+dev-00010-gc69b4de"                        #
 ################################################################################
-proc version_numeric {} {
+proc openocd_version_numeric {} {
     # note the trailing ".", so that we correctly process every digit
     # (otherwise the foreach will exit, leaving the last part of the version
     # still hanging)
@@ -42,5 +42,91 @@ proc version_numeric {} {
         }
     }
     return $versionnumber
+}
+
+################################################################################
+# openocd_rpc_set_socket                                                       #
+#                                                                              #
+################################################################################
+proc openocd_rpc_set_socket {s} {
+    global openocd_rpc_socket
+    set openocd_rpc_socket $s
+}
+
+################################################################################
+# openocd_rpc_get_socket                                                       #
+#                                                                              #
+################################################################################
+proc openocd_rpc_get_socket {} {
+    global openocd_rpc_socket
+    return $openocd_rpc_socket
+}
+
+################################################################################
+# openocd_rpc_disconnect                                                       #
+#                                                                              #
+################################################################################
+proc openocd_rpc_disconnect {} {
+    set s [openocd_rpc_get_socket]
+    close $s
+}
+
+################################################################################
+# openocd_rpc_rx                                                               #
+#                                                                              #
+# "noecho" disables output of received data messages output.                   #
+################################################################################
+proc openocd_rpc_rx {{mode ""}} {
+    set s [openocd_rpc_get_socket]
+    while {true} {
+        set data [read $s 4096]
+        if {[eof $s]} {
+            openocd_rpc_disconnect
+            return
+        }
+        if {[llength $data] > 0} {
+            set token [string first \x1A $data]
+            if {$token ne -1} {
+                # end of response, print and exit
+                set outdata [string range $data 0 [expr {$token - 1}]]
+                if {$mode ne "noecho"} {
+                    puts -nonewline stdout $outdata
+                }
+                break
+            } else {
+                # print and continue reading
+                set outdata $data
+                if {$mode ne "noecho"} {
+                    puts -nonewline stdout $outdata
+                }
+            }
+        }
+    }
+    return $outdata
+}
+
+################################################################################
+# openocd_rpc_tx                                                               #
+#                                                                              #
+################################################################################
+proc openocd_rpc_tx {command} {
+    set s [openocd_rpc_get_socket]
+    puts -nonewline $s $command\x1A
+}
+
+################################################################################
+# openocd_rpc_init                                                             #
+#                                                                              #
+################################################################################
+proc openocd_rpc_init {{server 127.0.0.1} {port 6666}} {
+    set s [socket $server $port]
+    openocd_rpc_set_socket $s
+    fconfigure $s \
+        -blocking false \
+        -buffering none \
+        -encoding binary \
+        -eofchar {}
+    fconfigure stdout \
+        -buffering none
 }
 
