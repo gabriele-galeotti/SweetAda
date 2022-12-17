91132fbbbacf1ae3b8238feb0676f450ffb0eeb9
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2022-12-17 12:37:57 +0100

    libopenocd.tcl: wipe out openocd_version_numeric since openocd.cfg could not import it under Python

--- libutils/libopenocd.tcl
+++ libutils/libopenocd.tcl
@@ -17,34 +17,6 @@
 #
 
 ################################################################################
-# openocd_version_numeric                                                      #
-#                                                                              #
-# "Open On-Chip Debugger 0.11.0-rc1+dev-00010-gc69b4de"                        #
-################################################################################
-proc openocd_version_numeric {} {
-    # note the trailing ".", so that we correctly process every digit
-    # (otherwise the foreach will exit, leaving the last part of the version
-    # still hanging)
-    set versionstring [split [lindex [split [version] " "] 3] ""].
-    set versionnumber 0
-    set level ""
-    foreach c $versionstring {
-        if {[string is digit $c]} {
-            set level $level$c
-        } else {
-            set level [format %02s $level]
-            set versionnumber [expr $versionnumber * 100 + $level]
-            if {$c eq "."} {
-                set level ""
-            } else {
-                break
-            }
-        }
-    }
-    return $versionnumber
-}
-
-################################################################################
 # openocd_rpc_set_socket                                                       #
 #                                                                              #
 ################################################################################
