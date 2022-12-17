8ed088273f72db5f53d142cbc0b7f2b34f8a39d3
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2022-12-17 15:41:39 +0100

    STM32F769I: Python & Tcl scripts to talk with OpenOCD

--- platforms/STM32F769I/programmer.tcl
+++ platforms/STM32F769I/programmer.tcl
@@ -11,11 +11,15 @@
 
 #
 # Arguments:
-# none
+# -server   start OpenOCD server
+# -shutdown shutdown OpenOCD server
 #
 # Environment variables:
 # SWEETADA_PATH
 # LIBUTILS_DIRECTORY
+# OPENOCD_PREFIX
+# PLATFORM_DIRECTORY
+# ELFTOOL
 # KERNEL_OUTFILE
 #
 
@@ -38,8 +42,32 @@ source [file join $::env(SWEETADA_PATH) $::env(LIBUTILS_DIRECTORY) libopenocd.tc
 #                                                                              #
 ################################################################################
 
-set KERNEL_OUTFILE [file join $::env(SWEETADA_PATH) $::env(KERNEL_OUTFILE)]
-set START_ADDRESS  0x20000000
+set OPENOCD_PREFIX  $::env(OPENOCD_PREFIX)
+set OPENOCD_CFGFILE [file join $OPENOCD_PREFIX share openocd scripts board stm32f769i-disco.cfg]
+set ELFTOOL         $::env(ELFTOOL)
+set KERNEL_OUTFILE  [file join $::env(SWEETADA_PATH) $::env(KERNEL_OUTFILE)]
+set START_SYMBOL    _start
+
+if {[lindex $argv 0] eq "-server"} {
+    set platform [platform_get]
+    if {$platform eq "windows"} {
+        set OPENOCD_EXECUTABLE [file join $OPENOCD_PREFIX bin openocd.exe]
+        exec cmd.exe /C START "" "$OPENOCD_EXECUTABLE" -f "$OPENOCD_CFGFILE" &
+    } elseif {$platform eq "unix"} {
+        set OPENOCD_EXECUTABLE [file join $OPENOCD_PREFIX bin openocd]
+        # __FIX__
+        # if this script is called from Makefile, and the output is redirected
+        # on a tee pipe (menu-dialog.sh), then the script hangs because tee
+        # seems to wait on an inherited stdout file descriptor, which is kept
+        # opened by the xterm sub-process; have we to re-open it?
+        close stdout
+        exec xterm -hold -e "$OPENOCD_EXECUTABLE" -f "$OPENOCD_CFGFILE" &
+    } else {
+        puts stderr "$SCRIPT_FILENAME: *** Error: platform not recognized."
+        exit 1
+    }
+    exit 0
+}
 
 # local OpenOCD server
 openocd_rpc_init 127.0.0.1 6666
@@ -48,6 +76,16 @@ if {[lindex $argv 0] eq "-shutdown"} {
     openocd_rpc_tx "shutdown"
     exit 0
 }
+
+if {[catch {exec $ELFTOOL -c findsymbol=$START_SYMBOL $KERNEL_OUTFILE} result] eq 0} {
+    # ARM Thumb functions have LSB = 1
+    set START_ADDRESS [format "0x%08X" [expr $result & 0xFFFFFFFE]]
+} else {
+    puts "*** Tcl error: no symbol $START_SYMBOL or no $KERNEL_OUTFILE file available."
+    openocd_rpc_disconnect
+    exit 1
+}
+puts "START ADDRESS = $START_ADDRESS"
 
 openocd_rpc_tx "reset init"
 openocd_rpc_rx
