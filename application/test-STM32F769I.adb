e303c4346ad0a3bcaf8af1cfe5bd8d8c807ab749
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2022-12-17 15:37:29 +0100

    STM32F769I: use CPU.NOP in delay

--- application/test-STM32F769I.adb
+++ application/test-STM32F769I.adb
@@ -1,6 +1,7 @@
 
 with Interfaces;
 with STM32F769I;
+with CPU;
 
 package body Application is
 
@@ -30,6 +31,7 @@ package body Application is
       Delay_Count : constant := 50_000_000;
       dummy       : Unsigned_32;
    begin
+      -- blink the two user LEDs LD1 & LD2
       --                           15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
       PORT_GPIOJ.MODER.Value  := 2#00_00_01_00_00_00_00_00_00_00_01_00_00_00_00_00#;
       --                           10 98 76 54 32 10 98 76 54 32 10 98 76 54 32 10
@@ -39,9 +41,9 @@ package body Application is
       while True loop
          dummy := PORT_GPIOJ.ODR.Value;
          PORT_GPIOJ.ODR.Value := dummy or 16#0000_2020#;
-         for Delay_Loop_Count in 1 .. Delay_Count loop null; end loop;
+         for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
          PORT_GPIOJ.ODR.Value := dummy and 16#0000_DFDF#;
-         for Delay_Loop_Count in 1 .. Delay_Count loop null; end loop;
+         for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
       end loop;
       -------------------------------------------------------------------------
       loop null; end loop;
