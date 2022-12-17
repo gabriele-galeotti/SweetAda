9332d42f8b71b58ff0be6a44c42ef3a80d1e24b0
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2022-12-17 16:05:08 +0100

    MSP432P401R: use CPU.NOP in delay, adjusted loop count

--- application/test-MSP432P401R.adb
+++ application/test-MSP432P401R.adb
@@ -1,6 +1,7 @@
 
 with Interfaces;
 with MSP432P401R;
+with CPU;
 
 package body Application is
 
@@ -27,20 +28,17 @@ package body Application is
    -- Run
    ----------------------------------------------------------------------------
    procedure Run is
-      Delay_Count : constant := 500_000;
+      Delay_Count : constant := 100_000;
       Dummy       : Unsigned_8;
    begin
       WDTCTL := 16#5A84#; -- stop WDT
-      -- blink on-board LED
+      -- blink on-board LED1
       PADIR_L := PADIR_L or 16#01#;
       Dummy := PAOUT_L;
       while True loop
          Dummy := Dummy xor 1;
          PAOUT_L := Dummy;
-         for Delay_Loop_Count in 1 .. Delay_Count loop null; end loop;
---          Dummy := Dummy xor 1;
---          PAOUT_L := Dummy;
---          for Delay_Loop_Count in 1 .. Delay_Count loop null; end loop;
+         for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
       end loop;
       -------------------------------------------------------------------------
       loop null; end loop;
