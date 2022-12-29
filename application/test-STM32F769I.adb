b086a8ce226d7c7fb732d4482257cd6a5898658b
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2022-12-29 23:47:00 +0100

    STM32F769I: change test code

--- application/test-STM32F769I.adb
+++ application/test-STM32F769I.adb
@@ -32,22 +32,26 @@ package body Application is
    procedure Run is
       Delay_Count : constant := 50_000_000;
    begin
-      -- blink user LEDs LD1 & LD2 --------------------------------------------
+      -- blink user LEDs alternately ------------------------------------------
+      -- LD1: RED PJ13 (B9)
+      -- LD2: GRN PJ5 (M14)
       GPIOJ.MODER  := [5 | 13 => GPIO_OUT, others => <>];
       GPIOJ.OTYPER := [5 | 13 => GPIO_PP, others => <>];
       GPIOJ.PUPDR  := [others => GPIO_NOPUPD];
       while True loop
          if False then
             -- direct output values
-            GPIOJ.ODR := [5 | 13 => BITON, others => <>];
+            GPIOJ.ODR := [5 => BITOFF, 13 => BITON, others => <>];
             for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
-            GPIOJ.ODR := [5 | 13 => BITOFF, others => <>];
+            GPIOJ.ODR := [5 => BITON, 13 => BITOFF, others => <>];
             for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
          else
             -- set/reset
-            GPIOJ.BSRR.SET := [5 | 13 => True, others => <>];
+            GPIOJ.BSRR.RST := [5 => True, others => <>];
+            GPIOJ.BSRR.SET := [13 => True, others => <>];
             for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
-            GPIOJ.BSRR.RST := [5 | 13 => True, others => <>];
+            GPIOJ.BSRR.SET := [5 => True, others => <>];
+            GPIOJ.BSRR.RST := [13 => True, others => <>];
             for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
          end if;
       end loop;
