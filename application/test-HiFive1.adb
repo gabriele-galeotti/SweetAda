9660b156c0083f6b595835a8c7d316eb12a317fc
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-01-02 17:47:24 +0100

    HiFive1: use register bitmaps when doing I/O

--- application/test-HiFive1.adb
+++ application/test-HiFive1.adb
@@ -29,28 +29,38 @@ package body Application is
    procedure Run is
    begin
       -------------------------------------------------------------------------
-      -- iof_sel GPIO #19/21/22 = IOF0
-      HiFive1.GPIO_IOFSEL := @ and 16#FF97_FFFF#;
-      HiFive1.GPIO_PORT   := @ or 16#0068_0000#;
-      HiFive1.GPIO_OEN    := @ or 16#0068_0000#;
       declare
-         Delay_Count : constant := 3_000_000;
+         procedure SetPin (Pin : in Integer);
+         procedure SetPin (Pin : in Integer) is
+         begin
+            -- GPIO #19/21/22 = IOF0, output, enabled
+            HiFive1.GPIO_IOFSEL (Pin) := False;
+            HiFive1.GPIO_PORT (Pin) := True;
+            HiFive1.GPIO_OEN (Pin) := True;
+         end SetPin;
+      begin
+         SetPin (19);
+         SetPin (21);
+         SetPin (22);
+      end;
+      declare
+         Delay_Count : constant := 10_000_000;
       begin
          while True loop
             -- turn on RED (GPIO #22)
-            HiFive1.GPIO_PORT := @ and 16#FFBF_FFFF#;
+            HiFive1.GPIO_PORT (22) := False;
             for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
-            HiFive1.GPIO_PORT := @ or 16#0068_0000#;
+            HiFive1.GPIO_PORT (22) := True;
             for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
             -- turn on GREEN (GPIO #19)
-            HiFive1.GPIO_PORT := @ and 16#FFF7_FFFF#;
+            HiFive1.GPIO_PORT (19) := False;
             for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
-            HiFive1.GPIO_PORT := @ or 16#0068_0000#;
+            HiFive1.GPIO_PORT (19) := True;
             for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
             -- turn on BLUE (GPIO #21)
-            HiFive1.GPIO_PORT := @ and 16#FFDF_FFFF#;
+            HiFive1.GPIO_PORT (21) := False;
             for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
-            HiFive1.GPIO_PORT := @ or 16#0068_0000#;
+            HiFive1.GPIO_PORT (21) := True;
             for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
          end loop;
       end;
