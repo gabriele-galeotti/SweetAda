5baad9fb812191655fddc6cdade380c6318ef460
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-01-18 21:41:42 +0100

    libutils: introduce updatetm - update timestamp for win

---
+++ libutils/updatetm.ps1
@@ -0,0 +1,43 @@
+
+#
+# Update timestamp of a file.
+#
+# Copyright (C) 2020-2023 Gabriele Galeotti
+#
+# This work is licensed under the terms of the MIT License.
+# Please consult the LICENSE.txt file located in the top-level directory.
+#
+
+#
+# Arguments:
+# arguments specified in .bat script
+#
+# Environment variables:
+# none
+#
+
+################################################################################
+# ExitWithCode()                                                               #
+#                                                                              #
+################################################################################
+function ExitWithCode
+{
+  param($exitcode)
+  $host.SetShouldExit($exitcode)
+  exit $exitcode
+}
+
+################################################################################
+# Main loop.                                                                   #
+#                                                                              #
+################################################################################
+
+$scriptname = $MyInvocation.MyCommand.Name
+
+$input_filename = $args[0]
+
+$file = Get-Item -Path $input_filename
+$file.LastWriteTime = (Get-Date)
+
+ExitWithCode 0
+
