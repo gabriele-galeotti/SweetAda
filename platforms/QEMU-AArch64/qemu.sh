f8758b50ed3198e8d5c44b63ceeac61f52bbc904
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-01-21 14:57:33 +0100

    QEMU-AArch64: provide plain qemu script

---
+++ platforms/QEMU-AArch64/qemu.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+#
+# QEMU-AArch64 QEMU configuration file.
+#
+
+# QEMU executable
+QEMU_EXECUTABLE="/opt/sweetada/bin/qemu-system-aarch64"
+
+# telnet port numbers for serial ports
+SPPORT0=4446
+
+# console for serial port
+/usr/bin/xterm \
+  -T "QEMU-1" -geometry 80x24 -bg blue -fg white -sl 1024 -e \
+  /bin/telnet localhost ${SPPORT0} \
+  &
+# console for serial port
+/usr/bin/xterm \
+  -T "QEMU-2" -geometry 80x24 -bg blue -fg white -sl 1024 -e \
+  /bin/telnet localhost ${SPPORT1} \
+  &
+
+# QEMU machine
+"${QEMU_EXECUTABLE}" \
+  -M virt -cpu cortex-a53 -m 128 \
+  -kernel ${KERNEL_OUTFILE} \
+  -monitor "telnet:localhost:4445,server,nowait" \
+  -chardev "socket,id=SERIALPORT0,port=${SPPORT0},host=localhost,ipv4=on,server=on,telnet=on,wait=off" \
+  -serial "chardev:SERIALPORT0" \
+  -chardev "socket,id=SERIALPORT1,port=${SPPORT1},host=localhost,ipv4=on,server=on,telnet=on,wait=off" \
+  -serial "chardev:SERIALPORT1"
+
+exit 0
+
