106bb067c18aa4700f99a381b0e31c2123cde676
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-01-22 00:28:19 +0100

    QEMU-RISC-V: add sample debug session to qemu.sh

--- platforms/QEMU-RISC-V/qemu.sh
+++ platforms/QEMU-RISC-V/qemu.sh
@@ -8,6 +8,15 @@
 if [ "x${RISCV32}" = "xY" ] ; then
   QEMU_EXECUTABLE="/opt/sweetada/bin/qemu-system-riscv32"
 fi
+# GDB executable
+GDB_EXECUTABLE="/opt/sweetada/bin/riscv-sweetada-elf-gdb"
+
+# debug options
+if [ "x$1" = "x-debug" ] ; then
+  QEMU_DEBUG="-S -gdb tcp:localhost:1234,ipv4"
+else
+  QEMU_DEBUG=
+fi
 
 # telnet ports
 MONITORPORT=4445
@@ -15,12 +24,12 @@ SERIALPORT0=4446
 SERIALPORT1=4447
 
 # console for serial port
-/usr/bin/xterm \
+setsid /usr/bin/xterm \
   -T "QEMU-1" -geometry 80x24 -bg blue -fg white -sl 1024 -e \
   /bin/telnet localhost ${SERIALPORT0} \
   &
 # console for serial port
-/usr/bin/xterm \
+setsid /usr/bin/xterm \
   -T "QEMU-2" -geometry 80x24 -bg blue -fg white -sl 1024 -e \
   /bin/telnet localhost ${SERIALPORT1} \
   &
@@ -33,7 +42,25 @@ SERIALPORT1=4447
   -chardev "socket,id=SERIALPORT0,port=${SERIALPORT0},host=localhost,ipv4=on,server=on,telnet=on,wait=off" \
   -serial "chardev:SERIALPORT0" \
   -chardev "socket,id=SERIALPORT1,port=${SERIALPORT1},host=localhost,ipv4=on,server=on,telnet=on,wait=off" \
-  -serial "chardev:SERIALPORT1"
+  -serial "chardev:SERIALPORT1" \
+  ${QEMU_DEBUG} \
+  &
+QEMU_PID=$!
 
-exit 0
+# normal execution or debug execution
+if [ "x$1" != "x-debug" ] ; then
+  wait ${QEMU_PID}
+else
+  "${GDB_EXECUTABLE}" \
+    -q \
+    -iex "set basenames-may-differ" \
+    -iex "set architecture riscv" \
+    -iex "set language ada" \
+    "${SWEETADA_PATH}"/${KERNEL_OUTFILE} \
+    -ex "target remote tcp:localhost:1234" \
+    -ex "break *0x80000000" \
+    -ex "continue"
+fi
+
+exit $?
 
