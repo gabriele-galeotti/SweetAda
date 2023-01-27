7bc1c94982cbe326908efa791d4a33e30fe5cace
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-01-27 22:50:45 +0100

    Altera10M50GHRD: use plain QEMU for emulation, run & debug session in qemu startup script

--- platforms/Altera10M50GHRD/qemu.sh
+++ platforms/Altera10M50GHRD/qemu.sh
@@ -7,23 +7,46 @@
 # QEMU executable
 QEMU_EXECUTABLE="/opt/sweetada/bin/qemu-system-nios2"
 
+# debug options
+if [ "x$1" = "x-debug" ] ; then
+  QEMU_DEBUG="-S -gdb tcp:localhost:1234,ipv4"
+else
+  QEMU_DEBUG=
+fi
+
 # telnet ports
 MONITORPORT=4445
 SERIALPORT0=4446
 
 # console for serial port
-/usr/bin/xterm \
+setsid /usr/bin/xterm \
   -T "QEMU-1" -geometry 80x24 -bg blue -fg white -sl 1024 -e \
   /bin/telnet localhost ${SERIALPORT0} \
   &
 
 # QEMU machine
-"${QEMU_EXECUTABLE}" \
+setsid "${QEMU_EXECUTABLE}" \
   -M 10m50-ghrd \
   -kernel ${KERNEL_OUTFILE} \
   -monitor "telnet:localhost:${MONITORPORT},server,nowait" \
   -chardev "socket,id=SERIALPORT0,port=${SERIALPORT0},host=localhost,ipv4=on,server=on,telnet=on,wait=off" \
   -serial "chardev:SERIALPORT0" \
+  ${QEMU_DEBUG} \
+  &
+QEMU_PID=$!
+
+# normal execution or debug execution
+if [ "x$1" != "x-debug" ] ; then
+  wait ${QEMU_PID}
+else
+  "${GDB}" \
+    -q \
+    -iex "set basenames-may-differ" \
+    -iex "set architecture nios2" \
+    -iex "set language ada" \
+    "${SWEETADA_PATH}"/${KERNEL_OUTFILE} \
+    -ex "target remote tcp:localhost:1234"
+fi
 
 exit 0
 
