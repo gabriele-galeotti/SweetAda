399c0a981623667e2ff0f0cc31be789b105918c1
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-02-04 23:49:05 +0100

    Altera10M50GHRD/qemu.sh: function to check if QEMU is ready in order to startup a console

--- platforms/Altera10M50GHRD/qemu.sh
+++ platforms/Altera10M50GHRD/qemu.sh
@@ -4,6 +4,40 @@
 # Altera 10M50GHRD QEMU.
 #
 
+################################################################################
+# tcpport_is_listening()                                                       #
+#                                                                              #
+# $1 = port                                                                    #
+# $2 = timeout in 1 ms units                                                   #
+# $3 = error message prefix (empty = no message); example: "*** Error"         #
+################################################################################
+tcpport_is_listening()
+{
+local time_start
+local port_listening
+local time_current
+time_start=$(date +%s%3N)
+while true ; do
+  port_listening=$(netstat -l -t --numeric-ports | grep -c "^tcp.*localhost:$1.*LISTEN.*\$")
+  [ "${port_listening}" -eq 1 ] && break
+  # date +%s%3N = ms from Epoch
+  time_current=$(date +%s%3N)
+  if [ $((time_current-time_start)) -gt $2 ] ; then
+    if [ "x$3" != "x" ] ; then
+      printf "$3: timeout waiting for port $1.\n"
+    fi
+    return 1
+  fi
+  usleep 10000 # 10 ms
+done
+return 0
+}
+
+################################################################################
+# Main loop.                                                                   #
+#                                                                              #
+################################################################################
+
 # QEMU executable
 QEMU_EXECUTABLE="/opt/sweetada/bin/qemu-system-nios2"
 
@@ -20,33 +54,34 @@ fi
 MONITORPORT=4445
 SERIALPORT0=4446
 
-# console for serial port
-setsid /usr/bin/xterm \
-  -T "QEMU-1" -geometry 80x24 -bg blue -fg white -sl 1024 -e \
-  /bin/telnet localhost ${SERIALPORT0} \
-  &
-
 # QEMU machine
 ${QEMU_SETSID} "${QEMU_EXECUTABLE}" \
   -M 10m50-ghrd \
   -kernel ${KERNEL_OUTFILE} \
   -monitor "telnet:localhost:${MONITORPORT},server,nowait" \
-  -chardev "socket,id=SERIALPORT0,port=${SERIALPORT0},host=localhost,ipv4=on,server=on,telnet=on,wait=off" \
+  -chardev "socket,id=SERIALPORT0,port=${SERIALPORT0},host=localhost,ipv4=on,server=on,telnet=on,wait=on" \
   -serial "chardev:SERIALPORT0" \
   ${QEMU_DEBUG} \
   &
 QEMU_PID=$!
 
+# console for serial port
+tcpport_is_listening ${SERIALPORT0} 3000 "*** Error"
+setsid /usr/bin/xterm \
+  -T "QEMU-1" -geometry 80x24 -bg blue -fg white -sl 1024 -e \
+  /bin/telnet localhost ${SERIALPORT0} \
+  &
+
 # normal execution or debug execution
-if [ "x$1" != "x-debug" ] ; then
+if [ "x$1" = "x" ] ; then
   wait ${QEMU_PID}
-else
+elif [ "x$1" = "x-debug" ] ; then
   "${GDB}" \
     -q \
     -iex "set basenames-may-differ" \
     -iex "set architecture nios2" \
     -iex "set language ada" \
-    "${SWEETADA_PATH}"/${KERNEL_OUTFILE} \
+    ${KERNEL_OUTFILE} \
     -ex "target remote tcp:localhost:1234"
 fi
 
