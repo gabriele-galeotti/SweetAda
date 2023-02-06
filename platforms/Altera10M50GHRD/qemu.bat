318f45db077cd90f8f394693d986822d6d777a76
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-02-06 23:32:15 +0100

    Altera10M50GHRD/qemu.bat: provide the same functionalities of the Bash counterpart

--- platforms/Altera10M50GHRD/qemu.bat
+++ platforms/Altera10M50GHRD/qemu.bat
@@ -5,34 +5,34 @@ REM Altera 10M50GHRD QEMU.
 REM
 
 REM QEMU executable
-SET "QEMU_EXECUTABLE=C:\Program Files\QEMU\qemu-system-nios2w.exe
+SET "QEMU_FILENAME=qemu-system-nios2w.exe"
+SET "QEMU_EXECUTABLE=C:\Program Files\QEMU\%QEMU_FILENAME%"
 
 REM debug options
 IF "%1"=="-debug" (
-  SET "QEMU_BKGND=START """""
   SET "QEMU_DEBUG=-S -gdb tcp:localhost:1234,ipv4"
   SET "PYTHONHOME=%TOOLCHAIN_PREFIX%"
   ) ELSE (
-  SET QEMU_BKGND=
   SET QEMU_DEBUG=
-)
+  )
 
 REM telnet ports
 SET MONITORPORT=4445
 SET SERIALPORT0=4446
 SET SERIALPORT1=4447
 
-REM console for serial port
-START "" "C:\Program Files"\PuTTY\putty-w64.exe telnet://localhost:%SERIALPORT0%/
-
 REM QEMU machine
-%QEMU_BKGND% "%QEMU_EXECUTABLE%" ^
+START "" "%QEMU_EXECUTABLE%" ^
   -M 10m50-ghrd ^
   -kernel %KERNEL_OUTFILE% ^
   -monitor telnet:localhost:%MONITORPORT%,server,nowait ^
-  -chardev socket,id=SERIALPORT0,port=%SERIALPORT0%,host=localhost,ipv4=on,server=on,telnet=on,wait=off ^
+  -chardev socket,id=SERIALPORT0,port=%SERIALPORT0%,host=localhost,ipv4=on,server=on,telnet=on,wait=on ^
   -serial chardev:SERIALPORT0 ^
   %QEMU_DEBUG%
+
+REM console for serial port
+CALL :TCPPORT_IS_LISTENING %SERIALPORT0%
+START "" "C:\Program Files"\PuTTY\putty-w64.exe telnet://localhost:%SERIALPORT0%/
 
 REM debug session
 IF "%1"=="-debug" (
@@ -41,7 +41,37 @@ IF "%1"=="-debug" (
   -iex "set basenames-may-differ" ^
   %KERNEL_OUTFILE% ^
   -ex "target remote tcp:localhost:1234" ^
+  ) ELSE (
+  CALL :QEMUWAIT
   )
 
 EXIT /B %ERRORLEVEL%
+
+:TCPPORT_IS_LISTENING
+SET "PORTOK=N"
+SET "NLOOPS=0"
+:TIL_LOOP
+  timeout /NOBREAK /T 1 >NUL
+  FOR /F "tokens=*" %%I in ('netstat -an ^| find ":%1" ^| find /C "LISTENING"') DO SET VAR=%%I
+  IF "%VAR%" NEQ "0" (
+    SET "PORTOK=Y"
+    GOTO :TIL_LOOPEND
+    )
+  SET /A NLOOPS += 1
+  IF "%NLOOPS%" NEQ "3" GOTO :TIL_LOOP
+:TIL_LOOPEND
+IF NOT "%PORTOK%"=="Y" ECHO TIMEOUT WAITING FOR PORT %1
+GOTO :EOF
+
+:QEMUWAIT
+:QW_LOOP
+tasklist | find /I "%QEMU_FILENAME%" >NUL 2>&1
+IF ERRORLEVEL 1 (
+  GOTO :QW_LOOPEND
+  ) ELSE (
+  timeout /NOBREAK /T 5 >NUL
+  GOTO :QW_LOOP
+  )
+:QW_LOOPEND
+GOTO :EOF
 
