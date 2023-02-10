f5c8b2530339f82c00e2ecaba25e1fe6baa9528a
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-02-10 22:19:34 +0100

    qemu.sh: - remove bashisms - use 1 s resolution to simplify porting to other systems

--- platforms/Altera10M50GHRD/qemu.sh
+++ platforms/Altera10M50GHRD/qemu.sh
@@ -5,30 +5,33 @@
 #
 
 ################################################################################
+# Script initialization.                                                       #
+#                                                                              #
+################################################################################
+
+SCRIPT_FILENAME=$(basename "$0")
+
+################################################################################
 # tcpport_is_listening()                                                       #
 #                                                                              #
 # $1 = port                                                                    #
-# $2 = timeout in 1 ms units                                                   #
+# $2 = timeout in 1 s units                                                    #
 # $3 = error message prefix (empty = no message); example: "*** Error"         #
 ################################################################################
 tcpport_is_listening()
 {
-local time_start
-local port_listening
-local time_current
-time_start=$(date +%s%3N)
+_time_start=$(date +%s)
 while true ; do
-  port_listening=$(netstat -l -t --numeric-ports | grep -c "^tcp.*localhost:$1.*LISTEN.*\$")
-  [ "${port_listening}" -eq 1 ] && break
-  # date +%s%3N = ms from Epoch
-  time_current=$(date +%s%3N)
-  if [ $((time_current-time_start)) -gt $2 ] ; then
+  _port_listening=$(netstat -l -t --numeric-ports | grep -c "^tcp.*localhost:$1.*LISTEN.*\$")
+  [ "${_port_listening}" -eq 1 ] && break
+  _time_current=$(date +%s)
+  if [ $((_time_current-_time_start)) -gt $2 ] ; then
     if [ "x$3" != "x" ] ; then
       printf "$3: timeout waiting for port $1.\n"
     fi
     return 1
   fi
-  usleep 10000 # 10 ms
+  sleep 1
 done
 return 0
 }
@@ -66,7 +69,7 @@ ${QEMU_SETSID} "${QEMU_EXECUTABLE}" \
 QEMU_PID=$!
 
 # console for serial port
-tcpport_is_listening ${SERIALPORT0} 3000 "*** Error"
+tcpport_is_listening ${SERIALPORT0} 3 "*** Error"
 setsid /usr/bin/xterm \
   -T "QEMU-1" -geometry 80x24 -bg blue -fg white -sl 1024 -e \
   /bin/telnet localhost ${SERIALPORT0} \
