38d9889a48d58f7a27d55a010f87e03fb11d44f5
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-03-05 21:50:41 +0100

    Amiga-FS-UAE: - remove runsweetada - modify configuration for all CPUs - use shell script

---
+++ platforms/Amiga-FS-UAE/fs-uae.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+#
+# Amiga (FS-UAE emulator).
+#
+
+################################################################################
+# Script initialization.                                                       #
+#                                                                              #
+################################################################################
+
+SCRIPT_FILENAME=$(basename "$0")
+
+################################################################################
+# Main loop.                                                                   #
+#                                                                              #
+################################################################################
+
+# make the platform directory the CWD so that log files do not end up in the
+# top-level directory
+cd ${PLATFORM_DIRECTORY}
+
+# FS-UAE executable
+FSUAE_EXECUTABLE="/opt/FS-UAE/bin/fs-uae"
+
+"${FSUAE_EXECUTABLE}" \
+  fs-uae.conf
+
+exit 0
+
