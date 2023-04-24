f78c1ad6e2ad3a001feb4226e48d01912ee1b0e6
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-04-24 12:45:14 +0200

    System390: offload postbuild phase to script

---
+++ platforms/System390/postbuild.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+case ${IPL_MODE} in
+  "CARD_READER")
+    "${PYTHON}" S360obj.py -a 0x10000 -p -l loader.rdr ${KERNEL_PARENT_PATH}/${KERNEL_ROMFILE} ${KERNEL_IPLFILE}
+    #"${TCLSH}" S360obj.tcl -a 0x10000 -p -l loader.rdr ${KERNEL_PARENT_PATH}/${KERNEL_ROMFILE} ${KERNEL_IPLFILE}
+    ;;
+  "DASD")
+    rm -f SYSRES.DSD
+    "${PYTHON}" S360obj.py -a 0x10000 -p ${KERNEL_PARENT_PATH}/${KERNEL_ROMFILE} ${KERNEL_IPLFILE}
+    #$(TCLSH) S360obj.tcl -a 0x10000 -p ${KERNEL_PARENT_PATH}/${KERNEL_ROMFILE} ${KERNEL_IPLFILE}
+    "${HERCULES_PREFIX}"/bin/dasdload -0 dasd_ctlfile.txt SYSRES.DSD 5
+    ;;
+esac
+
+exit 0
+
