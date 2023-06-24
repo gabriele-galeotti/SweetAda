8629ff377331b77f358eec5850c4e41d396d16ad
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-06-25 00:39:37 +0200

    Android: - small adjustments - force ZFP - add null address to debug sections

--- platforms/Android/adb.sh
+++ platforms/Android/adb.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
+# ADB tool
 ADB_EXECUTABLE=/opt/platform-tools/adb
+# smartphone directory where to store the kernel in
 DEVICE_DIRECTORY=/sdcard
 
 ${ADB_EXECUTABLE} push kernel.o ${DEVICE_DIRECTORY}
