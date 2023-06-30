f5e4e9c5f3f62d8f3ad0aa0435fbb90bf230e62e
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-06-30 21:17:10 +0200

    MPC8306/application.adb: use mpc83xx unit

--- application/test-MPC8306-Switch.adb
+++ application/test-MPC8306-Switch.adb
@@ -1,7 +1,7 @@
 
 with Interfaces;
 with CPU;
-with MPC8306;
+with MPC83XX;
 with Switch;
 with Console;
 
@@ -33,33 +33,33 @@ package body Application is
 
    procedure SETR is
    begin
-      MPC8306.I2C.CR.MTX := MPC8306.MTX_Receive;
+      MPC83XX.I2C.CR.MTX := MPC83XX.MTX_Receive;
    end SETR;
 
    procedure SETW is
    begin
-      MPC8306.I2C.CR.MTX := MPC8306.MTX_Transmit;
+      MPC83XX.I2C.CR.MTX := MPC83XX.MTX_Transmit;
    end SETW;
 
    procedure SETACK is
    begin
-      MPC8306.I2C.CR.TXAK := False;
+      MPC83XX.I2C.CR.TXAK := False;
    end SETACK;
 
    procedure SETNOACK is
    begin
-      MPC8306.I2C.CR.TXAK := True;
+      MPC83XX.I2C.CR.TXAK := True;
    end SETNOACK;
 
    procedure TX (Code : in Unsigned_8) is
    begin
-      MPC8306.I2C.DR := Code;
+      MPC83XX.I2C.DR := Code;
       loop
-         exit when MPC8306.I2C.SR.MIF;
+         exit when MPC83XX.I2C.SR.MIF;
       end loop;
-      MPC8306.I2C.SR.MIF := False;
+      MPC83XX.I2C.SR.MIF := False;
       loop
-         exit when not MPC8306.I2C.SR.RXAK;
+         exit when not MPC83XX.I2C.SR.RXAK;
       end loop;
    end TX;
 
@@ -74,9 +74,9 @@ package body Application is
             Delay_Count : constant := 5_000_000;
          begin
             loop
-               MPC8306.GP1DAT := 16#0000_0200#;
+               MPC83XX.GP1DAT := 16#0000_0200#;
                for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
-               MPC8306.GP1DAT := 16#0000_0000#;
+               MPC83XX.GP1DAT := 16#0000_0000#;
                for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
             end loop;
          end;
@@ -89,42 +89,42 @@ package body Application is
             Data2 : Unsigned_8;
          begin
             loop
-               MPC8306.I2C.FDR      := 16#3F#; -- prescaler, /32768
-               MPC8306.I2C.ADR.ADDR := 16#02#; -- slave address
-               MPC8306.I2C.CR       := (
+               MPC83XX.I2C.FDR      := 16#3F#; -- prescaler, /32768
+               MPC83XX.I2C.ADR.ADDR := 16#02#; -- slave address
+               MPC83XX.I2C.CR       := (
                                         MEN    => False,
                                         MIEN   => False,
                                         MSTA   => True,
-                                        MTX    => MPC8306.MTX_Transmit,
+                                        MTX    => MPC83XX.MTX_Transmit,
                                         TXAK   => False,
                                         RSTA   => False,
                                         BCST   => False,
                                         others => 0
                                         );
-               MPC8306.I2C.CR.MEN   := True; -- master, transmit
+               MPC83XX.I2C.CR.MEN   := True; -- master, transmit
                -- wait bus busy
                loop
-                  exit when MPC8306.I2C.SR.MBB;
+                  exit when MPC83XX.I2C.SR.MBB;
                end loop;
                TX (Switch.LM75AIM_I2C_ADDRESS + 1);
                SETR;
                SETACK;
                --
-               Data := MPC8306.I2C.DR;
+               Data := MPC83XX.I2C.DR;
                loop
-                  exit when MPC8306.I2C.SR.MIF;
+                  exit when MPC83XX.I2C.SR.MIF;
                end loop;
-               MPC8306.I2C.SR.MIF := False;
+               MPC83XX.I2C.SR.MIF := False;
                SETNOACK;
                --
-               Data1 := MPC8306.I2C.DR;
+               Data1 := MPC83XX.I2C.DR;
                loop
-                  exit when MPC8306.I2C.SR.MIF;
+                  exit when MPC83XX.I2C.SR.MIF;
                end loop;
-               MPC8306.I2C.SR.MIF := False;
+               MPC83XX.I2C.SR.MIF := False;
                --
-               MPC8306.I2C.CR.MSTA := False;
-               Data2 := MPC8306.I2C.DR;
+               MPC83XX.I2C.CR.MSTA := False;
+               Data2 := MPC83XX.I2C.DR;
                --
                Console.Print (Data1, Prefix => "", NL => False);
                Console.Print (Data2, Prefix => "", NL => True);
