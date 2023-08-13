2f67c0eb45adf3de42b8115cc49db19d83f9dfe5
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-08-13 23:31:31 +0200

    RaspberryPi3: - remove a default GCC switch - cosmetically adjusted configuration and creation of SD card,   moving parameters from the script

--- platforms/RaspberryPi3/createbootdevice.sh
+++ platforms/RaspberryPi3/createbootdevice.sh
@@ -32,23 +32,21 @@ SCRIPT_FILENAME=$(basename "$0")
 #                                                                              #
 ################################################################################
 
-# path to FAT32 1st partition on a micro-SD memory card
-USDCARD_UUID=3FFC-F14C
-USDCARD_MOUNTPOINT=/mnt/hd
-
-cat > config.txt << EOF
-arm_64bit=1                     # ARMv8 mode
-core_freq=250                   # core clock frequency
-arm_freq=250                    # ARM clock frequency
-kernel=kernel.rom
-enable_jtag_gpio=1              # enable JTAG (GPIO22..27)
-EOF
+# install SweetAda on FAT32 1st partition of a micro-SD memory card
 
 USDCARD_DEVICE=$(blkid | grep UUID=\"${USDCARD_UUID}\" | sed -e "s|^\(/dev/.*\)\(:.*\)\$|\1|")
 if [ "x${USDCARD_DEVICE}" = "x" ] ; then
   echo "*** Error: UUID=${USDCARD_UUID}: no device found."
   exit 1
 fi
+
+cat > config.txt << EOF
+arm_64bit=1                     # ARMv8 mode
+core_freq=250                   # core clock frequency
+arm_freq=250                    # ARM clock frequency
+kernel=${KERNEL_ROMFILE}
+enable_jtag_gpio=1              # enable JTAG (GPIO22..27)
+EOF
 
 mount ${USDCARD_DEVICE} ${USDCARD_MOUNTPOINT}
 cp -f -v config.txt ${SWEETADA_PATH}/${KERNEL_ROMFILE} ${USDCARD_MOUNTPOINT}/ || exit 1
