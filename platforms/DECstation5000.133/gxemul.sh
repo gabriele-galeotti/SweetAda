6b6f49b82070ebaf0ca6bece9583877d920c45a9
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-09-03 15:53:54 +0200

    DECstation5000.133/gxemul.sh: allow run in background with a framebuffer window, so that closing it will make the process terminate rather than hanging

--- platforms/DECstation5000.133/gxemul.sh
+++ platforms/DECstation5000.133/gxemul.sh
@@ -41,7 +41,11 @@ GXEMUL_EXECUTABLE="/opt/GXemul/bin/gxemul"
 # print executed instructions (-i) and dump registers (-r) on stdout
 # -i -r
 #
-"${GXEMUL_EXECUTABLE}" -x -Q -E decstation -e 3min 0xbfc00000:${SWEETADA_PATH}/${KERNEL_ROMFILE}
+"${GXEMUL_EXECUTABLE}"                          \
+  -X -x -Q                                      \
+  -E decstation -e 3min                         \
+  0xBFC00000:${SWEETADA_PATH}/${KERNEL_ROMFILE} \
+  &
 
 exit $?
 
