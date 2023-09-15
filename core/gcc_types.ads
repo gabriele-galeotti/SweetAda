0530976b81e1be95e58ac912148e09d37420d4cb
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-09-15 02:42:16 +0200

    core/gcc_types.ads: use new style

--- core/gcc_types.ads
+++ core/gcc_types.ads
@@ -31,49 +31,57 @@ package GCC_Types is
    pragma Pure;
 
    -- 8-bit
-   type QI_Type is range -2**((System.Storage_Unit * 1) - 1) .. +2**((System.Storage_Unit * 1) - 1) - 1 with
-      Alignment => Interfaces.Integer_8'Alignment,
-      Size      => System.Storage_Unit * 1;
-   type UQI_Type is mod 2**(System.Storage_Unit * 1) with
-      Alignment => Interfaces.Unsigned_8'Alignment,
-      Size      => System.Storage_Unit * 1;
+   type QI_Type is range -2**((System.Storage_Unit * 1) - 1) .. +2**((System.Storage_Unit * 1) - 1) - 1
+      with Alignment => Interfaces.Integer_8'Alignment,
+           Size      => System.Storage_Unit * 1;
+   type UQI_Type is mod 2**(System.Storage_Unit * 1)
+      with Alignment => Interfaces.Unsigned_8'Alignment,
+           Size      => System.Storage_Unit * 1;
    -- 16-bit
-   type HI_Type is range -2**((System.Storage_Unit * 2) - 1) .. +2**((System.Storage_Unit * 2) - 1) - 1 with
-      Alignment => Interfaces.Integer_16'Alignment,
-      Size      => System.Storage_Unit * 2;
-   type UHI_Type is mod 2**(System.Storage_Unit * 2) with
-      Alignment => Interfaces.Unsigned_16'Alignment,
-      Size      => System.Storage_Unit * 2;
+   type HI_Type is range -2**((System.Storage_Unit * 2) - 1) .. +2**((System.Storage_Unit * 2) - 1) - 1
+      with Alignment => Interfaces.Integer_16'Alignment,
+           Size      => System.Storage_Unit * 2;
+   type UHI_Type is mod 2**(System.Storage_Unit * 2)
+      with Alignment => Interfaces.Unsigned_16'Alignment,
+           Size      => System.Storage_Unit * 2;
    -- 32-bit
-   type SI_Type is range -2**((System.Storage_Unit * 4) - 1) .. +2**((System.Storage_Unit * 4) - 1) - 1 with
-      Alignment => Interfaces.Integer_32'Alignment,
-      Size      => System.Storage_Unit * 4;
-   type USI_Type is mod 2**(System.Storage_Unit * 4) with
-      Alignment => Interfaces.Unsigned_32'Alignment,
-      Size      => System.Storage_Unit * 4;
+   type SI_Type is range -2**((System.Storage_Unit * 4) - 1) .. +2**((System.Storage_Unit * 4) - 1) - 1
+      with Alignment => Interfaces.Integer_32'Alignment,
+           Size      => System.Storage_Unit * 4;
+   type USI_Type is mod 2**(System.Storage_Unit * 4)
+      with Alignment => Interfaces.Unsigned_32'Alignment,
+           Size      => System.Storage_Unit * 4;
    -- 64-bit
-   type DI_Type is range -2**((System.Storage_Unit * 8) - 1) .. +2**((System.Storage_Unit * 8) - 1) - 1 with
-      Alignment => Interfaces.Integer_64'Alignment,
-      Size      => System.Storage_Unit * 8;
-   type UDI_Type is mod 2**(System.Storage_Unit * 8) with
-      Alignment => Interfaces.Unsigned_64'Alignment,
-      Size      => System.Storage_Unit * 8;
+   type DI_Type is range -2**((System.Storage_Unit * 8) - 1) .. +2**((System.Storage_Unit * 8) - 1) - 1
+      with Alignment => Interfaces.Integer_64'Alignment,
+           Size      => System.Storage_Unit * 8;
+   type UDI_Type is mod 2**(System.Storage_Unit * 8)
+      with Alignment => Interfaces.Unsigned_64'Alignment,
+           Size      => System.Storage_Unit * 8;
    -- 128-bit
    type TI_Type is new Interfaces.Integer_128;
    type UTI_Type is new Interfaces.Unsigned_128;
 
-   function Shift_Left (Value : USI_Type; Amount : Natural) return USI_Type with
-      Import     => True,
-      Convention => Intrinsic;
-   function Shift_Right (Value : USI_Type; Amount : Natural) return USI_Type with
-      Import     => True,
-      Convention => Intrinsic;
+   function Shift_Left
+      (Value : USI_Type; Amount : Natural)
+      return USI_Type
+      with Import     => True,
+           Convention => Intrinsic;
+   function Shift_Right
+      (Value : USI_Type; Amount : Natural)
+      return USI_Type
+      with Import     => True,
+           Convention => Intrinsic;
 
-   function Shift_Left (Value : UDI_Type; Amount : Natural) return UDI_Type with
-      Import     => True,
-      Convention => Intrinsic;
-   function Shift_Right (Value : UDI_Type; Amount : Natural) return UDI_Type with
-      Import     => True,
-      Convention => Intrinsic;
+   function Shift_Left
+      (Value : UDI_Type; Amount : Natural)
+      return UDI_Type
+      with Import     => True,
+           Convention => Intrinsic;
+   function Shift_Right
+      (Value : UDI_Type; Amount : Natural)
+      return UDI_Type
+      with Import     => True,
+           Convention => Intrinsic;
 
 end GCC_Types;
