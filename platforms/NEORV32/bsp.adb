16040e69830e68af6bd22f03e96688bec4d4ce5c
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-11-25 04:19:05 +0100

    NEORV32: update setup of timer interrupt

--- platforms/NEORV32/bsp.adb
+++ platforms/NEORV32/bsp.adb
@@ -110,6 +110,7 @@ package body BSP is
       -------------------------------------------------------------------------
       Timer_Value := mtime_Read + Timer_Constant;
       mtimecmp_Write (Timer_Value);
+      mie_Set_Interrupt ((MTIE => True, others => <>));
       Irq_Enable;
       -------------------------------------------------------------------------
    end Setup;
