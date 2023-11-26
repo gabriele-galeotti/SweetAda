1c2c0706c7323c7c4e5ce7f29af39741a968d936
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-11-26 15:27:48 +0100

    QEMU-R2D-PLUS: new Hitachi/Rensas SH4 QEMU platform

---
+++ platforms/QEMU-R2D-PLUS/bsp.adb
@@ -0,0 +1,107 @@
+-----------------------------------------------------------------------------------------------------------------------
+--                                                     SweetAda                                                      --
+-----------------------------------------------------------------------------------------------------------------------
+-- __HDS__                                                                                                           --
+-- __FLN__ bsp.adb                                                                                                   --
+-- __DSC__                                                                                                           --
+-- __HSH__ e69de29bb2d1d6434b8b29ae775ad8c2e48c5391                                                                  --
+-- __HDE__                                                                                                           --
+-----------------------------------------------------------------------------------------------------------------------
+-- Copyright (C) 2020-2023 Gabriele Galeotti                                                                         --
+--                                                                                                                   --
+-- SweetAda web page: http://sweetada.org                                                                            --
+-- contact address: gabriele.galeotti@sweetada.org                                                                   --
+-- This work is licensed under the terms of the MIT License.                                                         --
+-- Please consult the LICENSE.txt file located in the top-level directory.                                           --
+-----------------------------------------------------------------------------------------------------------------------
+
+with Definitions;
+with Bits;
+with Exceptions;
+with SH7750;
+with MMIO;
+with Console;
+
+package body BSP is
+
+   --========================================================================--
+   --                                                                        --
+   --                                                                        --
+   --                           Local declarations                           --
+   --                                                                        --
+   --                                                                        --
+   --========================================================================--
+
+   use Interfaces;
+   use Definitions;
+   use Bits;
+   use SH7750;
+
+   --========================================================================--
+   --                                                                        --
+   --                                                                        --
+   --                           Package subprograms                          --
+   --                                                                        --
+   --                                                                        --
+   --========================================================================--
+
+   ----------------------------------------------------------------------------
+   -- Console wrappers
+   ----------------------------------------------------------------------------
+
+   procedure Console_Putchar (C : in Character) is
+   begin
+      -- wait for transmitter available
+      loop
+         exit when SCIF.SCFDR2.T < 16#10#;
+      end loop;
+      SCIF.SCFTDR2 := To_U8 (C);
+   end Console_Putchar;
+
+   procedure Console_Getchar (C : out Character) is
+      Data : Unsigned_8;
+   begin
+      -- wait for receiver available
+      loop
+         exit when SCIF.SCFDR2.R > 0;
+      end loop;
+      Data := SCIF.SCFRDR2;
+      SCIF.SCFSR2.RDF := False;
+      C := To_Ch (Data);
+   end Console_Getchar;
+
+   ----------------------------------------------------------------------------
+   -- Setup
+   ----------------------------------------------------------------------------
+   procedure Setup is
+   begin
+      -- enable SCIF RX/TX ----------------------------------------------------
+      SCIF.SCSCR2 := (
+         CKE1   => CKE1_Internal,
+         REIE   => False,
+         RE     => True,
+         TE     => True,
+         RIE    => False,
+         TIE    => False,
+         others => <>
+         );
+      SCIF.SCFCR2 := (
+         LOOPBACK => False,
+         RFRST    => False,
+         TFRST    => False,
+         MCE      => False,
+         TTRG     => TTRG_1,
+         RTRG     => RTRG_1,
+         RSTRG    => 0,
+         others   => 0
+         );
+      -- Console --------------------------------------------------------------
+      Console.Console_Descriptor.Write := Console_Putchar'Access;
+      Console.Console_Descriptor.Read  := Console_Getchar'Access;
+      Console.Print (ANSI_CLS & ANSI_CUPHOME & VT100_LINEWRAP);
+      -------------------------------------------------------------------------
+      Console.Print ("Renesas R2D-PLUS (QEMU emulator)", NL => True);
+      -------------------------------------------------------------------------
+   end Setup;
+
+end BSP;
