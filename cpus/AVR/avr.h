13d9d02f84dc0ed72861afd69f3fc4e2891c0a2e
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-11-27 20:55:31 +0100

    cpus/AVR: create avr.h

---
+++ cpus/AVR/avr.h
@@ -0,0 +1,21 @@
+
+/*
+ * avr.h - AVR architecture definitions.
+ *
+ * Copyright (C) 2020-2023 Gabriele Galeotti
+ *
+ * This work is licensed under the terms of the MIT License.
+ * Please consult the LICENSE.txt file located in the top-level directory.
+ */
+
+#ifndef _AVR_H
+#define _AVR_H 1
+
+#define RAMPZ  0x3B
+#define SPL    0x3D
+#define SPH    0x3E
+#define SREG   0x3F
+#define RAMEND 0x8FF
+
+#endif /* _AVR_H */
+
