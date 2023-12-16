b5423cfe2526229d69c0efd59135d0729ca63149
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-12-16 14:25:42 +0100

    clibrary/c_wrappers.adb: use new style

--- clibrary/c_wrappers.adb
+++ clibrary/c_wrappers.adb
@@ -15,7 +15,8 @@
 -- Please consult the LICENSE.txt file located in the top-level directory.                                           --
 -----------------------------------------------------------------------------------------------------------------------
 
-package body C_Wrappers is
+package body C_Wrappers
+   is
 
    --========================================================================--
    --                                                                        --
