009f7211ab60ba58fd0187ed284bfb5270415315
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-12-27 14:35:53 +0100

    applications/test-QEMU-OpenRISC.ad[b|s]: minimal test application

---
+++ application/test-QEMU-OpenRISC.adb
@@ -0,0 +1,38 @@
+
+with System;
+with CPU;
+with Console;
+
+package body Application is
+
+   --========================================================================--
+   --                                                                        --
+   --                                                                        --
+   --                           Package subprograms                          --
+   --                                                                        --
+   --                                                                        --
+   --========================================================================--
+
+   ----------------------------------------------------------------------------
+   -- Run
+   ----------------------------------------------------------------------------
+   procedure Run is
+   begin
+      Console.Print_Memory (System.Null_Address, 16, 16);
+      -------------------------------------------------------------------------
+      if True then
+         declare
+            Delay_Count : constant := 50_000_000;
+         begin
+            loop
+               Console.Print ("hello, SweetAda", NL => True);
+               for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
+            end loop;
+         end;
+      end if;
+      -------------------------------------------------------------------------
+      loop null; end loop;
+      -------------------------------------------------------------------------
+   end Run;
+
+end Application;
