be05781a4e4d45dc23fbfea5f98eb35c8a011bb8
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-12-27 12:01:16 +0100

    M5235: remove hardwired postbuild command and make it a script

---
+++ platforms/M5235BCC/postbuild.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# create an S-record kernel file for dBUG download
+${OBJCOPY}                                 \
+  -O srec                                  \
+  ${SWEETADA_PATH}/${KERNEL_OUTFILE}       \
+  ${SWEETADA_PATH}/${KERNEL_BASENAME}.srec
+
