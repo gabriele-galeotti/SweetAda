6c3f9c43d1a434b6050294c41e7d598349606d12
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2023-12-27 12:20:24 +0100

    platforms/QEMU-OpenRISC: new platform
    Does nothing, but is able to startup and send messages to console.

---
+++ platforms/QEMU-OpenRISC/bsp.adb
@@ -0,0 +1,110 @@
+-----------------------------------------------------------------------------------------------------------------------
+--                                                     SweetAda                                                      --
+-----------------------------------------------------------------------------------------------------------------------
+-- __HDS__                                                                                                           --
+-- __FLN__ bsp.adb                                                                                                   --
+-- __DSC__                                                                                                           --
+-- __HSH__ e69de29bb2d1d6434b8b29ae775ad8c2e48c5391                                                                  --
+-- __HDE__                                                                                                           --
+-----------------------------------------------------------------------------------------------------------------------
+-- Copyright (C) 2020-2023 Gabriele Galeotti                                                                         --
+--                                                                                                                   --
+-- SweetAda web page: http://sweetada.org                                                                            --
+-- contact address: gabriele.galeotti@sweetada.org                                                                   --
+-- This work is licensed under the terms of the MIT License.                                                         --
+-- Please consult the LICENSE.txt file located in the top-level directory.                                           --
+-----------------------------------------------------------------------------------------------------------------------
+
+with System;
+with System.Parameters;
+with System.Secondary_Stack;
+with System.Storage_Elements;
+with Configure;
+with Definitions;
+with Bits;
+with MMIO;
+with Exceptions;
+with Virt;
+with Console;
+
+package body BSP is
+
+   --========================================================================--
+   --                                                                        --
+   --                                                                        --
+   --                           Local declarations                           --
+   --                                                                        --
+   --                                                                        --
+   --========================================================================--
+
+   use System.Storage_Elements;
+   use Interfaces;
+   use Definitions;
+   use Bits;
+   use Virt;
+
+   BSP_SS_Stack : System.Secondary_Stack.SS_Stack_Ptr;
+
+   function Get_Sec_Stack return System.Secondary_Stack.SS_Stack_Ptr with
+      Export        => True,
+      Convention    => C,
+      External_Name => "__gnat_get_secondary_stack";
+
+   --========================================================================--
+   --                                                                        --
+   --                                                                        --
+   --                           Package subprograms                          --
+   --                                                                        --
+   --                                                                        --
+   --========================================================================--
+
+   ----------------------------------------------------------------------------
+   -- Get_Sec_Stack
+   ----------------------------------------------------------------------------
+   function Get_Sec_Stack return System.Secondary_Stack.SS_Stack_Ptr is
+   begin
+      return BSP_SS_Stack;
+   end Get_Sec_Stack;
+
+   ----------------------------------------------------------------------------
+   -- Console wrappers
+   ----------------------------------------------------------------------------
+
+   procedure Console_Putchar (C : in Character) is
+   begin
+      UART16x50.TX (UART_Descriptor, To_U8 (C));
+   end Console_Putchar;
+
+   procedure Console_Getchar (C : out Character) is
+      Data : Unsigned_8;
+   begin
+      UART16x50.RX (UART_Descriptor, Data);
+      C := To_Ch (Data);
+   end Console_Getchar;
+
+   ----------------------------------------------------------------------------
+   -- Setup
+   ----------------------------------------------------------------------------
+   procedure Setup is
+   begin
+      -------------------------------------------------------------------------
+      System.Secondary_Stack.SS_Init (BSP_SS_Stack, System.Parameters.Unspecified_Size);
+      -------------------------------------------------------------------------
+      Exceptions.Init;
+      -- UART -----------------------------------------------------------------
+      UART_Descriptor.Read_8        := MMIO.Read'Access;
+      UART_Descriptor.Write_8       := MMIO.Write'Access;
+      UART_Descriptor.Base_Address  := To_Address (UART_BASEADDRESS);
+      UART_Descriptor.Scale_Address := 0;
+      UART_Descriptor.Baud_Clock    := 1_843_200;
+      UART16x50.Init (UART_Descriptor);
+      -- Console --------------------------------------------------------------
+      Console.Console_Descriptor.Write := Console_Putchar'Access;
+      Console.Console_Descriptor.Read  := Console_Getchar'Access;
+      Console.Print (ANSI_CLS & ANSI_CUPHOME & VT100_LINEWRAP);
+      -------------------------------------------------------------------------
+      Console.Print ("OpenRISC " & Configure.CPU_MODEL & " (QEMU emulator)", NL => True);
+      -------------------------------------------------------------------------
+   end Setup;
+
+end BSP;
