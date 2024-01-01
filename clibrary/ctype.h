d3801b72ebc42a2fd036067c125252c7cc1573a7
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-01-01 21:39:03 +0100

    clibrary: update copyright

--- clibrary/ctype.h
+++ clibrary/ctype.h
@@ -2,7 +2,7 @@
 /*
  * ctype.h - CTYPE library header.
  *
- * Copyright (C) 2020-2023 Gabriele Galeotti
+ * Copyright (C) 2020-2024 Gabriele Galeotti
  *
  * This work is licensed under the terms of the MIT License.
  * Please consult the LICENSE.txt file located in the top-level directory.
--- clibrary/string.c
+++ clibrary/string.c
@@ -2,7 +2,7 @@
 /*
  * string.c - STRING implementation library.
  *
- * Copyright (C) 2020-2023 Gabriele Galeotti
+ * Copyright (C) 2020-2024 Gabriele Galeotti
  *
  * This work is licensed under the terms of the MIT License.
  * Please consult the LICENSE.txt file located in the top-level directory.
