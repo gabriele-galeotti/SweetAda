1b9a5530fea63867808ce05dfd7c877bc58f58d9
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-01-02 11:43:01 +0100

    cpus: update copyright notice

--- cpus/ARM/armv4.h
+++ cpus/ARM/armv4.h
@@ -2,7 +2,7 @@
 /*
  * armv4.h - ARM architecture definitions.
  *
- * Copyright (C) 2020-2023 Gabriele Galeotti
+ * Copyright (C) 2020-2024 Gabriele Galeotti
  *
  * This work is licensed under the terms of the MIT License.
  * Please consult the LICENSE.txt file located in the top-level directory.
