00f807dc7af5029e3ff03d325bd55935d6459fa2
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-01-03 16:01:39 +0100

    Amiga-FS-UAE/mmu.*: use new style

--- platforms/Amiga-FS-UAE/platform-68030/mmu.adb
+++ platforms/Amiga-FS-UAE/platform-68030/mmu.adb
@@ -22,7 +22,8 @@ with Interfaces;
 with Bits;
 with M68030;
 
-package body MMU is
+package body MMU
+   is
 
    --========================================================================--
    --                                                                        --
@@ -40,13 +41,13 @@ package body MMU is
 
    Root_Pointer : aliased RPDSC_Type;
 
-   type Table4k_Type is array (Natural range <>) of SFPDSC_Type with
-      Alignment => 16,
-      Pack      => True;
+   type Table4k_Type is array (Natural range <>) of SFPDSC_Type
+      with Alignment => 16,
+           Pack      => True;
 
-   Table_1 : aliased Table4k_Type (0 .. 4095) with
-      Volatile                => True,
-      Suppress_Initialization => True;
+   Table_1 : aliased Table4k_Type (0 .. 4095)
+      with Volatile                => True,
+           Suppress_Initialization => True;
 
    --========================================================================--
    --                                                                        --
@@ -64,29 +65,28 @@ package body MMU is
       TA  : Unsigned_32;
       TCR : TCR_Type;
       function To_U32 is new Ada.Unchecked_Conversion (Integer_Address, Unsigned_32);
-      procedure Page_Setup (
-                            Idx : in Natural;
-                            DT  : in DT_Type;
-                            BA  : in Unsigned_32
-                           );
-      procedure Page_Setup (
-                            Idx : in Natural;
-                            DT  : in DT_Type;
-                            BA  : in Unsigned_32
-                           ) is
+      procedure Page_Setup
+         (Idx : in Natural;
+          DT  : in DT_Type;
+          BA  : in Unsigned_32);
+      procedure Page_Setup
+         (Idx : in Natural;
+          DT  : in DT_Type;
+          BA  : in Unsigned_32)
+         is
       begin
          -- the right shift is equivalent to mask off the first 8 bits of the
          -- address, which are occupied by the flags; this way the address
          -- (masked) appears "normally" laid out, with LSB in the 0 position
          Table_1 (Idx) := (
-                           DT     => DT,
-                           WP     => False,
-                           U      => False,
-                           M      => False,
-                           CI     => False,
-                           PA     => Bits_24 (Shift_Right (BA, 8) and 16#00FF_FFFF#),
-                           others => <>
-                          );
+            DT     => DT,
+            WP     => False,
+            U      => False,
+            M      => False,
+            CI     => False,
+            PA     => Bits_24 (Shift_Right (BA, 8) and 16#00FF_FFFF#),
+            others => <>
+            );
       end Page_Setup;
    begin
       -- cleanup
@@ -99,14 +99,10 @@ package body MMU is
          Page_Setup (Idx, DT_PAGEDSC, A);
          A := @ + 16#1000#;
       end loop;
-      -- CIAB
-      Page_Setup (16#BFD#, DT_PAGEDSC, 16#00BF_D000#);
-      -- CIAA
-      Page_Setup (16#BFE#, DT_PAGEDSC, 16#00BF_E000#);
-      -- Gayle
-      Page_Setup (16#DD2#, DT_PAGEDSC, 16#00DD_2000#);
-      -- CUSTOM
-      Page_Setup (16#DFF#, DT_PAGEDSC, 16#00DF_F000#);
+      Page_Setup (16#BFD#, DT_PAGEDSC, 16#00BF_D000#); -- CIAB
+      Page_Setup (16#BFE#, DT_PAGEDSC, 16#00BF_E000#); -- CIAA
+      Page_Setup (16#DD2#, DT_PAGEDSC, 16#00DD_2000#); -- Gayle
+      Page_Setup (16#DFF#, DT_PAGEDSC, 16#00DF_F000#); -- CUSTOM
       -- ZORROII
       A := 16#00E8_0000#;
       for Idx in 16#E80# .. 16#EFF# loop
@@ -119,29 +115,29 @@ package body MMU is
          Page_Setup (Idx, DT_PAGEDSC, A);
          A := @ + 16#1000#;
       end loop;
-      -- Enable MMU
+      -- enable MMU
       TA := To_U32 (To_Integer (Table_1 (0)'Address));
       Root_Pointer := (
-                       TA_LO  => Bits_12 (Shift_Right (LWord (TA), 4)), -- Table Address 4 .. 15
-                       TA_HI  => HWord (TA),                            -- Table Address 16 .. 31
-                       DT     => DT_VALID4,                             -- Descriptor Type
-                       LIMIT  => 16#4000#,                              -- Limit
-                       LU     => LU_UPPER,                              -- Lower/Upper
-                       others => <>
-                      );
+         TA_LO  => Bits_12 (Shift_Right (LWord (TA), 4)), -- Table Address 4 .. 15
+         TA_HI  => HWord (TA),                            -- Table Address 16 .. 31
+         DT     => DT_VALID4,                             -- Descriptor Type
+         LIMIT  => 16#4000#,                              -- Limit
+         LU     => LU_UPPER,                              -- Lower/Upper
+         others => <>
+         );
       CRP_Set (Root_Pointer'Address);
       TCR := (
-              TID    => 0,     -- Table Index
-              TIC    => 0,     -- Table Index
-              TIB    => 0,     -- Table Index
-              TIA    => 12,    -- Table Index (1st table has 4096 entries)
-              ISHIFT => 8,     -- Initial Shift 24-bit address
-              PS     => PS_4k, -- Page Size (12-bit)
-              FCL    => False, -- Function Code Lookup
-              SRE    => False, -- use CRP
-              E      => True,  -- Enable
-              others => <>
-             );
+         TID    => 0,     -- Table Index
+         TIC    => 0,     -- Table Index
+         TIB    => 0,     -- Table Index
+         TIA    => 12,    -- Table Index (1st table has 4096 entries)
+         ISHIFT => 8,     -- Initial Shift 24-bit address
+         PS     => PS_4k, -- Page Size (12-bit)
+         FCL    => False, -- Function Code Lookup
+         SRE    => False, -- use CRP
+         E      => True,  -- Enable
+         others => <>
+         );
       TCR_Set (TCR);
    end Init;
 
--- platforms/Amiga-FS-UAE/platform-68040/mmu.adb
+++ platforms/Amiga-FS-UAE/platform-68040/mmu.adb
@@ -22,7 +22,8 @@ with Bits;
 with LLutils;
 with M68040;
 
-package body MMU is
+package body MMU
+   is
 
    --========================================================================--
    --                                                                        --
@@ -41,38 +42,37 @@ package body MMU is
 
    -- RI: A31 .. A25, 128 * 32 MByte blocks
 
-   type Root_Table_Type is array (Natural range <>) of RTDSC_Type with
-      Alignment => 2**8,
-      Pack      => True;
+   type Root_Table_Type is array (Natural range <>) of RTDSC_Type
+      with Alignment => 2**8,
+           Pack      => True;
 
-   Root_Table : aliased Root_Table_Type (0 .. 127) with
-      Volatile                => True,
-      Suppress_Initialization => True;
+   Root_Table : aliased Root_Table_Type (0 .. 127)
+      with Volatile                => True,
+           Suppress_Initialization => True;
 
    -- PI: A24 .. A18, 128 * 256 kByte blocks
 
 pragma Warnings (Off, "pragma Pack affects convention ""C"" components");
-   type Pointer_Table_4k_Type is array (Natural range <>) of PTDSC_Type (PAGESIZE => PAGESIZE4k) with
-      Alignment => 2**9,
-      Pack      => True;
+   type Pointer_Table_4k_Type is array (Natural range <>) of PTDSC_Type (PAGESIZE => PAGESIZE4k)
+      with Alignment => 2**9,
+           Pack      => True;
 pragma Warnings (On, "pragma Pack affects convention ""C"" components");
 
-   Pointer_Table : aliased Pointer_Table_4k_Type (0 .. 127) with
-      Volatile                => True,
-      Suppress_Initialization => True;
+   Pointer_Table : aliased Pointer_Table_4k_Type (0 .. 127)
+      with Volatile                => True,
+           Suppress_Initialization => True;
 
    -- PGI: A17 .. A12/13
 
-   type Page4k_Type is array (0 .. 63) of P4DSC_Type with
-      Alignment => 2**8,
-      Pack      => True;
+   type Page4k_Type is array (0 .. 63) of P4DSC_Type
+      with Alignment => 2**8,
+           Pack      => True;
 
    type Page4k_Ptr is access all Page4k_Type;
 
    -- Memory space
 
-   type Memory_Descriptor_Type is
-   record
+   type Memory_Descriptor_Type is record
       Base_Address   : Integer_Address;
       Descriptor_Ptr : Page4k_Ptr;
    end record;
@@ -115,23 +115,26 @@ pragma Warnings (On, "pragma Pack affects convention ""C"" components");
    ----------------------------------------------------------------------------
    -- Page_Setup
    ----------------------------------------------------------------------------
-   procedure Page_Setup (P : in Page4k_Ptr; BA : in Address) is
+   procedure Page_Setup
+      (P  : in Page4k_Ptr;
+       BA : in Address)
+      is
       Page         : P4DSC_Type;
       Base_Address : Address := BA;
    begin
       Page := (
-               PDT => PDT_RESIDENT, -- Page Descriptor Type
-               W   => False,        -- Write Protected
-               U   => False,        -- Used
-               M   => False,        -- Modified
-               CM  => CM_NC,        -- Cache Mode
-               S   => False,        -- Supervisor Protected
-               U0  => 0,            -- User Page Attributes
-               U1  => 0,            -- User Page Attributes
-               G   => True,         -- Global
-               UR0 => 0,            -- User Reserved
-               PA4 => 0             -- Physical Address
-              );
+         PDT => PDT_RESIDENT, -- Page Descriptor Type
+         W   => False,        -- Write Protected
+         U   => False,        -- Used
+         M   => False,        -- Modified
+         CM  => CM_NC,        -- Cache Mode
+         S   => False,        -- Supervisor Protected
+         U0  => 0,            -- User Page Attributes
+         U1  => 0,            -- User Page Attributes
+         G   => True,         -- Global
+         UR0 => 0,            -- User Reserved
+         PA4 => 0             -- Physical Address
+         );
       for Idx in P.all'Range loop
          Page.PA4 := Bits_20 (Select_Address_Bits (Base_Address, 12, 31));
          P.all (Idx) := Page;
@@ -142,44 +145,44 @@ pragma Warnings (On, "pragma Pack affects convention ""C"" components");
    ----------------------------------------------------------------------------
    -- Init
    ----------------------------------------------------------------------------
-   procedure Init is
+   procedure Init
+      is
       Page_Address : Integer_Address;
    begin
       -- Root table cleanup
       for Idx in Root_Table'Range loop
          Root_Table (Idx) := (
-                              UDT    => UDT_INVALID, -- Upper Level Descriptor Type
-                              W      => False,       -- Write Protected
-                              U      => False,       -- Used
-                              PTA    => 0,           -- Pointer Table Address
-                              others => <>
-                             );
+            UDT    => UDT_INVALID, -- Upper Level Descriptor Type
+            W      => False,       -- Write Protected
+            U      => False,       -- Used
+            PTA    => 0,           -- Pointer Table Address
+            others => <>
+            );
       end loop;
       -- Pointer table cleanup
       for Idx in Pointer_Table'Range loop
          Pointer_Table (Idx) := (
-                                 PAGESIZE => PAGESIZE4k,
-                                 UDT      => UDT_INVALID,
-                                 W        => False,
-                                 U        => False,
-                                 PTA4     => 0,
-                                 others   => <>
-                                );
+            PAGESIZE => PAGESIZE4k,
+            UDT      => UDT_INVALID,
+            W        => False,
+            U        => False,
+            PTA4     => 0,
+            others   => <>
+            );
       end loop;
       -- 1st 32-MB block, 128 * 256-kB blocks, 0x00000000..0x02000000
       Root_Table (0) := (
-                         UDT    => UDT_RESIDENT, -- Upper Level Descriptor Type
-                         W      => False,        -- Write Protected
-                         U      => False,        -- Used
-                         PTA    => Bits_23 (Select_Address_Bits (Pointer_Table (0)'Address, 9, 31)),
-                         others => <>
-                        );
+         UDT    => UDT_RESIDENT, -- Upper Level Descriptor Type
+         W      => False,        -- Write Protected
+         U      => False,        -- Used
+         PTA    => Bits_23 (Select_Address_Bits (Pointer_Table (0)'Address, 9, 31)),
+         others => <>
+         );
       -- 1-1 mapping
       for Idx in Memory_Space'Range loop
          Page_Address := Memory_Space (Idx).Base_Address;
          Page_Setup (Memory_Space (Idx).Descriptor_Ptr, To_Address (Page_Address));
-         Pointer_Table (Natural (Page_Address / 2**18)) :=
-            (
+         Pointer_Table (Natural (Page_Address / 2**18)) := (
              PAGESIZE => PAGESIZE4k,
              UDT      => UDT_RESIDENT,
              W        => False,
@@ -193,10 +196,10 @@ pragma Warnings (On, "pragma Pack affects convention ""C"" components");
       SRP_Set (Root_Table (0)'Address);
       PFLUSHA;
       TCR_Set ((
-                P      => PAGESIZE4k,
-                E      => True,
-                others => <>
-               ));
+         P      => PAGESIZE4k,
+         E      => True,
+         others => <>
+         ));
    end Init;
 
 end MMU;
