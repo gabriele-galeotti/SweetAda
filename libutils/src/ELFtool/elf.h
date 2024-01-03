ffbafee272b2d228666e406ceec6c657fda0899e
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-01-03 15:59:19 +0100

    libutils/src/ELFtool/elf.h: cosmetics

--- libutils/src/ELFtool/elf.h
+++ libutils/src/ELFtool/elf.h
@@ -13,23 +13,22 @@
 
 #include <stdint.h>
 
-typedef uint16_t Elf32_Half;
-typedef uint16_t Elf64_Half;
-typedef uint32_t Elf32_Word;
-typedef int32_t  Elf32_Sword;
-typedef uint32_t Elf64_Word;
-typedef int32_t  Elf64_Sword;
-typedef uint64_t Elf32_Xword;
-typedef int64_t  Elf32_Sxword;
-typedef uint64_t Elf64_Xword;
-typedef int64_t  Elf64_Sxword;
-typedef uint32_t Elf32_Address;
-typedef uint64_t Elf64_Address;
-typedef uint32_t Elf32_Off;
-typedef uint64_t Elf64_Off;
-typedef uint16_t Elf32_Section;
-typedef uint16_t Elf64_Section;
-
+typedef uint16_t   Elf32_Half;
+typedef uint16_t   Elf64_Half;
+typedef uint32_t   Elf32_Word;
+typedef int32_t    Elf32_Sword;
+typedef uint32_t   Elf64_Word;
+typedef int32_t    Elf64_Sword;
+typedef uint64_t   Elf32_Xword;
+typedef int64_t    Elf32_Sxword;
+typedef uint64_t   Elf64_Xword;
+typedef int64_t    Elf64_Sxword;
+typedef uint32_t   Elf32_Address;
+typedef uint64_t   Elf64_Address;
+typedef uint32_t   Elf32_Offset;
+typedef uint64_t   Elf64_Offset;
+typedef uint16_t   Elf32_Section;
+typedef uint16_t   Elf64_Section;
 typedef Elf32_Half Elf32_Versym;
 typedef Elf64_Half Elf64_Versym;
 
@@ -50,8 +49,8 @@ typedef struct {
         Elf32_Half    e_machine;                // architecture
         Elf32_Word    e_version;                // file version
         Elf32_Address e_entry;                  // entry point virtual address
-        Elf32_Off     e_phoff;                  // program header table file offset
-        Elf32_Off     e_shoff;                  // section header table file offset
+        Elf32_Offset  e_phoff;                  // program header table file offset
+        Elf32_Offset  e_shoff;                  // section header table file offset
         Elf32_Word    e_flags;                  // processor specific flags
         Elf32_Half    e_ehsize;                 // ELF header size in bytes
         Elf32_Half    e_phentsize;              // program header table entry size
@@ -67,8 +66,8 @@ typedef struct {
         Elf64_Half    e_machine;                // architecture
         Elf64_Word    e_version;                // file version
         Elf64_Address e_entry;                  // entry point virtual address
-        Elf64_Off     e_phoff;                  // program header table file offset
-        Elf64_Off     e_shoff;                  // section header table file offset
+        Elf64_Offset  e_phoff;                  // program header table file offset
+        Elf64_Offset  e_shoff;                  // section header table file offset
         Elf64_Word    e_flags;                  // processor-specific flags
         Elf64_Half    e_ehsize;                 // ELF header size in bytes
         Elf64_Half    e_phentsize;              // program header table entry size
@@ -83,7 +82,7 @@ typedef struct {
         Elf32_Word    sh_type;                  // type
         Elf32_Word    sh_flags;                 // flags
         Elf32_Address sh_address;               // virtual address (execution)
-        Elf32_Off     sh_offset;                // file offset
+        Elf32_Offset  sh_offset;                // file offset
         Elf32_Word    sh_size;                  // size in bytes
         Elf32_Word    sh_link;                  // link to another section
         Elf32_Word    sh_info;                  // additional section information
@@ -96,7 +95,7 @@ typedef struct {
         Elf64_Word    sh_type;                  // type
         Elf64_Xword   sh_flags;                 // flags
         Elf64_Address sh_address;               // virtual address (execution)
-        Elf64_Off     sh_offset;                // file offset
+        Elf64_Offset  sh_offset;                // file offset
         Elf64_Xword   sh_size;                  // size in bytes
         Elf64_Word    sh_link;                  // link to another section
         Elf64_Word    sh_info;                  // additional section information
