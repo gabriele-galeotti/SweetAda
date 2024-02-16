589d62cc557dd22595110d991abf4b85cb69d8e4
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-02-16 22:38:44 +0100

    FRDM-KL46Z: - more definitions (clocking and UART) - assign some aspects on register access - correct some typos

--- platforms/FRDM-KL46Z/kl46z.ads
+++ platforms/FRDM-KL46Z/kl46z.ads
@@ -44,6 +44,57 @@ package KL46Z
    PORTx_MUX_BASEADDRESS : constant := 16#4004_9000#;
    GPIO_BASEADDRESS      : constant := 16#400F_F000#;
 
+   -- 12.2.8 System Clock Gating Control Register 4 (SIM_SCGC4)
+
+   type SIM_SCGC4_Type is record
+      Reserved1 : Bits_4 := 0;
+      Reserved2 : Bits_2 := 2#11#;
+      I2C0      : Boolean;           -- I2C0 Clock Gate Control
+      I2C1      : Boolean;           -- I2C1 Clock Gate Control
+      Reserved3 : Bits_2 := 0;
+      UART0     : Boolean;           -- UART0 Clock Gate Control
+      UART1     : Boolean;           -- UART1 Clock Gate Control
+      UART2     : Boolean;           -- UART2 Clock Gate Control
+      Reserved4 : Bits_1 := 0;
+      Reserved5 : Bits_4 := 0;
+      USBOTG    : Boolean;           -- USB Clock Gate Control
+      CMP       : Boolean;           -- Comparator Clock Gate Control
+      Reserved6 : Bits_2 := 0;
+      SPI0      : Boolean;           -- SPI0 Clock Gate Control
+      SPI1      : Boolean;           -- SPI1 Clock Gate Control
+      Reserved7 : Bits_4 := 0;
+      Reserved8 : Bits_4 := 2#1111#;
+   end record
+      with Bit_Order => Low_Order_First,
+           Size      => 32;
+   for SIM_SCGC4_Type use record
+      Reserved1 at 0 range  0 ..  3;
+      Reserved2 at 0 range  4 ..  5;
+      I2C0      at 0 range  6 ..  6;
+      I2C1      at 0 range  7 ..  7;
+      Reserved3 at 0 range  8 ..  9;
+      UART0     at 0 range 10 .. 10;
+      UART1     at 0 range 11 .. 11;
+      UART2     at 0 range 12 .. 12;
+      Reserved4 at 0 range 13 .. 13;
+      Reserved5 at 0 range 14 .. 17;
+      USBOTG    at 0 range 18 .. 18;
+      CMP       at 0 range 19 .. 19;
+      Reserved6 at 0 range 20 .. 21;
+      SPI0      at 0 range 22 .. 22;
+      SPI1      at 0 range 23 .. 23;
+      Reserved7 at 0 range 24 .. 27;
+      Reserved8 at 0 range 28 .. 31;
+   end record;
+
+   SIM_SCGC4_ADDRESS : constant := SIM_BASEADDRESS + 16#1034#;
+
+   SIM_SCGC4 : aliased SIM_SCGC4_Type
+      with Address              => To_Address (SIM_SCGC4_ADDRESS),
+           Volatile_Full_Access => True,
+           Import               => True,
+           Convention           => Ada;
+
    -- 12.2.9 System Clock Gating Control Register 5
 
    type SIM_SCGC5_Type is record
@@ -84,10 +135,10 @@ package KL46Z
    SIM_SCGC5_ADDRESS : constant := SIM_BASEADDRESS + 16#1038#;
 
    SIM_SCGC5 : aliased SIM_SCGC5_Type
-      with Address    => To_Address (SIM_SCGC5_ADDRESS),
-           Volatile   => True,
-           Import     => True,
-           Convention => Ada;
+      with Address              => To_Address (SIM_SCGC5_ADDRESS),
+           Volatile_Full_Access => True,
+           Import               => True,
+           Convention           => Ada;
 
    -- Port x multiplexing control
 
@@ -129,8 +180,9 @@ package KL46Z
       ISF       : Boolean;     -- Interrupt Status Flag
       Reserved6 : Bits_7 := 0;
    end record
-      with Bit_Order => Low_Order_First,
-           Size      => 32;
+      with Bit_Order            => Low_Order_First,
+           Size                 => 32,
+           Volatile_Full_Access => True;
    for PORTx_PCRn_Type use record
       PS        at 0 range  0 ..  0;
       PE        at 0 range  1 ..  1;
@@ -148,42 +200,372 @@ package KL46Z
       Reserved6 at 0 range 25 .. 31;
    end record;
 
-   PORTA_PCR_BASEADRESS : constant := PORTx_MUX_BASEADDRESS + 16#0000#;
+   PORTA_PCR_BASEADDRESS : constant := PORTx_MUX_BASEADDRESS + 16#0000#;
 
    PORTA_PCR : aliased array (0 .. 31) of PORTx_PCRn_Type
-      with Address    => To_Address (PORTA_PCR_BASEADRESS),
+      with Address    => To_Address (PORTA_PCR_BASEADDRESS),
            Volatile   => True,
            Import     => True,
            Convention => Ada;
 
-   PORTB_PCR_BASEADRESS : constant := PORTx_MUX_BASEADDRESS + 16#1000#;
+   PORTB_PCR_BASEADDRESS : constant := PORTx_MUX_BASEADDRESS + 16#1000#;
 
    PORTB_PCR : aliased array (0 .. 31) of PORTx_PCRn_Type
-      with Address    => To_Address (PORTB_PCR_BASEADRESS),
+      with Address    => To_Address (PORTB_PCR_BASEADDRESS),
            Volatile   => True,
            Import     => True,
            Convention => Ada;
 
-   PORTC_PCR_BASEADRESS : constant := PORTx_MUX_BASEADDRESS + 16#2000#;
+   PORTC_PCR_BASEADDRESS : constant := PORTx_MUX_BASEADDRESS + 16#2000#;
 
    PORTC_PCR : aliased array (0 .. 31) of PORTx_PCRn_Type
-      with Address    => To_Address (PORTC_PCR_BASEADRESS),
+      with Address    => To_Address (PORTC_PCR_BASEADDRESS),
            Volatile   => True,
            Import     => True,
            Convention => Ada;
 
-   PORTD_PCR_BASEADRESS : constant := PORTx_MUX_BASEADDRESS + 16#3000#;
+   PORTD_PCR_BASEADDRESS : constant := PORTx_MUX_BASEADDRESS + 16#3000#;
 
    PORTD_PCR : aliased array (0 .. 31) of PORTx_PCRn_Type
-      with Address    => To_Address (PORTD_PCR_BASEADRESS),
+      with Address    => To_Address (PORTD_PCR_BASEADDRESS),
            Volatile   => True,
            Import     => True,
            Convention => Ada;
 
-   PORTE_PCR_BASEADRESS : constant := PORTx_MUX_BASEADDRESS + 16#4000#;
+   PORTE_PCR_BASEADDRESS : constant := PORTx_MUX_BASEADDRESS + 16#4000#;
 
    PORTE_PCR : aliased array (0 .. 31) of PORTx_PCRn_Type
-      with Address    => To_Address (PORTE_PCR_BASEADRESS),
+      with Address    => To_Address (PORTE_PCR_BASEADDRESS),
+           Volatile   => True,
+           Import     => True,
+           Convention => Ada;
+
+   -- 12.2.3 System Options Register 2 (SIM_SOPT2)
+
+   RTCCLKOUTSEL_RTC      : constant := 0; -- RTC 1 Hz clock is output on the RTC_CLKOUT pin.
+   RTCCLKOUTSEL_OSCERCLK : constant := 1; -- OSCERCLK clock is output on the RTC_CLKOUT pin.
+
+   CLKOUTSEL_Reserved1 : constant := 2#000#; -- Reserved
+   CLKOUTSEL_Reserved2 : constant := 2#001#; -- Reserved
+   CLKOUTSEL_BUSCLOCK  : constant := 2#010#; -- Bus clock
+   CLKOUTSEL_LPO       : constant := 2#011#; -- LPO clock (1 kHz)
+   CLKOUTSEL_MCGIRCLK  : constant := 2#100#; -- MCGIRCLK
+   CLKOUTSEL_Reserved3 : constant := 2#101#; -- Reserved
+   CLKOUTSEL_OSCERCLK  : constant := 2#110#; -- OSCERCLK
+   CLKOUTSEL_Reserved4 : constant := 2#111#; -- Reserved
+
+   PLLFLLSEL_MCGFLLCLK     : constant := 0; -- MCGFLLCLK clock
+   PLLFLLSEL_MCGFLLCLKDIV2 : constant := 1; -- MCGPLLCLK clock with fixed divide by two
+
+   USBSRC_EXT    : constant := 0; -- External bypass clock (USB_CLKIN).
+   USBSRC_MCGxLL : constant := 1; -- MCGPLLCLK/2 or MCGFLLCLK clock
+
+   TPMSRC_DISABLED  : constant := 2#00#; -- Clock disabled
+   TPMSRC_MCGxLLCLK : constant := 2#01#; -- MCGFLLCLK clock or MCGPLLCLK/2
+   TPMSRC_OSCERCLK  : constant := 2#10#; -- OSCERCLK clock
+   TPMSRC_MCGIRCLK  : constant := 2#11#; -- MCGIRCLK clock
+
+   UART0SRC_DISABLED  : constant := 2#00#; -- Clock disabled
+   UART0SRC_MCGxLLCLK : constant := 2#01#; -- MCGFLLCLK clock or MCGPLLCLK/2
+   UART0SRC_OSCERCLK  : constant := 2#10#; -- OSCERCLK clock
+   UART0SRC_MCGIRCLK  : constant := 2#11#; -- MCGIRCLK clock
+
+   type SIM_SOPT2_Type is record
+      Reserved1    : Bits_4 := 0;
+      RTCCLKOUTSEL : Bits_1;      -- RTC clock out select
+      CLKOUTSEL    : Bits_3;      -- CLKOUT select
+      Reserved2    : Bits_8 := 0;
+      PLLFLLSEL    : Bits_1;      -- PLL/FLL clock select
+      Reserved3    : Bits_1 := 0;
+      USBSRC       : Bits_1;      -- USB clock source select
+      Reserved4    : Bits_5 := 0;
+      TPMSRC       : Bits_2;      -- TPM Clock Source Select
+      UART0SRC     : Bits_2;      -- UART0 Clock Source Select
+      Reserved5    : Bits_4 := 0;
+   end record
+      with Bit_Order => Low_Order_First,
+           Size      => 32;
+   for SIM_SOPT2_Type use record
+      Reserved1    at 0 range  0 ..  3;
+      RTCCLKOUTSEL at 0 range  4 ..  4;
+      CLKOUTSEL    at 0 range  5 ..  7;
+      Reserved2    at 0 range  8 .. 15;
+      PLLFLLSEL    at 0 range 16 .. 16;
+      Reserved3    at 0 range 17 .. 17;
+      USBSRC       at 0 range 18 .. 18;
+      Reserved4    at 0 range 19 .. 23;
+      TPMSRC       at 0 range 24 .. 25;
+      UART0SRC     at 0 range 26 .. 27;
+      Reserved5    at 0 range 28 .. 31;
+   end record;
+
+   SIM_SOPT2_BASEADDRESS : constant := 16#4004_8004#;
+
+   SIM_SOPT2 : aliased SIM_SOPT2_Type
+      with Address              => To_Address (SIM_SOPT2_BASEADDRESS),
+           Volatile_Full_Access => True,
+           Import               => True,
+           Convention           => Ada;
+
+   -- 39.2.1 UART Baud Rate Register High (UARTx_BDH)
+
+   SBNS_1 : constant := 0; -- One stop bit.
+   SBNS_2 : constant := 1; -- Two stop bit.
+
+   type UARTx_BDH_Type is record
+      SBR     : Bits_5;  -- Baud Rate Modulo Divisor.
+      SBNS    : Bits_1;  -- Stop Bit Number Select
+      RXEDGIE : Boolean; -- RX Input Active Edge Interrupt Enable (for RXEDGIF)
+      LBKDIE  : Boolean; -- LIN Break Detect Interrupt Enable (for LBKDIF)
+   end record
+      with Bit_Order => Low_Order_First,
+           Size      => 8;
+   for UARTx_BDH_Type use record
+      SBR     at 0 range 0 .. 4;
+      SBNS    at 0 range 5 .. 5;
+      RXEDGIE at 0 range 6 .. 6;
+      LBKDIE  at 0 range 7 .. 7;
+   end record;
+
+   -- 39.2.3 UART Control Register 1 (UARTx_C1)
+
+   PT_EVEN : constant := 0; -- Even parity.
+   PT_ODD  : constant := 1; -- Odd parity.
+
+   ILT_START : constant := 0; -- Idle character bit count starts after start bit.
+   ILT_STOP  : constant := 1; -- Idle character bit count starts after stop bit.
+
+   WAKE_IDLELINE : constant := 0; -- Idle-line wakeup.
+   WAKE_ADDRMARK : constant := 1; -- Address-mark wakeup.
+
+   M_8 : constant := 0; -- Receiver and transmitter use 8-bit data characters.
+   M_9 : constant := 1; -- Receiver and transmitter use 9-bit data characters.
+
+   RSRC_LOOPBACK  : constant := 0; -- Provided LOOPS is set, RSRC is cleared, selects internal loop back mode and the UART ...
+   RSRC_UART1WIRE : constant := 1; -- Single-wire UART mode where the UART_TX pin is connected to the transmitter output and ...
+
+   LOOPS_NORMAL        : constant := 0; -- Normal operation - UART_RX and UART_TX use separate pins.
+   LOOPS_LOOPUART1WIRE : constant := 1; -- Loop mode or single-wire mode where transmitter outputs are internally connected ...
+
+   type UARTx_C1_Type is record
+      PT     : Bits_1;  -- Parity Type
+      PE     : Boolean; -- Parity Enable
+      ILT    : Bits_1;  -- Idle Line Type Select
+      WAKE   : Bits_1;  -- Receiver Wakeup Method Select
+      M      : Bits_1;  -- 9-Bit or 8-Bit Mode Select
+      RSRC   : Bits_1;  -- Receiver Source Select
+      DOZEEN : Boolean; -- Doze Enable
+      LOOPS  : Bits_1;  -- Loop Mode Select
+   end record
+      with Bit_Order => Low_Order_First,
+           Size      => 8;
+   for UARTx_C1_Type use record
+      PT     at 0 range 0 .. 0;
+      PE     at 0 range 1 .. 1;
+      ILT    at 0 range 2 .. 2;
+      WAKE   at 0 range 3 .. 3;
+      M      at 0 range 4 .. 4;
+      RSRC   at 0 range 5 .. 5;
+      DOZEEN at 0 range 6 .. 6;
+      LOOPS  at 0 range 7 .. 7;
+   end record;
+
+   -- 39.2.4 UART Control Register 2 (UARTx_C2)
+
+   type UARTx_C2_Type is record
+      SBK  : Boolean; -- Send Break
+      RWU  : Boolean; -- Receiver Wakeup Control
+      RE   : Boolean; -- Receiver Enable
+      TE   : Boolean; -- Transmitter Enable
+      ILIE : Boolean; -- Idle Line Interrupt Enable for IDLE
+      RIE  : Boolean; -- Receiver Interrupt Enable for RDRF
+      TCIE : Boolean; -- Transmission Complete Interrupt Enable for TC
+      TIE  : Boolean; -- Transmit Interrupt Enable for TDRE
+   end record
+      with Bit_Order => Low_Order_First,
+           Size      => 8;
+   for UARTx_C2_Type use record
+      SBK  at 0 range 0 .. 0;
+      RWU  at 0 range 1 .. 1;
+      RE   at 0 range 2 .. 2;
+      TE   at 0 range 3 .. 3;
+      ILIE at 0 range 4 .. 4;
+      RIE  at 0 range 5 .. 5;
+      TCIE at 0 range 6 .. 6;
+      TIE  at 0 range 7 .. 7;
+   end record;
+
+   -- 39.2.5 UART Status Register 1 (UARTx_S1)
+
+   type UARTx_S1_Type is record
+      PF   : Boolean := False; -- Parity Error Flag
+      FE   : Boolean := False; -- Framing Error Flag
+      NF   : Boolean := False; -- Noise Flag
+      OVR  : Boolean := False; -- Receiver Overrun Flag
+      IDLE : Boolean := False; -- Idle Line Flag
+      RDRF : Boolean := False; -- Receive Data Register Full Flag
+      TC   : Boolean := True;  -- Transmission Complete Flag
+      TDRE : Boolean := True;  -- Transmit Data Register Empty Flag
+   end record
+      with Bit_Order => Low_Order_First,
+           Size      => 8;
+   for UARTx_S1_Type use record
+      PF   at 0 range 0 .. 0;
+      FE   at 0 range 1 .. 1;
+      NF   at 0 range 2 .. 2;
+      OVR  at 0 range 3 .. 3;
+      IDLE at 0 range 4 .. 4;
+      RDRF at 0 range 5 .. 5;
+      TC   at 0 range 6 .. 6;
+      TDRE at 0 range 7 .. 7;
+   end record;
+
+   -- 39.2.6 UART Status Register 2 (UARTx_S2)
+
+   type UARTx_S2_Type is record
+      RAF     : Boolean; -- Receiver Active Flag
+      LBKDE   : Boolean; -- LIN Break Detection Enable
+      BRK13   : Boolean; -- Break Character Generation Length
+      RWUID   : Boolean; -- Receive Wake Up Idle Detect
+      RXINV   : Boolean; -- Receive Data Inversion
+      MSBF    : Boolean; -- MSB First
+      RXEDGIF : Boolean; -- UART_RX Pin Active Edge Interrupt Flag
+      LBKDIF  : Boolean; -- LIN Break Detect Interrupt Flag
+   end record
+      with Bit_Order => Low_Order_First,
+           Size      => 8;
+   for UARTx_S2_Type use record
+      RAF     at 0 range 0 .. 0;
+      LBKDE   at 0 range 1 .. 1;
+      BRK13   at 0 range 2 .. 2;
+      RWUID   at 0 range 3 .. 3;
+      RXINV   at 0 range 4 .. 4;
+      MSBF    at 0 range 5 .. 5;
+      RXEDGIF at 0 range 6 .. 6;
+      LBKDIF  at 0 range 7 .. 7;
+   end record;
+
+   -- 39.2.7 UART Control Register 3 (UARTx_C3)
+
+   type UARTx_C3_Type is record
+      PEIE  : Boolean; -- Parity Error Interrupt Enable
+      FEIE  : Boolean; -- Framing Error Interrupt Enable
+      NEIE  : Boolean; -- Noise Error Interrupt Enable
+      ORIE  : Boolean; -- Overrun Interrupt Enable
+      TXINV : Boolean; -- Transmit Data Inversion
+      TXDIR : Boolean; -- UART_TX Pin Direction in Single-Wire Mode
+      R9T8  : Boolean; -- Receive Bit 9 / Transmit Bit 8
+      R8T9  : Boolean; -- Receive Bit 8 / Transmit Bit 9
+   end record
+      with Bit_Order => Low_Order_First,
+           Size      => 8;
+   for UARTx_C3_Type use record
+      PEIE  at 0 range 0 .. 0;
+      FEIE  at 0 range 1 .. 1;
+      NEIE  at 0 range 2 .. 2;
+      ORIE  at 0 range 3 .. 3;
+      TXINV at 0 range 4 .. 4;
+      TXDIR at 0 range 5 .. 5;
+      R9T8  at 0 range 6 .. 6;
+      R8T9  at 0 range 7 .. 7;
+   end record;
+
+   -- 39.2.9 UART Match Address Registers 1 (UARTx_MA1)
+   -- 39.2.10 UART Match Address Registers 2 (UARTx_MA2)
+   -- 39.2.11 UART Control Register 4 (UARTx_C4)
+   -- 39.2.12 UART Control Register 5 (UARTx_C5)
+
+   -- 39.2 Register definition
+
+   type UART_0_Type is record
+        BDH : UARTx_BDH_Type with Volatile_Full_Access => True;
+        BDL : Unsigned_8     with Volatile_Full_Access => True;
+        C1  : UARTx_C1_Type  with Volatile_Full_Access => True;
+        C2  : UARTx_C2_Type  with Volatile_Full_Access => True;
+        S1  : UARTx_C1_Type  with Volatile_Full_Access => True;
+        S2  : UARTx_C2_Type  with Volatile_Full_Access => True;
+        C3  : UARTx_C3_Type  with Volatile_Full_Access => True;
+        D   : Unsigned_8     with Volatile_Full_Access => True;
+   end record
+      with Size => 8 * 8;
+   for UART_0_Type use record
+        BDH at 0 range 0 .. 7;
+        BDL at 1 range 0 .. 7;
+        C1  at 2 range 0 .. 7;
+        C2  at 3 range 0 .. 7;
+        S1  at 4 range 0 .. 7;
+        S2  at 5 range 0 .. 7;
+        C3  at 6 range 0 .. 7;
+        D   at 7 range 0 .. 7;
+   end record;
+
+   UART0_BASEADDRESS : constant := 16#4006_A000#;
+
+   UART0 : aliased UART_0_Type
+      with Address    => To_Address (UART0_BASEADDRESS),
+           Volatile   => True,
+           Import     => True,
+           Convention => Ada;
+
+   -- 40.2.9 UART Control Register 4 (UARTx_C4)
+
+   type UARTx_C4_Type is record
+      Reserved1 : Bits_3 := 0;
+      Reserved2 : Bits_1 := 0;
+      Reserved3 : Bits_1 := 0;
+      RDMAS     : Boolean;     -- Receiver Full DMA Select
+      Reserved4 : Bits_1 := 0;
+      TDMAS     : Boolean;     -- Transmitter DMA Select
+   end record
+      with Bit_Order => Low_Order_First,
+           Size      => 8;
+   for UARTx_C4_Type use record
+      Reserved1 at 0 range 0 .. 2;
+      Reserved2 at 0 range 3 .. 3;
+      Reserved3 at 0 range 4 .. 4;
+      RDMAS     at 0 range 5 .. 5;
+      Reserved4 at 0 range 6 .. 6;
+      TDMAS     at 0 range 7 .. 7;
+   end record;
+
+   -- 40.2 Register definition
+
+   type UART_12_Type is record
+        BDH : UARTx_BDH_Type with Volatile_Full_Access => True;
+        BDL : Unsigned_8     with Volatile_Full_Access => True;
+        C1  : UARTx_C1_Type  with Volatile_Full_Access => True;
+        C2  : UARTx_C2_Type  with Volatile_Full_Access => True;
+        S1  : UARTx_C1_Type  with Volatile_Full_Access => True;
+        S2  : UARTx_C2_Type  with Volatile_Full_Access => True;
+        C3  : UARTx_C3_Type  with Volatile_Full_Access => True;
+        D   : Unsigned_8     with Volatile_Full_Access => True;
+        C4  : UARTx_C4_Type  with Volatile_Full_Access => True;
+   end record
+      with Size => 9 * 8;
+   for UART_12_Type use record
+        BDH at 0 range 0 .. 7;
+        BDL at 1 range 0 .. 7;
+        C1  at 2 range 0 .. 7;
+        C2  at 3 range 0 .. 7;
+        S1  at 4 range 0 .. 7;
+        S2  at 5 range 0 .. 7;
+        C3  at 6 range 0 .. 7;
+        D   at 7 range 0 .. 7;
+        C4  at 8 range 0 .. 7;
+   end record;
+
+   UART1_BASEADDRESS : constant := 16#4006_B000#;
+
+   UART1 : aliased UART_12_Type
+      with Address    => To_Address (UART1_BASEADDRESS),
+           Volatile   => True,
+           Import     => True,
+           Convention => Ada;
+
+   UART2_BASEADDRESS : constant := 16#4006_C000#;
+
+   UART2 : aliased UART_12_Type
+      with Address    => To_Address (UART2_BASEADDRESS),
            Volatile   => True,
            Import     => True,
            Convention => Ada;
