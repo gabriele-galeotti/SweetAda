00792f19825b69e9ca52e7acf2f9b1dce94d4a96
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-02-21 22:50:05 +0100

    DE10-Lite: - use OS independent functions in programmer.py - beautify the Ada package output of the design parser

--- platforms/DE10-Lite/design_parser.py
+++ platforms/DE10-Lite/design_parser.py
@@ -55,6 +55,10 @@ def errprintf(format, *args):
 #                                                                              #
 ################################################################################
 
+if len(sys.argv) < 4:
+    errprintf('%s: *** Error: wrong number of arguments.\n', SCRIPT_FILENAME)
+    exit(1)
+
 qsys_filename = sys.argv[1]
 package_name = sys.argv[2]
 end_list = []
@@ -66,10 +70,16 @@ ads_filename = package_name.lower() + '.ads'
 fdout = open(ads_filename, 'w')
 sys.stdout = fdout
 
-print('package {}'.format(package_name))
-print('   with Pure => True')
-print('   is')
+indent = '   '
 
+print('')
+print('package {0:s}'.format(package_name))
+print(indent + 'with Pure => True')
+print(indent + 'is')
+print('')
+
+items = []
+max_name_length = 0
 tree = ET.parse(qsys_filename)
 root = tree.getroot()
 for connection in root.findall('connection'):
@@ -80,11 +90,21 @@ for connection in root.findall('connection'):
             name = parameter.get('name')
             if name == 'baseAddress':
                 address = parameter.get('value').replace('0x', '')
-                #address = address[:4] + '_' + address[4:]
-                print('   {}_ADDRESS : constant := 16#{}#;'.format(end, address))
+                address = '{0:s}_{1:s}'.format(address[:4], address[4:])
+                items.append({
+                   'name'    : end,
+                   'address' : address
+                   })
+                l = len(end)
+                if l > max_name_length:
+                    max_name_length = l
                 break
+for i in items:
+    padstring = ' ' * (max_name_length - len(i['name']))
+    print(indent + '{0:s}_ADDRESS {1:s}: constant := 16#{2:s}#;'.format(i['name'], padstring, i['address']))
 
-print('end {};'.format(package_name))
+print('')
+print('end {0:s};'.format(package_name))
 
 fdout.close()
 
--- platforms/DE10-Lite/programmer.py
+++ platforms/DE10-Lite/programmer.py
@@ -74,7 +74,7 @@ if platform != 'unix':
 
 if len(sys.argv) > 1:
     if sys.argv[1] == '-jtagd':
-        jtagd_command = [os.path.join(QUARTUS_PATH, 'quartus/bin/jtagd')]
+        jtagd_command = [os.path.join(QUARTUS_PATH, 'quartus', 'bin', 'jtagd')]
         result = subprocess.run(jtagd_command)
         exit(result.returncode)
     else:
@@ -84,11 +84,11 @@ if len(sys.argv) > 1:
 printf('Running nios2-configure-sof ...\n')
 configure_sof_command = [
     'sh', '-c',
-    'cd ' + QUARTUS_PATH + '/nios2eds && \
-     ./nios2_command_shell.sh            \
-     nios2-configure-sof                 \
-       --cable "' + CABLE_NAME + '"      \
-       --device ' + DEVICE_NO + '        \
+    'cd ' + os.path.join(QUARTUS_PATH, 'nios2eds') + ' && \
+     ./nios2_command_shell.sh                             \
+     nios2-configure-sof                                  \
+       --cable "' + CABLE_NAME + '"                       \
+       --device ' + DEVICE_NO + '                         \
     ' + SOF_FILE
     ]
 result = subprocess.run(configure_sof_command)
@@ -100,15 +100,15 @@ if result.returncode != 0:
 printf('Running nios2-download ...\n')
 download_command = [
     'sh', '-c',
-    'cd ' + QUARTUS_PATH + '/nios2eds &&           \
-     PATH=' + TOOLCHAIN_PROGRAM_PREFIX + ':${PATH} \
-     ./nios2_command_shell.sh                      \
-     nios2-download                                \
-       --cable "' + CABLE_NAME + '"                \
-       --device ' + DEVICE_NO + '                  \
-       --jdi ' + JDI_FILE + '                      \
-       --reset-target                              \
-       --go                                        \
+    'cd ' + os.path.join(QUARTUS_PATH, 'nios2eds') + ' && \
+     PATH=' + TOOLCHAIN_PROGRAM_PREFIX + ':${PATH}        \
+     ./nios2_command_shell.sh                             \
+     nios2-download                                       \
+       --cable "' + CABLE_NAME + '"                       \
+       --device ' + DEVICE_NO + '                         \
+       --jdi ' + JDI_FILE + '                             \
+       --reset-target                                     \
+       --go                                               \
     ' + ELF_FILE
     ]
 result = subprocess.run(download_command)
