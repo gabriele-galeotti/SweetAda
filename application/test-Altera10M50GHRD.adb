cc67ff49aab4eddab9ac0254013d0f19dac016e9
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-02-26 21:01:17 +0100

    Altera10M50GHRD: remove all IOEMU stuff + use new style

--- application/test-Altera10M50GHRD.adb
+++ application/test-Altera10M50GHRD.adb
@@ -1,12 +1,11 @@
 
 with Interfaces;
-with Configure;
 with GHRD;
 with BSP;
-with IOEMU;
 with Console;
 
-package body Application is
+package body Application
+   is
 
    --========================================================================--
    --                                                                        --
@@ -18,7 +17,10 @@ package body Application is
 
    use Interfaces;
 
-   function Tick_Count_Expired (Flash_Count : Unsigned_32; Timeout : Unsigned_32) return Boolean;
+   function Tick_Count_Expired
+      (Flash_Count : Unsigned_32;
+       Timeout     : Unsigned_32)
+      return Boolean;
 
    --========================================================================--
    --                                                                        --
@@ -31,7 +33,11 @@ package body Application is
    ----------------------------------------------------------------------------
    -- Tick_Count_Expired
    ----------------------------------------------------------------------------
-   function Tick_Count_Expired (Flash_Count : Unsigned_32; Timeout : Unsigned_32) return Boolean is
+   function Tick_Count_Expired
+      (Flash_Count : Unsigned_32;
+       Timeout     : Unsigned_32)
+      return Boolean
+      is
    begin
       return (BSP.Tick_Count - Flash_Count) > Timeout;
    end Tick_Count_Expired;
@@ -39,30 +45,18 @@ package body Application is
    ----------------------------------------------------------------------------
    -- Run
    ----------------------------------------------------------------------------
-   procedure Run is
+   procedure Run
+      is
    begin
       -------------------------------------------------------------------------
       if True then
          declare
             TC1 : Unsigned_32;
-            TC2 : Unsigned_32;
          begin
             TC1 := BSP.Tick_Count;
-            TC2 := BSP.Tick_Count;
-            if Configure.USE_QEMU_IOEMU then
-               IOEMU.IO1 := 0;
-               IOEMU.IO2 := 0;
-            end if;
             loop
-               if Tick_Count_Expired (TC1, 2_000) then
+               if Tick_Count_Expired (TC1, 3_000) then
                   TC1 := BSP.Tick_Count;
-                  if Configure.USE_QEMU_IOEMU then
-                     IOEMU.IO1 := @ + 1;
-                     IOEMU.IO2 := @ + 1;
-                  end if;
-               end if;
-               if Tick_Count_Expired (TC2, 3_000) then
-                  TC2 := BSP.Tick_Count;
                   Console.Print ("hello, SweetAda", NL => True);
                end if;
             end loop;
