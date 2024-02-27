4f0be0c2540d646a49a432a93c1bfc288ac822c0
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-02-27 21:25:05 +0100

    Quadra800: remove all IOEMU stuff + use new style

--- application/test-Quadra800.adb
+++ application/test-Quadra800.adb
@@ -1,13 +1,11 @@
 
 with System.Storage_Elements;
 with Interfaces;
-with Configure;
-with BSP;
 with CPU;
-with IOEMU;
 with Console;
 
-package body Application is
+package body Application
+   is
 
    --========================================================================--
    --                                                                        --
@@ -19,7 +17,6 @@ package body Application is
 
    use System.Storage_Elements;
    use Interfaces;
-   use BSP;
 
    --========================================================================--
    --                                                                        --
@@ -39,13 +36,7 @@ package body Application is
          declare
             Delay_Count : constant := 100_000_000;
          begin
-            if Configure.USE_QEMU_IOEMU then
-               IOEMU.IO1 := 0;
-            end if;
             loop
-               if Configure.USE_QEMU_IOEMU then
-                  IOEMU.IO1 := @ + 1;
-               end if;
                Console.Print (Character'('.'));
                for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
             end loop;
