53aa5b102931269ad58c57c7ef6d852728c0d0c1
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-02-28 21:08:23 +0100

    SPARC: add missing MMIO overriding subprogram wrappers

---
+++ cpus/SPARC/mmio/mmio-write_u32.adb
@@ -0,0 +1,27 @@
+-----------------------------------------------------------------------------------------------------------------------
+--                                                     SweetAda                                                      --
+-----------------------------------------------------------------------------------------------------------------------
+-- __HDS__                                                                                                           --
+-- __FLN__ mmio-write_u32.adb                                                                                        --
+-- __DSC__                                                                                                           --
+-- __HSH__ e69de29bb2d1d6434b8b29ae775ad8c2e48c5391                                                                  --
+-- __HDE__                                                                                                           --
+-----------------------------------------------------------------------------------------------------------------------
+-- Copyright (C) 2020-2024 Gabriele Galeotti                                                                         --
+--                                                                                                                   --
+-- SweetAda web page: http://sweetada.org                                                                            --
+-- contact address: gabriele.galeotti@sweetada.org                                                                   --
+-- This work is licensed under the terms of the MIT License.                                                         --
+-- Please consult the LICENSE.txt file located in the top-level directory.                                           --
+-----------------------------------------------------------------------------------------------------------------------
+
+with CPU.MMIO;
+
+separate (MMIO)
+procedure Write_U32
+   (Memory_Address : in System.Address;
+    Value          : in Interfaces.Unsigned_32)
+   is
+begin
+   CPU.MMIO.Write_U32 (Memory_Address, Value);
+end Write_U32;
