6afb86e5e1137bd910edf465bcbd1b25a3bafd60
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-03-01 00:30:52 +0100

    MIPS/R3000: provide byte swap functions
    R3000 has ISA level = 1, so GCC doesn't provide "builtin" swap functions.
    We provide those functions by means of the internal CPU-agnostic LibGCC.
    
    Otherwise, you could set USE_LIBGCC := Y in the platform's configuration.in.

---
+++ cpus/MIPS/R3000/libgcc.ads
@@ -0,0 +1,46 @@
+-----------------------------------------------------------------------------------------------------------------------
+--                                                     SweetAda                                                      --
+-----------------------------------------------------------------------------------------------------------------------
+-- __HDS__                                                                                                           --
+-- __FLN__ libgcc.ads                                                                                                --
+-- __DSC__                                                                                                           --
+-- __HSH__ e69de29bb2d1d6434b8b29ae775ad8c2e48c5391                                                                  --
+-- __HDE__                                                                                                           --
+-----------------------------------------------------------------------------------------------------------------------
+-- Copyright (C) 2020-2024 Gabriele Galeotti                                                                         --
+--                                                                                                                   --
+-- SweetAda web page: http://sweetada.org                                                                            --
+-- contact address: gabriele.galeotti@sweetada.org                                                                   --
+-- This work is licensed under the terms of the MIT License.                                                         --
+-- Please consult the LICENSE.txt file located in the top-level directory.                                           --
+-----------------------------------------------------------------------------------------------------------------------
+
+with GCC_Types;
+
+package LibGCC
+   with Pure => True
+   is
+
+   --========================================================================--
+   --                                                                        --
+   --                                                                        --
+   --                               Public part                              --
+   --                                                                        --
+   --                                                                        --
+   --========================================================================--
+
+   function BswapSI2
+      (V : GCC_Types.USI_Type)
+      return GCC_Types.USI_Type
+      with Export        => True,
+           Convention    => C,
+           External_Name => "__bswapsi2";
+
+   function BswapDI2
+      (V : GCC_Types.UDI_Type)
+      return GCC_Types.UDI_Type
+      with Export        => True,
+           Convention    => C,
+           External_Name => "__bswapdi2";
+
+end LibGCC;
