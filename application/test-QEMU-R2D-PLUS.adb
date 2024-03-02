b09ca00acd50ece4546140f09ae91258c42c9752
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-03-02 18:46:02 +0100

    QEMU-R2D-PLUS: remove all IOEMU stuff

--- application/test-QEMU-R2D-PLUS.adb
+++ application/test-QEMU-R2D-PLUS.adb
@@ -1,9 +1,7 @@
 
 with System.Storage_Elements;
 with Interfaces;
-with Configure;
 with CPU;
-with IOEMU;
 with Console;
 
 package body Application
@@ -38,14 +36,8 @@ package body Application
       if True then
          declare
             Delay_Count : constant := 100_000_000;
-            Value       : Unsigned_8;
          begin
-            Value := 0;
             loop
-               if Configure.USE_QEMU_IOEMU then
-                  IOEMU.IO1 := Value;
-               end if;
-               Value := @ + 1;
                for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
                Console.Print (Character'('.'));
             end loop;
