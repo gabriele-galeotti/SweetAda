6997c52e03aad8a5c84a891162365fd56822004c
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-03-12 21:55:43 +0100

    core/secondary_stack.ad[b|s]: create a proxy unit
    We will slowly convert the secstack code for platforms that use this
    features, avoiding same things in multiple places.

---
+++ core/secondary_stack.adb
@@ -0,0 +1,67 @@
+-----------------------------------------------------------------------------------------------------------------------
+--                                                     SweetAda                                                      --
+-----------------------------------------------------------------------------------------------------------------------
+-- __HDS__                                                                                                           --
+-- __FLN__ secondary_stack.adb                                                                                       --
+-- __DSC__                                                                                                           --
+-- __HSH__ e69de29bb2d1d6434b8b29ae775ad8c2e48c5391                                                                  --
+-- __HDE__                                                                                                           --
+-----------------------------------------------------------------------------------------------------------------------
+-- Copyright (C) 2020-2024 Gabriele Galeotti                                                                         --
+--                                                                                                                   --
+-- SweetAda web page: http://sweetada.org                                                                            --
+-- contact address: gabriele.galeotti@sweetada.org                                                                   --
+-- This work is licensed under the terms of the MIT License.                                                         --
+-- Please consult the LICENSE.txt file located in the top-level directory.                                           --
+-----------------------------------------------------------------------------------------------------------------------
+
+with System.Parameters;
+with System.Secondary_Stack;
+
+package body Secondary_Stack
+   is
+
+   --========================================================================--
+   --                                                                        --
+   --                                                                        --
+   --                           Local declarations                           --
+   --                                                                        --
+   --                                                                        --
+   --========================================================================--
+
+   SS_Stack : System.Secondary_Stack.SS_Stack_Ptr;
+
+   function Get_Sec_Stack
+      return System.Secondary_Stack.SS_Stack_Ptr
+      with Export        => True,
+           Convention    => C,
+           External_Name => "__gnat_get_secondary_stack";
+
+   --========================================================================--
+   --                                                                        --
+   --                                                                        --
+   --                           Package subprograms                          --
+   --                                                                        --
+   --                                                                        --
+   --========================================================================--
+
+   ----------------------------------------------------------------------------
+   -- Get_Sec_Stack
+   ----------------------------------------------------------------------------
+   function Get_Sec_Stack
+      return System.Secondary_Stack.SS_Stack_Ptr
+      is
+   begin
+      return SS_Stack;
+   end Get_Sec_Stack;
+
+   ----------------------------------------------------------------------------
+   -- Init
+   ----------------------------------------------------------------------------
+   procedure Init
+      is
+   begin
+      System.Secondary_Stack.SS_Init (SS_Stack, System.Parameters.Unspecified_Size);
+   end Init;
+
+end Secondary_Stack;
