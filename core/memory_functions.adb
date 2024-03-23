1ab875725f0908bf5f197839e6df9452965b36ff
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-03-23 03:57:28 +0100

    core/memory_functions: pragma Restrictions (No_Elaboration_Code)

--- core/memory_functions.adb
+++ core/memory_functions.adb
@@ -15,6 +15,8 @@
 -- Please consult the LICENSE.txt file located in the top-level directory.                                           --
 -----------------------------------------------------------------------------------------------------------------------
 
+pragma Restrictions (No_Elaboration_Code);
+
 with Ada.Unchecked_Conversion;
 
 package body Memory_Functions
