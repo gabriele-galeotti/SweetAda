17d27b8ea8953f8e72fde2b5c9256b12a596aba3
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-05-10 21:32:51 +0200

    RISC-V/riscv.h: define the log2(MXLEN bytesize)

--- cpus/RISC-V/riscv.h
+++ cpus/RISC-V/riscv.h
@@ -12,11 +12,13 @@
 #define _RISCV_H 1
 
 #if   __riscv_xlen == 32
-# define SAVEREG sw
-# define LOADREG lw
+# define SAVEREG    sw
+# define LOADREG    lw
+# define XLEN_ORDER 2
 #elif __riscv_xlen == 64
-# define SAVEREG sd
-# define LOADREG ld
+# define SAVEREG    sd
+# define LOADREG    ld
+# define XLEN_ORDER 3
 #else
 # error "__riscv_xlen: value not recognized"
 #endif
