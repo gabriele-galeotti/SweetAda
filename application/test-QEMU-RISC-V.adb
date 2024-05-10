0268d66ab713418fe14f928b5a0d1d3dbc600b63
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-05-10 21:43:47 +0200

    QEMU-RISC-V:
    We can start playing dangerous games with multicore SMP.
    
    The AP (application processor) harts will be triggered by the main hart.
    They get a stack and a start procedure from the main hart, printing their
    respective hart id on the console.
    
    There are no locking and/or semaphores in SweetAda so far for the RISC-V,
    but for now we want only introduce some primitive form of multithreading.
    
    For those who feel skeptical about this approach, the mechanism is used
    also in the Linux kernel.

--- application/test-QEMU-RISC-V.adb
+++ application/test-QEMU-RISC-V.adb
@@ -1,8 +1,11 @@
 
+with System;
 with System.Storage_Elements;
+with Ada.Unchecked_Conversion;
 with Interfaces;
 with Bits;
 with CPU;
+with RISCV;
 with Virt;
 with Console;
 with BSP;
@@ -20,8 +23,30 @@ package body Application
    --                                                                        --
    --========================================================================--
 
+   use System;
    use System.Storage_Elements;
    use Interfaces;
+   use RISCV;
+
+   -- "application" harts
+
+   type AP_x_Type is array (0 .. 3) of MXLEN_Type
+      with Pack       => True,
+           Convention => Asm;
+
+   AP_sp : aliased AP_x_Type := [0, 0, 0, 0]
+      with Export        => True,
+           External_Name => "ap_sp";
+   AP_pc : aliased AP_x_Type := [0, 0, 0, 0]
+      with Export        => True,
+           External_Name => "ap_pc";
+
+   -- AP hart stack size = 4 kB
+   SP1 : aliased array (0 .. 2**12 - 1) of MXLEN_Type;
+   SP2 : aliased array (0 .. 2**12 - 1) of MXLEN_Type;
+   SP3 : aliased array (0 .. 2**12 - 1) of MXLEN_Type;
+
+   procedure StartAP;
 
    --========================================================================--
    --                                                                        --
@@ -32,11 +57,47 @@ package body Application
    --========================================================================--
 
    ----------------------------------------------------------------------------
+   -- StartAP
+   ----------------------------------------------------------------------------
+   procedure StartAP
+      is
+      Delay_Count : constant := 100_000_000;
+      HartID      : MXLEN_Type;
+      C           : Character;
+   begin
+      HartID := mhartid_Read;
+      case HartID is
+         when 1      => C := '1';
+         when 2      => C := '2';
+         when 3      => C := '3';
+         when others => C := 'X';
+      end case;
+      loop
+         -- need a lock on the device, just use it for a demo
+         BSP.Console_Putchar (C);
+         for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
+      end loop;
+   end StartAP;
+
+   ----------------------------------------------------------------------------
    -- Run
    ----------------------------------------------------------------------------
    procedure Run
       is
    begin
+      -- start "application" harts --------------------------------------------
+      if True then
+         declare
+            function To_MXLEN is new Ada.Unchecked_Conversion (Address, MXLEN_Type);
+         begin
+            AP_sp (1) := To_MXLEN (SP1 (SP1'Last)'Address) + MXLEN_Type'Size / 8;
+            AP_pc (1) := To_MXLEN (StartAP'Address);
+            AP_sp (2) := To_MXLEN (SP2 (SP2'Last)'Address) + MXLEN_Type'Size / 8;
+            AP_pc (2) := To_MXLEN (StartAP'Address);
+            AP_sp (3) := To_MXLEN (SP3 (SP3'Last)'Address) + MXLEN_Type'Size / 8;
+            AP_pc (3) := To_MXLEN (StartAP'Address);
+         end;
+      end if;
       -------------------------------------------------------------------------
       if True then
          declare
