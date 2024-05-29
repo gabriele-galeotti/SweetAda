bb7b59b08648ec060c85db542ac64609932ffd0a
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-05-29 21:08:57 +0200

    GEMI/bsp.adb: simplify expressions, cosmetics

--- platforms/GEMI/bsp.adb
+++ platforms/GEMI/bsp.adb
@@ -16,7 +16,6 @@
 -----------------------------------------------------------------------------------------------------------------------
 
 with System;
-with System.Storage_Elements;
 with Configure;
 with Definitions;
 with Bits;
@@ -37,13 +36,12 @@ package body BSP
    --========================================================================--
 
    use System;
-   use System.Storage_Elements;
    use Definitions;
    use Bits;
    use Interfaces;
 
    procedure GEMI_Last_Chance_Handler
-      (Source_Location : in System.Address;
+      (Source_Location : in Address;
        Line            : in Integer)
       with Export        => True,
            Convention    => C,
@@ -62,14 +60,15 @@ package body BSP
    -- GEMI_Last_Chance_Handler
    ----------------------------------------------------------------------------
    procedure GEMI_Last_Chance_Handler
-      (Source_Location : in System.Address;
+      (Source_Location : in Address;
        Line            : in Integer)
       is
       pragma Unreferenced (Source_Location);
       pragma Unreferenced (Line);
       Delay_Count : constant := 30_000;
-      Value       : Unsigned_8 := 16#F0#;
+      Value       : Unsigned_8;
    begin
+      Value := 16#F0#;
       loop
          GEMI.LEDPORT := Value;
          for Delay_Loop_Count in 1 .. Delay_Count loop
@@ -106,22 +105,30 @@ package body BSP
       is
    begin
       -- UART 16x50 -----------------------------------------------------------
-      UART_Descriptor.Base_Address  := To_Address (GEMI.UART_BASEADDRESS);
-      UART_Descriptor.Scale_Address := 4;
-      UART_Descriptor.Baud_Clock    := Configure.CLK_FREQUENCY;
-      UART_Descriptor.Read_8        := MMIO.Read'Access;
-      UART_Descriptor.Write_8       := MMIO.Write'Access;
-      UART_Descriptor.Data_Queue    := ([others => 0], 0, 0, 0);
+      UART_Descriptor := (
+         Uart_Model    => UART16x50.UART16450,
+         Base_Address  => System'To_Address (GEMI.UART_BASEADDRESS),
+         Scale_Address => 4,
+         Baud_Clock    => Configure.CLK_FREQUENCY,
+         Flags         => (PC_UART => False),
+         Read_8        => MMIO.Read'Access,
+         Write_8       => MMIO.Write'Access,
+         Data_Queue    => ([others => 0], 0, 0, 0)
+         );
       UART16x50.Init (UART_Descriptor);
       -- RTC uPD4991A ---------------------------------------------------------
-      RTC_Descriptor.Base_Address  := To_Address (GEMI.RTC_BASEADDRESS);
-      RTC_Descriptor.Scale_Address := 0;
-      RTC_Descriptor.Read_8        := MMIO.Read'Access;
-      RTC_Descriptor.Write_8       := MMIO.Write'Access;
+      RTC_Descriptor := (
+         Base_Address  => System'To_Address (GEMI.RTC_BASEADDRESS),
+         Scale_Address => 0,
+         Read_8        => MMIO.Read'Access,
+         Write_8       => MMIO.Write'Access
+         );
       uPD4991A.Init (RTC_Descriptor);
       -- Console --------------------------------------------------------------
-      Console.Console_Descriptor.Write := Console_Putchar'Access;
-      Console.Console_Descriptor.Read  := Console_Getchar'Access;
+      Console.Console_Descriptor := (
+         Write => Console_Putchar'Access,
+         Read  => Console_Getchar'Access
+         );
       Console.Print (ANSI_CLS & ANSI_CUPHOME & VT100_LINEWRAP);
       -------------------------------------------------------------------------
       Console.Print ("GEMI SH7032", NL => True);
