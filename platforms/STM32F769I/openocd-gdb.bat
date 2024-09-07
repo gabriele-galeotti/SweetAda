a84a9eedaa0f5c3be46afb8b0ab65469ce20c509
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-09-07 15:45:47 +0200

    STM32F769I: friendly run/debug interface plus shell files to handle debug mode

---
+++ platforms/STM32F769I/openocd-gdb.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+%GDB%                                         ^
+  -ex "target extended-remote localhost:3333" ^
+  -ex "set language asm"                      ^
+  -ex "set $pc=_start"                        ^
+  %KERNEL_OUTFILE%
+
+EXIT /B %ERRORLEVEL%
+
