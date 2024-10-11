3315755f378a9cfda4c46061de90e123b9d2cbfb
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-10-11 22:18:36 +0200

    PK-S5D9/application.adb: refactoring

--- application/test-PK-S5D9.adb
+++ application/test-PK-S5D9.adb
@@ -4,8 +4,10 @@ with Bits;
 with CPU;
 with S5D9;
 with LCD;
+with BSP;
 
-package body Application is
+package body Application
+   is
 
    --========================================================================--
    --                                                                        --
@@ -29,33 +31,26 @@ package body Application is
    ----------------------------------------------------------------------------
    -- Run
    ----------------------------------------------------------------------------
-   procedure Run is
+   procedure Run
+      is
    begin
-      PFSR (P600).PMR := False;
-      PORT (6).PDR.PDR00 := True;
-      PORT (6).PODR.PODR00 := True; -- LED1 (green) off
-      PFSR (P601).PMR := False;
-      PORT (6).PDR.PDR01 := True;
-      PORT (6).PODR.PODR01 := True; -- LED2 (red) off
-      PFSR (P602).PMR := False;
-      PORT (6).PDR.PDR02 := True;
-      PORT (6).PODR.PODR02 := True; -- LED3 (yellow) off
-      -- blink LED3 -----------------------------------------------------------
       declare
          Delay_Count : constant := 5_000_000;
          C           : Unsigned_8;
       begin
          C := 16#20#;
          while True loop
+            -- blink LED3 (yellow)
             PORT (6).PODR.PODR02 := False;
             for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
             PORT (6).PODR.PODR02 := True;
             for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
-            SCI (3).TDR := C;
+            BSP.Console_Putchar (Bits.To_Ch (C));
             C := @ + 1;
             if C > 16#7F# then
                C := 16#20#;
             end if;
+            for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
          end loop;
       end;
       -------------------------------------------------------------------------
