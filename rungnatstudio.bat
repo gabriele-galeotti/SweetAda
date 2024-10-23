f026a3d7450c897b2eabb844bba4d61080be9d9a
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-10-23 21:34:35 +0200

    rungnatstudio.[bat|sh]: two handy little scripts to run GNAT Studio from command line

---
+++ rungnatstudio.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+REM be sure to have a Make available online
+SET MAKE=make.exe
+REM SET MAKE=mingw32-make.exe
+
+REM GNAT Studio executable
+SET "GNATSTUDIO=C:\GNATSTUDIO\bin\gnatstudio.exe"
+
+REM detect toolchain from configuration.in
+FOR /F "delims=" %%A IN ('%MAKE% PROBEVARIABLE^=TOOLCHAIN_PREFIX probevariable') DO SET TOOLCHAIN_PREFIX=%%A
+IF "%TOOLCHAIN_PREFIX%"=="" ECHO *** Warning: no TOOLCHAIN_PREFIX detected.
+
+REM avoid complaints about gnatls
+SET "PATH=%TOOLCHAIN_PREFIX%\bin;%PATH%
+
+START "" "%GNATSTUDIO%" sweetada.gpr
+
+EXIT /B %ERRORLEVEL%
+
---
+++ rungnatstudio.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# GNAT Studio executable
+GNATSTUDIO=/opt/GNAT/2021/bin/gnatstudio
+
+# detect toolchain from configuration.in
+TOOLCHAIN_PREFIX=$(make PROBEVARIABLE=TOOLCHAIN_PREFIX probevariable)
+if [ "x${TOOLCHAIN_PREFIX}" = "x" ] ; then
+  printf "%s\n" "*** Warning: no TOOLCHAIN_PREFIX detected."
+fi
+
+# avoid complaints about gnatls
+export PATH=${TOOLCHAIN_PREFIX}\bin:${PATH}
+
+"${GNATSTUDIO}" sweetada.gpr &
+
+exit 0
+
