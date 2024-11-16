cb374d1b473f36004323faf9000d15068bcb4323
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-11-16 12:40:54 +0100

    RaspberryPi3/createbootdevice.sh:
    We make the createbootdevice.sh script independent from the context, runnable
    out of the SweetAda build system.
    
    The script will write an executable (with a JTAG-enable config.txt)
    on a SD-card device, either specifiyng its UUID or /dev/???.
    
    It is useful to write the share/rpi3_bootloop.bin stub, which keeps the
    CPU hang in a loop. This way you can then download the SweetAda kernel with
    normal "make run" or "make debug" operations (after a "make session-start"
    to activate the OpenOCD server, of course).

--- platforms/RaspberryPi3/createbootdevice.sh
+++ platforms/RaspberryPi3/createbootdevice.sh
@@ -11,13 +11,14 @@
 
 #
 # Arguments:
-# none
+# -h            help
+# -d DEVICE     block device name (without -u option)
+# -u UUID       block device UUID (without -d option)
+# -m MOUNTPOINT filesystem mountpoint
+# <filename>    executable filename
 #
 # Environment variables:
-# SWEETADA_PATH
-# KERNEL_ROMFILE
-# USDCARD_UUID
-# USDCARD_MOUNTPOINT
+# none
 #
 
 ################################################################################
@@ -28,33 +29,136 @@
 SCRIPT_FILENAME=$(basename "$0")
 
 ################################################################################
+# usage()                                                                      #
+#                                                                              #
+################################################################################
+usage()
+{
+printf "%s\n" "Usage:"
+printf "%s\n" "createbootdevice [-h] -d DEVICE|-u UUID -m MOUNTPOINT <filename>"
+printf "%s\n" "-h              help"
+printf "%s\n" "-d DEVICE       block device to be used (e.g., /dev/sdcard1)"
+printf "%s\n" "-u UUID         use the device UUID as shown by the \"blkid\" utility (e.g., 3FFC-F14C)"
+printf "%s\n" "-m MOUNTPOINT   MOUNTPOINT is the filesystem location where the device will be mounted"
+printf "%s\n" "filename        executable filename to be stored in the device"
+printf "%s\n" ""
+return 0
+}
+
+################################################################################
 # Main loop.                                                                   #
 #                                                                              #
 ################################################################################
 
-# install SweetAda on FAT32 1st partition of a micro-SD memory card
+DEVICE=
+UUID=
+MOUNTPOINT=
+FILENAME=
 
-USDCARD_DEVICE=$(blkid | grep -e "UUID=\"${USDCARD_UUID}\"" | sed -e "s|^\(/dev/.*\)\(:.*\)\$|\1|")
-if [ "x${USDCARD_DEVICE}" = "x" ] ; then
-  echo "*** Error: UUID=${USDCARD_UUID}: no device found."
+#
+# Parse command line arguments.
+#
+token_seen=N
+while [ $# -gt 0 ] ; do
+  if [ "x${1:0:1}" = "x-" ] ; then
+    # "-" option
+    argument=${1:1}
+    case ${argument} in
+      "h")
+        usage
+        exit $?
+        ;;
+      "d")
+        if [ "x${UUID}" != "x" ] ; then
+          printf "%s\n" echo "*** Error: only one of -d or -u option must be specified." 1>&2
+          usage
+          exit 1
+        fi
+        shift
+        DEVICE="$1"
+        ;;
+      "u")
+        if [ "x${DEVICE}" != "x" ] ; then
+          printf "%s\n" echo "*** Error: only one of -d or -u option must be specified." 1>&2
+          usage
+          exit 1
+        fi
+        shift
+        UUID="$1"
+        ;;
+      "m")
+        shift
+        MOUNTPOINT="$1"
+        ;;
+      *)
+        printf "%s\n" "*** Error: unknown option \"${argument}\"." 1>&2
+        usage
+        exit 1
+        ;;
+    esac
+  else
+    # no "-" option
+    if [ "x${token_seen}" = "xN" ] ; then
+      token_seen=Y
+      FILENAME="$1"
+    else
+      printf "%s\n" echo "*** Error: multiple files specified." 1>&2
+      usage
+      exit 1
+    fi
+  fi
+  shift
+done
+
+#UUID=3FFC-F14C
+#MOUNTPOINT=/mnt/hd
+
+if [ "x${DEVICE}" = "x" ] && [ "x${UUID}" = "x" ] ; then
+  printf "%s\n" "*** Error: no -d nor -u option specified." 1>&2
+  usage
+  exit 1
+fi
+if [ "x${FILENAME}" = "x" ] ; then
+  printf "%s\n" "*** Error: no executable filename." 1>&2
+  usage
   exit 1
 fi
 
-mount ${USDCARD_DEVICE} ${USDCARD_MOUNTPOINT}
+# install executable on FAT32 1st partition
+if [ "x${UUID}" != "x" ] ; then
+  DEVICE=$(blkid                                | \
+           grep -e "UUID=\"${UUID}\""           | \
+           sed -e "s|^\(/dev/.*\)\(:.*\)\$|\1|"   \
+          )
+  if [ "x${DEVICE}" = "x" ] ; then
+    printf "%s\n" "*** Error: UUID=${UUID}: no device found." 1>&2
+    exit 1
+  fi
+fi
 
-cat > ${USDCARD_MOUNTPOINT}/config.txt << EOF || exit 1
+mount ${DEVICE} ${MOUNTPOINT} || exit 1
+
+cat > ${MOUNTPOINT}/config.txt << EOF
 arm_64bit=1                     # ARMv8 mode
 core_freq=250                   # core clock frequency
 arm_freq=250                    # ARM clock frequency
-kernel=${KERNEL_ROMFILE}
+kernel=$(basename ${FILENAME})
 enable_jtag_gpio=1              # enable JTAG (GPIO22..27)
 EOF
+if [ $? -ne 0 ] ; then
+  umount ${MOUNTPOINT}
+  exit 1
+fi
 
-cp -f -v ${SWEETADA_PATH}/${KERNEL_ROMFILE} ${USDCARD_MOUNTPOINT}/ || exit 1
+cp -f -v ${FILENAME} ${MOUNTPOINT}/
+if [ $? -ne 0 ] ; then
+  umount ${MOUNTPOINT}
+  exit 1
+fi
 
 sync ; sync
 
-umount ${USDCARD_MOUNTPOINT}
+umount ${MOUNTPOINT} || exit 1
 
 exit 0
 
