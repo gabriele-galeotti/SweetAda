31909c0cfd6ab1b215341393389ca4b6a96cd67c
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-11-23 00:10:05 +0100

    NEORV32/platform-GHDL: use terminal handling + cosmetics

--- platforms/NEORV32/platform-GHDL/neorv32.sh
+++ platforms/NEORV32/platform-GHDL/neorv32.sh
@@ -34,6 +34,9 @@ SCRIPT_FILENAME=$(basename "$0")
 #                                                                              #
 ################################################################################
 
+# load terminal handling
+. ${SHARE_DIRECTORY}/terminal.sh
+
 # paths and filenames extracted from "<NEORV32_HOME>/sw/common/common.mk"
 NEORV32_RTL_PATH="${NEORV32_HOME}"/rtl/core
 NEORV32_SIM_PATH="${NEORV32_HOME}"/sim
@@ -41,17 +44,16 @@ IMAGE_GEN="${NEORV32_HOME}"/sw/image_gen/image_gen
 APP_IMG=neorv32_application_image.vhd
 
 # generate a pure binary image out of .text/.rodata/.data sections
-${OBJCOPY}                                     \
-  -j .text -j .rodata -j .data                 \
-  -I elf32-little ${KERNEL_OUTFILE}            \
+${OBJCOPY} \
+  -j .text -j .rodata -j .data \
+  -I elf32-little ${KERNEL_OUTFILE} \
   -O binary ${PLATFORM_DIRECTORY}/sweetada.bin
 # elaborate a SweetAda VHDL source
 cd ${PLATFORM_DIRECTORY}
 "${IMAGE_GEN}" -app_img sweetada.bin "${NEORV32_RTL_PATH}"/${APP_IMG}
 # run the simulation
-PATH=${GHDL_PATH}/bin:${PATH} xterm                \
-  -T "NEORV32" -geometry 132x50 -bg blue -fg white \
-  -e "${NEORV32_SIM_PATH}"/simple/ghdl.sh          \
+PATH=${GHDL_PATH}/bin:${PATH} $(terminal ${TERMINAL}) sh -c \
+  "${NEORV32_SIM_PATH}"/simple/ghdl.sh \
   &
 
 exit 0
