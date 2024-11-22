95c5c7f6b2a11913deceac9f16546b46f7e64ecd
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-11-23 00:08:07 +0100

    PC-x86/platform-Bochs/bochs.sh: revamp script

--- platforms/PC-x86/platform-Bochs-ROM/bochs.sh
+++ platforms/PC-x86/platform-Bochs-ROM/bochs.sh
@@ -3,6 +3,25 @@
 #
 # PC-x86 (Bochs emulator).
 #
+# Copyright (C) 2020-2024 Gabriele Galeotti
+#
+# This work is licensed under the terms of the MIT License.
+# Please consult the LICENSE.txt file located in the top-level directory.
+#
+
+#
+# Arguments:
+# -debug
+#
+# Environment variables:
+# OSTYPE
+# PLATFORM_DIRECTORY
+# SHARE_DIRECTORY
+# KERNEL_OUTFILE
+# KERNEL_ROMFILE
+# TERMINAL
+# GDB
+#
 
 ################################################################################
 # Script initialization.                                                       #
@@ -46,58 +65,76 @@ return 0
 #                                                                              #
 ################################################################################
 
+# MSYS: hand over to cmd.exe
+if [ "x${OSTYPE}" = "xmsys" ] ; then
+  exec ${PLATFORM_DIRECTORY}/qemu.bat "$@"
+  exit $?
+fi
+
+# load terminal handling
+. ${SHARE_DIRECTORY}/terminal.sh
+
+# Bochs executable and CPU model
+BOCHS_EXECUTABLE="/opt/Bochs/bin/bochs-gdb"
+
+# debug options
+if [ "x$1" = "x-debug" ] ; then
+  BOCHS_DEBUG="gdbstub:  enabled=1, port=1234, text_base=0, data_base=0, bss_base=0"
+else
+  BOCHS_DEBUG=
+fi
+
 # telnet port numbers and listening timeout in s
 SERIALPORT0=4446
 SERIALPORT1=4447
 TILTIMEOUT=3
 
-BOCHS_EXEC=/opt/Bochs/bin/bochs
-BOCHS_RC=()
-BOCHS_RC+=("log:      ./${PLATFORM_DIRECTORY}/bochs.log")
-BOCHS_RC+=("cpu:      count=1, ips=16000000")
-BOCHS_RC+=("memory:   guest=4, host=4")
-BOCHS_RC+=("romimage: file=./kernel.rom")
-BOCHS_RC+=("vga:      extension=vbe")
-BOCHS_RC+=("com1:     enabled=1, mode=socket-server, dev=localhost:${SERIALPORT0}")
-BOCHS_RC+=("com2:     enabled=1, mode=socket-server, dev=localhost:${SERIALPORT1}")
-BOCHS_RC+=("floppya:  1_44=bootfd, status=inserted")
-BOCHS_RC+=("ata0:     enabled=1, ioaddr1=0x1F0, ioaddr2=0x3F0, irq=14")
-if [ "x$1" = "x-debug" ] ; then
-  BOCHS_RC+=("gdbstub:  enabled=1, port=1234, text_base=0, data_base=0, bss_base=0")
-fi
-rm -f ./${PLATFORM_DIRECTORY}/bochsrc
-touch ./${PLATFORM_DIRECTORY}/bochsrc
-for rc in "${BOCHS_RC[@]}" ; do
-  echo "${rc}" >> ./${PLATFORM_DIRECTORY}/bochsrc
-done
-"${BOCHS_EXEC}" -f ./${PLATFORM_DIRECTORY}/bochsrc -q &
+# create bochsrc
+cat > ./${PLATFORM_DIRECTORY}/bochsrc << EOF
+log:      ./${PLATFORM_DIRECTORY}/bochs.log
+cpu:      count=1, ips=16000000
+memory:   guest=4, host=4
+romimage: file=${KERNEL_ROMFILE}
+vga:      extension=vbe
+com1:     enabled=1, mode=socket-server, dev=localhost:${SERIALPORT0}
+com2:     enabled=1, mode=socket-server, dev=localhost:${SERIALPORT1}
+floppya:  1_44=bootfd, status=inserted
+ata0:     enabled=1, ioaddr1=0x1F0, ioaddr2=0x3F0, irq=14
+${BOCHS_DEBUG}
+EOF
+
+# Bochs machine
+"${BOCHS_EXECUTABLE}" -f ./${PLATFORM_DIRECTORY}/bochsrc -q &
 BOCHS_PID=$!
 
 # console for serial port
 tcpport_is_listening ${SERIALPORT0} ${TILTIMEOUT} "*** Error"
-  setsid /usr/bin/xterm \
-  -T "BOCHS-1" -geometry 80x24 -bg blue -fg white -sl 1024 -e \
-  /bin/telnet localhost ${SERIALPORT0} \
-  &
+    $(terminal ${TERMINAL}) /bin/telnet localhost ${SERIALPORT0} &
 # console for serial port
 tcpport_is_listening ${SERIALPORT1} ${TILTIMEOUT} "*** Error"
-  setsid /usr/bin/xterm \
-  -T "BOCHS-2" -geometry 80x24 -bg blue -fg white -sl 1024 -e \
-  /bin/telnet localhost ${SERIALPORT1} \
-  &
+    $(terminal ${TERMINAL}) /bin/telnet localhost ${SERIALPORT1} &
 
-if [ "x$1" = "x" ] ; then
-  wait ${BOCHS_PID}
-elif [ "x$1" = "x-debug" ] ; then
-  GDB_EXEC=/opt/toolchains/bin/i686-elf-gdb
-  GDB_ARGS=()
-  GDB_ARGS+=("-iex" "set basenames-may-differ")
-  GDB_ARGS+=("-iex" "set architecture i386")
-  GDB_ARGS+=("-ex" "target remote tcp:localhost:1234")
-  GDB_ARGS+=("-ex" "tbreak _start" "-ex" "continue")
-  GDB_ARGS+=(${KERNEL_OUTFILE})
-  "${GDB_EXEC}" "${GDB_ARGS[@]}"
+# debug session
+if [ "x$1" = "x-debug" ] ; then
+  $(terminal ${TERMINAL}) sh -c '\
+    "'"${GDB}"'" \
+      -q \
+      -iex "set basenames-may-differ" \
+      -iex "set architecture i386" \
+      -ex "set tcp connect-timeout 30" \
+      -ex "target extended-remote tcp:localhost:1234" \
+      -ex "tbreak _start" -ex "continue" \
+      '${KERNEL_OUTFILE}' \
+    ; \
+    if [ $? -ne 0 ] ; then \
+      printf "%s" "Press any key to continue ... " ; \
+      read answer ; \
+    fi \
+    '
 fi
+
+# wait Bochs termination
+wait ${BOCHS_PID}
 
 exit $?
 
