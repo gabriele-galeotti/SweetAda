31266485ba80ca1ea7c455a6aece14d5a9659bc0
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-12-05 22:23:02 +0100

    Atlas/M5235BCC/MVME162-510A/postbuild.sh: uniform scripts

--- platforms/MVME162-510A/postbuild.sh
+++ platforms/MVME162-510A/postbuild.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
 # create an S-record kernel file for 162Bug download
+
 ${OBJCOPY}                                 \
   -O srec                                  \
   ${SWEETADA_PATH}/${KERNEL_OUTFILE}       \
   ${SWEETADA_PATH}/${KERNEL_BASENAME}.srec
+chmod a-x ${SWEETADA_PATH}/${KERNEL_BASENAME}.srec
 
 exit $?
 
