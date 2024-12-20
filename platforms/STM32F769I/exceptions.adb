2cfac29af1a011c0dd844fe9befe80677e13a0d9
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-12-20 23:53:05 +0100

    STM32F769I/exceptions.adb: print message for exception happened

--- platforms/STM32F769I/exceptions.adb
+++ platforms/STM32F769I/exceptions.adb
@@ -52,6 +52,7 @@ package body Exceptions
    procedure Exception_Process
       is
    begin
+      Console.Print (Prefix => "*** EXCEPTION", NL => True);
       Abort_Library.System_Abort;
    end Exception_Process;
 
