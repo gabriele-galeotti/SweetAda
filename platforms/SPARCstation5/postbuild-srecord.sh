37857dcfa368a91f4bb8dc31526f60ef93ed53b3
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-12-22 22:38:33 +0100

    SPARCstation5/postbuild-[network|srecord].sh: cosmetics

--- platforms/SPARCstation5/postbuild-srecord.sh
+++ platforms/SPARCstation5/postbuild-srecord.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
 
-# create an S-record
-${OBJCOPY} -O srec "${SWEETADA_PATH}"/${KERNEL_OUTFILE} "${SWEETADA_PATH}"/${KERNEL_BASENAME}.srec
-chmod a-x "${SWEETADA_PATH}"/${KERNEL_BASENAME}.srec
+# create an S-record kernel file
+
+${OBJCOPY}                                 \
+  -O srec                                  \
+  ${SWEETADA_PATH}/${KERNEL_OUTFILE}       \
+  ${SWEETADA_PATH}/${KERNEL_BASENAME}.srec
+chmod a-x ${SWEETADA_PATH}/${KERNEL_BASENAME}.srec
 
 exit 0
 
