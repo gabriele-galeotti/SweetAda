9bba925684cb62dda3b522a9fbeb74f908e9286e
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2024-12-28 03:32:38 +0100

    drivers/ne2000.ads: default values for descriptor

--- drivers/ne2000.ads
+++ drivers/ne2000.ads
@@ -45,19 +45,19 @@ package NE2000
    type Port_Write_32_Ptr is access procedure (Port : in Unsigned_16; Value : in Unsigned_32);
 
    type Descriptor_Type is record
-      NE2000PCI     : Boolean;
-      Device_Number : PCI.Device_Number_Type;
-      BAR           : Unsigned_16;
-      PCI_Irq_Line  : Unsigned_8;
-      Base_Address  : Address;
-      MAC           : Ethernet.MAC_Address_Type;
-      Read_8        : Port_Read_8_Ptr;
-      Write_8       : Port_Write_8_Ptr;
-      Read_16       : Port_Read_16_Ptr;
-      Write_16      : Port_Write_16_Ptr;
-      Read_32       : Port_Read_32_Ptr;
-      Write_32      : Port_Write_32_Ptr;
-      Next_Ptr      : Unsigned_8 with Volatile => True;
+      NE2000PCI     : Boolean                   := False;
+      Device_Number : PCI.Device_Number_Type    := 0;
+      BAR           : Unsigned_16               := 0;
+      PCI_Irq_Line  : Unsigned_8                := 0;
+      Base_Address  : Address                   := Null_Address;
+      MAC           : Ethernet.MAC_Address_Type := [others => 0];
+      Read_8        : Port_Read_8_Ptr           := null;
+      Write_8       : Port_Write_8_Ptr          := null;
+      Read_16       : Port_Read_16_Ptr          := null;
+      Write_16      : Port_Write_16_Ptr         := null;
+      Read_32       : Port_Read_32_Ptr          := null;
+      Write_32      : Port_Write_32_Ptr         := null;
+      Next_Ptr      : Unsigned_8                := 0             with Volatile => True;
    end record;
 
    DESCRIPTOR_INVALID : constant Descriptor_Type :=
@@ -67,7 +67,7 @@ package NE2000
        BAR           => 0,
        PCI_Irq_Line  => 0,
        Base_Address  => Null_Address,
-       MAC           => [0, 0, 0, 0, 0, 0],
+       MAC           => [others => 0],
        Read_8        => null,
        Write_8       => null,
        Read_16       => null,
