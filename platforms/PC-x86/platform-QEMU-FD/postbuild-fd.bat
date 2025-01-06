44aaee79d40b6c4908ed0886daec0389c1f037c8
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-01-06 17:58:49 +0100

    PC-x86/platform-QEMU-FD: lower load address

--- platforms/PC-x86/platform-QEMU-FD/postbuild-fd.bat
+++ platforms/PC-x86/platform-QEMU-FD/postbuild-fd.bat
@@ -5,12 +5,12 @@ CALL %FILEPAD% %KERNEL_PARENT_PATH%\%KERNEL_ROMFILE% 512
 "%PYTHON%"                                             ^
   "%SWEETADA_PATH%"\%SHARE_DIRECTORY%\pc-x86-bootfd.py ^
   %KERNEL_PARENT_PATH%\%KERNEL_ROMFILE%                ^
-  0x4000                                               ^
+  0x2000                                               ^
   +pcbootfd.dsk
 REM "%TCLSH%"                                               ^
 REM   "%SWEETADA_PATH%"\%SHARE_DIRECTORY%\pc-x86-bootfd.tcl ^
 REM   %KERNEL_PARENT_PATH%\%KERNEL_ROMFILE%                 ^
-REM   0x4000                                                ^
+REM   0x2000                                                ^
 REM   +pcbootfd.dsk
 
 EXIT /B %ERRORLEVEL%
--- platforms/PC-x86/platform-QEMU-FD/postbuild-fd.sh
+++ platforms/PC-x86/platform-QEMU-FD/postbuild-fd.sh
@@ -5,12 +5,12 @@ ${FILEPAD} ${KERNEL_PARENT_PATH}/${KERNEL_ROMFILE} 512 || exit $?
 "${PYTHON}"                                                 \
   ${KERNEL_PARENT_PATH}/${SHARE_DIRECTORY}/pc-x86-bootfd.py \
   ${KERNEL_PARENT_PATH}/${KERNEL_ROMFILE}                   \
-  0x4000                                                    \
+  0x2000                                                    \
   +pcbootfd.dsk
 #"${TCLSH}"                                                   \
 #  ${KERNEL_PARENT_PATH}/${SHARE_DIRECTORY}/pc-x86-bootfd.tcl \
 #  ${KERNEL_PARENT_PATH}/${KERNEL_ROMFILE}                    \
-#  0x4000                                                     \
+#  0x2000                                                     \
 #  +pcbootfd.dsk
 
 exit $?
