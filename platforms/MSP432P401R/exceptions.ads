a21e5d5669ac682c2a9ef3e8b7faf04218271e0d
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-01-07 22:17:04 +0100

    MSP432P401R/exceptions.ad[b|s]: definitions for vectors

--- platforms/MSP432P401R/exceptions.ads
+++ platforms/MSP432P401R/exceptions.ads
@@ -15,6 +15,8 @@
 -- Please consult the LICENSE.txt file located in the top-level directory.                                           --
 -----------------------------------------------------------------------------------------------------------------------
 
+with Bits;
+
 package Exceptions
    is
 
@@ -25,6 +27,25 @@ package Exceptions
    --                                                                        --
    --                                                                        --
    --========================================================================--
+
+   use Bits;
+
+   SP_Main      : aliased Asm_Entry_Point with Import => True, External_Name => "SP_Main";
+   Reset        : aliased Asm_Entry_Point with Import => True, External_Name => "Reset";
+   NMI          : aliased Asm_Entry_Point with Import => True, External_Name => "NMI";
+   HardFault    : aliased Asm_Entry_Point with Import => True, External_Name => "HardFault";
+   MemManage    : aliased Asm_Entry_Point with Import => True, External_Name => "MemManage";
+   BusFault     : aliased Asm_Entry_Point with Import => True, External_Name => "BusFault";
+   UsageFault   : aliased Asm_Entry_Point with Import => True, External_Name => "UsageFault";
+   ReservedExc7 : aliased Asm_Entry_Point with Import => True, External_Name => "ReservedExc7";
+   ReservedExc8 : aliased Asm_Entry_Point with Import => True, External_Name => "ReservedExc8";
+   ReservedExc9 : aliased Asm_Entry_Point with Import => True, External_Name => "ReservedExc9";
+   ReservedExcA : aliased Asm_Entry_Point with Import => True, External_Name => "ReservedExcA";
+   SVCall       : aliased Asm_Entry_Point with Import => True, External_Name => "SVCall";
+   DebugMonitor : aliased Asm_Entry_Point with Import => True, External_Name => "DebugMonitor";
+   ReservedExcD : aliased Asm_Entry_Point with Import => True, External_Name => "ReservedExcD";
+   PendSV       : aliased Asm_Entry_Point with Import => True, External_Name => "PendSV";
+   SysTick      : aliased Asm_Entry_Point with Import => True, External_Name => "SysTick";
 
    procedure Exception_Process
       with Export        => True,
