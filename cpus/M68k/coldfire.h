4f48bd61b1d3d7bace539eef140da85ecaf9bfa3
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-01-12 18:43:28 +0100

    cpus/M68k/coldfire.h: add RAMBAR register and SRAM default

--- cpus/M68k/coldfire.h
+++ cpus/M68k/coldfire.h
@@ -11,10 +11,12 @@
 #ifndef _COLDFIRE_H
 #define _COLDFIRE_H 1
 
-#define VBR  0x801
-#define MBAR 0xC0F
+#define VBR    0x801
+#define RAMBAR 0xC05
+#define MBAR   0xC0F
 
 #define IPSBAR_DEFAULT 0x40000000
+#define SRAM_DEFAULT   0x20000000
 
 #define SCM_CWCR (IPSBAR + 0x11)
 #define CCM_TEST (IPSBAR + 0x11000C)
