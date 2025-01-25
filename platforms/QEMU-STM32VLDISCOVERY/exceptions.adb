56bfd42df69e1f41a61ab460385777c9fe2e01d5
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-01-25 18:23:04 +0100

    QEMU-STM32VLDISCOVERY/exceptions.adb: print exception happened

--- platforms/QEMU-STM32VLDISCOVERY/exceptions.adb
+++ platforms/QEMU-STM32VLDISCOVERY/exceptions.adb
@@ -50,6 +50,7 @@ package body Exceptions
    procedure Exception_Process
       is
    begin
+      Console.Print ("*** EXCEPTION", NL => True);
       Abort_Library.System_Abort;
    end Exception_Process;
 
