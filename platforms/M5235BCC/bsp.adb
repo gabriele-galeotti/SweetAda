72ded2e27dfdcc9a9ab5dc932e920f1779e7db0f
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-02-01 16:28:12 +0100

    M5235BCC/bsp.adb: detect MII, write out SYN* clock register settings

--- platforms/M5235BCC/bsp.adb
+++ platforms/M5235BCC/bsp.adb
@@ -18,6 +18,7 @@
 with System;
 with System.Storage_Elements;
 with Definitions;
+with Configure;
 with Bits;
 with MMIO;
 with Secondary_Stack;
@@ -46,6 +47,7 @@ package body BSP
    use M5235BCC;
 
    procedure FlashROM_Detect;
+   procedure MII_Detect;
 
    --========================================================================--
    --                                                                        --
@@ -83,6 +85,34 @@ package body BSP
       -- Read/Reset
       MMIO.Write_U16 (FlashROM_Address, 16#F0#);
    end FlashROM_Detect;
+
+   ----------------------------------------------------------------------------
+   -- MII_Detect
+   ----------------------------------------------------------------------------
+   procedure MII_Detect
+      is
+      MII_FREQUENCY : constant := 2_500 * kHz1; -- MII speed must be 2.5 MHz
+      KS8721BLSL_ID : constant := 16#0022_1619#; -- Micrel KS8721BL/SL
+      MII_PHYID1    : constant := 2;
+      MII_PHYID2    : constant := 3;
+      DATA          : Unsigned_16;
+      MII_ID        : Unsigned_32;
+   begin
+      MSCR := (MII_SPEED => Bits_6 (Configure.FSYS_FREQUENCY / (2 * MII_FREQUENCY)), others => <>);
+      EIMR.MII := False;
+      MMFR := (RA => MII_PHYID1, PA => Configure.MII_ADDRESS, OP => OP_READ, others => <>);
+      loop exit when EIR.MII; end loop;
+      EIR.MII := True;
+      DATA := Unsigned_16 (MMFR.DATA);
+      MMFR := (RA => MII_PHYID2, PA => Configure.MII_ADDRESS, OP => OP_READ, others => <>);
+      loop exit when EIR.MII; end loop;
+      EIR.MII := True;
+      MII_ID := Make_Word (DATA, Unsigned_16 (MMFR.DATA));
+      Console.Print (Prefix => "MII id: ", Value => MII_ID, NL => True);
+      if MII_ID = KS8721BLSL_ID then
+         Console.Print ("Micrel KS8721BL/SL", NL => True);
+      end if;
+   end MII_Detect;
 
    ----------------------------------------------------------------------------
    -- Console wrappers
@@ -128,9 +158,12 @@ package body BSP
       Console.Print (Prefix => "PRN:    ", Value => Unsigned_32 (CIR.PRN), NL => True);
       pragma Warnings (Off, "volatile actual passed by copy");
       Console.Print (Prefix => "IPSBAR: ", Value => To_U32 (IPSBAR) and 16#FFFF_FFFE#, NL => True);
+      Console.Print (Prefix => "SYNCR:  ", Value => To_U32 (SYNCR), NL => True);
+      Console.Print (Prefix => "SYNSR:  ", Value => To_U32 (SYNSR), NL => True);
       pragma Warnings (On, "volatile actual passed by copy");
       -------------------------------------------------------------------------
       FlashROM_Detect;
+      MII_Detect;
       -------------------------------------------------------------------------
    end Setup;
 
