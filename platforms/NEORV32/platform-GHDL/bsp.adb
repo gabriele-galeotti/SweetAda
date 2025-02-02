1abed1b02b0a8eb884bde979d9bb427cc429aa5f
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-02-02 12:01:23 +0100

    NEORV32-GHDL/bsp.adb: remove unused conversion

--- platforms/NEORV32/platform-GHDL/bsp.adb
+++ platforms/NEORV32/platform-GHDL/bsp.adb
@@ -15,7 +15,6 @@
 -- Please consult the LICENSE.txt file located in the top-level directory.                                           --
 -----------------------------------------------------------------------------------------------------------------------
 
-with Ada.Unchecked_Conversion;
 with Definitions;
 with Bits;
 with RISCV;
@@ -79,7 +78,6 @@ package body BSP
    ----------------------------------------------------------------------------
    procedure Setup
       is
-      function To_U32 is new Ada.Unchecked_Conversion (UART_CTRL_Type, Unsigned_32);
    begin
       -- UART0 ----------------------------------------------------------------
       UART0.CTRL := (
