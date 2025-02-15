3ed08c25714db4a8f5ee190aabae1fb34e4f80d2
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-02-15 15:16:58 +0100

    toolchains/GDB.sh: update to gdb-16.2

--- toolchains/GDB.sh
+++ toolchains/GDB.sh
@@ -60,7 +60,7 @@ return ${exit_status}
 #                                                                              #
 ################################################################################
 
-PACKAGE_NAME=gdb-15.2
+PACKAGE_NAME=gdb-16.2
 PACKAGE_NAME_SIMPLE=gdb
 PACKAGE_PARENT_PATH="/usr/local/src"
 PACKAGE_SOURCE_PATH=${PACKAGE_PARENT_PATH}/${PACKAGE_NAME}
