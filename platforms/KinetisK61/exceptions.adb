15c6a57ce97017177fff5a568f0f418a70e2afe9
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-02-28 20:52:03 +0100

    KinetisK61: very primitive exception handling

--- platforms/KinetisK61/exceptions.adb
+++ platforms/KinetisK61/exceptions.adb
@@ -15,7 +15,6 @@
 -- Please consult the LICENSE.txt file located in the top-level directory.                                           --
 -----------------------------------------------------------------------------------------------------------------------
 
-with Interfaces;
 with Configure;
 with LLutils;
 with Abort_Library;
@@ -36,7 +35,6 @@ package body Exceptions
    --                                                                        --
    --========================================================================--
 
-   use Interfaces;
    use K61;
 
    --========================================================================--
@@ -51,16 +49,34 @@ package body Exceptions
    -- Exception_Process
    ----------------------------------------------------------------------------
    procedure Exception_Process
+      (VectorN : in Unsigned_32;
+       LR      : in Unsigned_32)
       is
    begin
       Console.Print ("*** EXCEPTION", NL => True);
+      case VectorN is
+         when 1  => Console.Print (ARMv7M.MsgPtr_Reset.all, NL => True);
+         when 2  => Console.Print (ARMv7M.MsgPtr_NMI.all, NL => True);
+         when 3  => Console.Print (ARMv7M.MsgPtr_HardFault.all, NL => True);
+         when 4  => Console.Print (ARMv7M.MsgPtr_MemManage.all, NL => True);
+         when 5  => Console.Print (ARMv7M.MsgPtr_BusFault.all, NL => True);
+         when 6  => Console.Print (ARMv7M.MsgPtr_UsageFault.all, NL => True);
+         when 11 => Console.Print (ARMv7M.MsgPtr_SVCall.all, NL => True);
+         when 14 => Console.Print (ARMv7M.MsgPtr_PendSV.all, NL => True);
+         when 15 => Console.Print (ARMv7M.MsgPtr_SysTick.all, NL => True);
+         when 7 .. 10 | 12 .. 13 =>
+            Console.Print (ARMv7M.MsgPtr_Reserved.all, NL => True);
+         when others             =>
+            Console.Print ("UNKNOWN", NL => True);
+      end case;
+      Console.Print (LR, NL => True);
       Abort_Library.System_Abort;
    end Exception_Process;
 
    ----------------------------------------------------------------------------
-   -- Irq_Process
+   -- SysTick_Process
    ----------------------------------------------------------------------------
-   procedure Irq_Process
+   procedure SysTick_Process
       is
    begin
       BSP.Tick_Count := @ + 1;
@@ -68,6 +84,15 @@ package body Exceptions
       if (BSP.Tick_Count mod Configure.TICK_FREQUENCY) = 0 then
          GPIOF.PTOR (11) := True;
       end if;
+   end SysTick_Process;
+
+   ----------------------------------------------------------------------------
+   -- Irq_Process
+   ----------------------------------------------------------------------------
+   procedure Irq_Process
+      is
+   begin
+      null;
    end Irq_Process;
 
    ----------------------------------------------------------------------------
