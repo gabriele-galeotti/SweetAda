6123aefd6e0403b9328de2d64a72d895e805d415
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-03-13 21:01:36 +0100

    M5235BCC/exceptions.ad[b|s]: add units (will be used in next commits)

---
+++ platforms/M5235BCC/exceptions.adb
@@ -0,0 +1,84 @@
+-----------------------------------------------------------------------------------------------------------------------
+--                                                     SweetAda                                                      --
+-----------------------------------------------------------------------------------------------------------------------
+-- __HDS__                                                                                                           --
+-- __FLN__ exceptions.adb                                                                                            --
+-- __DSC__                                                                                                           --
+-- __HSH__ e69de29bb2d1d6434b8b29ae775ad8c2e48c5391                                                                  --
+-- __HDE__                                                                                                           --
+-----------------------------------------------------------------------------------------------------------------------
+-- Copyright (C) 2020-2025 Gabriele Galeotti                                                                         --
+--                                                                                                                   --
+-- SweetAda web page: http://sweetada.org                                                                            --
+-- contact address: gabriele.galeotti@sweetada.org                                                                   --
+-- This work is licensed under the terms of the MIT License.                                                         --
+-- Please consult the LICENSE.txt file located in the top-level directory.                                           --
+-----------------------------------------------------------------------------------------------------------------------
+
+with Interfaces;
+with Abort_Library;
+with Bits;
+with CPU;
+with MCF523x;
+with BSP;
+with Console;
+
+package body Exceptions
+   is
+
+   --========================================================================--
+   --                                                                        --
+   --                                                                        --
+   --                           Local declarations                           --
+   --                                                                        --
+   --                                                                        --
+   --========================================================================--
+
+   use Interfaces;
+   use Bits;
+   use MCF523x;
+
+   --========================================================================--
+   --                                                                        --
+   --                                                                        --
+   --                           Package subprograms                          --
+   --                                                                        --
+   --                                                                        --
+   --========================================================================--
+
+   ----------------------------------------------------------------------------
+   -- Exception_Process
+   ----------------------------------------------------------------------------
+   procedure Exception_Process
+      is
+   begin
+      Console.Print ("*** EXCEPTION", NL => True);
+      Abort_Library.System_Abort;
+   end Exception_Process;
+
+   ----------------------------------------------------------------------------
+   -- Irq_Process
+   ----------------------------------------------------------------------------
+   procedure Irq_Process
+      is
+   begin
+      BSP.Tick_Count := @ + 1;
+      PIT0.PCSR.PIF := True;
+      if BSP.Tick_Count mod 1_000 = 0 then
+         Console.Print ("OK", NL => True);
+      end if;
+   end Irq_Process;
+
+   ----------------------------------------------------------------------------
+   -- Init
+   ----------------------------------------------------------------------------
+   procedure Init
+      is
+      Vectors_Table : aliased Asm_Entry_Point
+         with Import        => True,
+              External_Name => "vectors_table";
+   begin
+      CPU.VBR_Set (Vectors_Table'Address);
+   end Init;
+
+end Exceptions;
