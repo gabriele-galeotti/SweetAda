c8599bf8fb36c3a5635653ab3c8f402234b5dea1
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-03-13 21:02:39 +0100

    QEMU-PPC64/p8.ads: use Interfaces, cosmetics

--- platforms/QEMU-PPC64/p8.ads
+++ platforms/QEMU-PPC64/p8.ads
@@ -36,16 +36,14 @@ package P8
    use System;
    package SSE renames System.Storage_Elements;
    use SSE;
+   use Interfaces;
 
    XSCOM_BASEADDRESS : constant := 16#0003_FC00_0000_0000#;
 
    CFAM_REG      : constant := 16#000F_000F#;
-   -- P8 Venice DD2.0
-   CFAM_ID_P8    : constant := 16#220E_A049_8000_0000#;
-   -- P8 Murano DD2.1
-   CFAM_ID_P8E   : constant := 16#221E_F049_8000_0000#;
-   -- P8 Naples DD1.0
-   CFAM_ID_P8NVL : constant := 16#120D_3049_8000_0000#;
+   CFAM_ID_P8    : constant := 16#220E_A049_8000_0000#; -- P8 Venice DD2.0
+   CFAM_ID_P8E   : constant := 16#221E_F049_8000_0000#; -- P8 Murano DD2.1
+   CFAM_ID_P8NVL : constant := 16#120D_3049_8000_0000#; -- P8 Naples DD1.0
 
    PNV_XSCOM_LPC_BASE : constant := 16#000B_0020#;
    LPC_IO_OPB_ADDR    : constant := 16#D001_0000#;
@@ -56,17 +54,17 @@ package P8
    ECCB_DATA  : constant := 3;
 
    type LPC_CMD_Type is record
-      Magic   : Bits.Bits_4 := 0;       -- 0xD
-      Size    : Bits.Bits_4;            -- size
+      Magic   : Bits.Bits_4 := 0; -- 0xD
+      Size    : Bits.Bits_4;      -- size
       Unused2 : Bits.Bits_7 := 0;
-      Read    : Boolean;                -- read/write
+      Read    : Boolean;          -- read/write
       Unused3 : Bits.Bits_7 := 0;
-      Addrlen : Bits.Bits_3 := 0;       -- address length (4)
+      Addrlen : Bits.Bits_3 := 0; -- address length (4)
       Unused4 : Bits.Bits_6 := 0;
-      Address : Interfaces.Unsigned_32; -- address
+      Address : Unsigned_32;      -- address
    end record
       with Bit_Order => High_Order_First,
-           Size => 64;
+           Size      => 64;
    for LPC_CMD_Type use record
       Magic   at 0 range  0 ..  3;
       Size    at 0 range  4 ..  7;
@@ -78,27 +76,27 @@ package P8
       Address at 0 range 32 .. 63;
    end record;
 
-   function To_U64 is new Ada.Unchecked_Conversion (LPC_CMD_Type, Interfaces.Unsigned_64);
+   function To_U64 is new Ada.Unchecked_Conversion (LPC_CMD_Type, Unsigned_64);
 
    procedure HMER_Clear
       with Inline => True;
    function HMER_Read
-      return Interfaces.Unsigned_64
+      return Unsigned_64
       with Inline => True;
    function XSCOM_Address
-      (Base   : Interfaces.Unsigned_64;
-       Offset : Interfaces.Unsigned_64)
-      return SSE.Integer_Address
+      (Base   : Unsigned_64;
+       Offset : Unsigned_64)
+      return Integer_Address
       with Inline => True;
    procedure XSCOM_Wait_Done
       with Inline => True;
    function XSCOM_In64
-      (Device_Address : SSE.Integer_Address)
-      return Interfaces.Unsigned_64
+      (Device_Address : Integer_Address)
+      return Unsigned_64
       with Inline => True;
    procedure XSCOM_Out64
-      (Device_Address : in SSE.Integer_Address;
-       Value          : in Interfaces.Unsigned_64)
+      (Device_Address : in Integer_Address;
+       Value          : in Unsigned_64)
       with Inline => True;
 
 end P8;
