d2dd1705863c329219a1db0677fdf624964354bf
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-03-18 21:17:45 +0100

    ZOOM: - add some little infrastructure - cleanup, remove old code and install some vectors

--- platforms/ZOOM/exceptions.adb
+++ platforms/ZOOM/exceptions.adb
@@ -2,7 +2,7 @@
 --                                                     SweetAda                                                      --
 -----------------------------------------------------------------------------------------------------------------------
 -- __HDS__                                                                                                           --
--- __FLN__ bsp.adb                                                                                                   --
+-- __FLN__ exceptions.adb                                                                                            --
 -- __DSC__                                                                                                           --
 -- __HSH__ e69de29bb2d1d6434b8b29ae775ad8c2e48c5391                                                                  --
 -- __HDE__                                                                                                           --
@@ -15,15 +15,24 @@
 -- Please consult the LICENSE.txt file located in the top-level directory.                                           --
 -----------------------------------------------------------------------------------------------------------------------
 
-with Definitions;
-with Bits;
-with Secondary_Stack;
+with Abort_Library;
+with CPU;
 with MCF5373;
-with Exceptions;
+with BSP;
 with Console;
 
-package body BSP
+package body Exceptions
    is
+
+   --========================================================================--
+   --                                                                        --
+   --                                                                        --
+   --                           Local declarations                           --
+   --                                                                        --
+   --                                                                        --
+   --========================================================================--
+
+   use MCF5373;
 
    --========================================================================--
    --                                                                        --
@@ -33,53 +42,32 @@ package body BSP
    --                                                                        --
    --========================================================================--
 
-   use Interfaces;
-   use Definitions;
-   use Bits;
-
    ----------------------------------------------------------------------------
-   -- Console wrappers
+   -- Exception_Process
    ----------------------------------------------------------------------------
-
-   procedure Console_Putchar
-      (C : in Character)
+   procedure Exception_Process
       is
    begin
-      -- wait for transmitter available
-      loop exit when MCF5373.USR0.TXRDY; end loop;
-      MCF5373.UTB0 := To_U8 (C);
-   end Console_Putchar;
-
-   procedure Console_Getchar
-      (C : out Character)
-      is
-   begin
-      -- wait for receiver available
-      loop exit when MCF5373.USR0.RXRDY; end loop;
-      C := To_Ch (MCF5373.URB0);
-   end Console_Getchar;
+      Console.Print ("*** EXCEPTION", NL => True);
+      Abort_Library.System_Abort;
+   end Exception_Process;
 
    ----------------------------------------------------------------------------
-   -- Setup.
+   -- Irq_Process
    ----------------------------------------------------------------------------
-   procedure Setup
+   procedure Irq_Process
       is
    begin
-      -------------------------------------------------------------------------
-      Secondary_Stack.Init;
-      -- Console --------------------------------------------------------------
-      Console.Console_Descriptor := (
-         Write => Console_Putchar'Access,
-         Read  => Console_Getchar'Access
-         );
-      Console.Print (ANSI_CLS & ANSI_CUPHOME & VT100_LINEWRAP);
-      -------------------------------------------------------------------------
-      Console.Print ("ZOOM (MCF5373)", NL => True);
-      Console.Print (Prefix => "PIN: ", Value => Unsigned_32 (MCF5373.CIR.PIN), NL => True);
-      Console.Print (Prefix => "PRN: ", Value => Unsigned_32 (MCF5373.CIR.PRN), NL => True);
-      -------------------------------------------------------------------------
-      Exceptions.Init;
-      -------------------------------------------------------------------------
-   end Setup;
+      null;
+   end Irq_Process;
 
-end BSP;
+   ----------------------------------------------------------------------------
+   -- Init
+   ----------------------------------------------------------------------------
+   procedure Init
+      is
+   begin
+      CPU.VBR_Set (Vectors_Table'Address);
+   end Init;
+
+end Exceptions;
--- platforms/ZOOM/exceptions.ads
+++ platforms/ZOOM/exceptions.ads
@@ -2,7 +2,7 @@
 --                                                     SweetAda                                                      --
 -----------------------------------------------------------------------------------------------------------------------
 -- __HDS__                                                                                                           --
--- __FLN__ bsp.adb                                                                                                   --
+-- __FLN__ exceptions.ads                                                                                            --
 -- __DSC__                                                                                                           --
 -- __HSH__ e69de29bb2d1d6434b8b29ae775ad8c2e48c5391                                                                  --
 -- __HDE__                                                                                                           --
@@ -15,71 +15,40 @@
 -- Please consult the LICENSE.txt file located in the top-level directory.                                           --
 -----------------------------------------------------------------------------------------------------------------------
 
-with Definitions;
-with Bits;
-with Secondary_Stack;
-with MCF5373;
-with Exceptions;
-with Console;
+with Interfaces;
 
-package body BSP
+package Exceptions
    is
 
    --========================================================================--
    --                                                                        --
    --                                                                        --
-   --                           Package subprograms                          --
+   --                               Public part                              --
    --                                                                        --
    --                                                                        --
    --========================================================================--
 
    use Interfaces;
-   use Definitions;
-   use Bits;
 
-   ----------------------------------------------------------------------------
-   -- Console wrappers
-   ----------------------------------------------------------------------------
+   type Vectors_Table_Type is array (Natural range <>) of Unsigned_32
+      with Pack => True;
 
-   procedure Console_Putchar
-      (C : in Character)
-      is
-   begin
-      -- wait for transmitter available
-      loop exit when MCF5373.USR0.TXRDY; end loop;
-      MCF5373.UTB0 := To_U8 (C);
-   end Console_Putchar;
+   Vectors_Table : aliased Vectors_Table_Type (0 .. 191)
+      with Size          => 192 * Unsigned_32'Object_Size,
+           Volatile      => True,
+           Import        => True,
+           External_Name => "vectors_table";
 
-   procedure Console_Getchar
-      (C : out Character)
-      is
-   begin
-      -- wait for receiver available
-      loop exit when MCF5373.USR0.RXRDY; end loop;
-      C := To_Ch (MCF5373.URB0);
-   end Console_Getchar;
+   procedure Exception_Process
+      with Export        => True,
+           Convention    => Asm,
+           External_Name => "exception_process";
 
-   ----------------------------------------------------------------------------
-   -- Setup.
-   ----------------------------------------------------------------------------
-   procedure Setup
-      is
-   begin
-      -------------------------------------------------------------------------
-      Secondary_Stack.Init;
-      -- Console --------------------------------------------------------------
-      Console.Console_Descriptor := (
-         Write => Console_Putchar'Access,
-         Read  => Console_Getchar'Access
-         );
-      Console.Print (ANSI_CLS & ANSI_CUPHOME & VT100_LINEWRAP);
-      -------------------------------------------------------------------------
-      Console.Print ("ZOOM (MCF5373)", NL => True);
-      Console.Print (Prefix => "PIN: ", Value => Unsigned_32 (MCF5373.CIR.PIN), NL => True);
-      Console.Print (Prefix => "PRN: ", Value => Unsigned_32 (MCF5373.CIR.PRN), NL => True);
-      -------------------------------------------------------------------------
-      Exceptions.Init;
-      -------------------------------------------------------------------------
-   end Setup;
+   procedure Irq_Process
+      with Export        => True,
+           Convention    => Asm,
+           External_Name => "irq_process";
 
-end BSP;
+   procedure Init;
+
+end Exceptions;
