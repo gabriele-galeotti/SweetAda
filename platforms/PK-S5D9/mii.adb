a0fb34578695b33768e06c44efd8398cc51a2b02
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-04-20 01:01:07 +0200

    PK-S5D9/mii.adb: completely rewrite the unit (still needs delay removal)

--- platforms/PK-S5D9/mii.adb
+++ platforms/PK-S5D9/mii.adb
@@ -36,18 +36,29 @@ package body MII
    use Bits;
    use S5D9;
 
-   MII_Delay : constant := 10_000;
+   READ  : constant := 0;
+   WRITE : constant := 1;
 
+   procedure MII_Delay
+      (Count : in Integer)
+      with Inline => True;
    function Bit_Read
       return Bits_1
       with Inline => True;
-
-   procedure Bit_Send
+   procedure Bit_Write
       (Bit : in Bits_1)
       with Inline => True;
-
-   procedure Bus_Release
+   procedure Turn_Around
       with Inline => True;
+
+   procedure ReadOrWrite
+      (ReadWrite : in Integer);
+   procedure AddressOrRegister
+      (Value : in Unsigned_8);
+   function RegisterRead
+      return Unsigned_16;
+   procedure RegisterWrite
+      (Value : in Unsigned_16);
 
    --========================================================================--
    --                                                                        --
@@ -57,41 +68,114 @@ package body MII
    --                                                                        --
    --========================================================================--
 
+   ----------------------------------------------------------------------------
+   -- MII_Delay
+   ----------------------------------------------------------------------------
+   procedure MII_Delay
+      (Count : in Integer)
+      is
+   begin
+      for Delay_Loop_Count in 1 .. Count loop CPU.NOP; end loop;
+   end MII_Delay;
+
+   ----------------------------------------------------------------------------
+   -- Bit_Read
+   ----------------------------------------------------------------------------
    function Bit_Read
       return Bits_1
       is
       Bit : Bits_1;
    begin
-      PIR := (MMD => 0, MDC => 1, others => <>);
-      for Delay_Loop_Count in 1 .. MII_Delay loop CPU.NOP; end loop;
       Bit := PIR.MDI;
+      PIR := (MMD => 0, MDC => 1, others => <>);
+      MII_Delay (1_000);
       PIR := (MMD => 0, MDC => 0, others => <>);
-      for Delay_Loop_Count in 1 .. MII_Delay loop CPU.NOP; end loop;
+      MII_Delay (1_000);
       return Bit;
    end Bit_Read;
 
-   procedure Bit_Send
+   ----------------------------------------------------------------------------
+   -- Bit_Write
+   ----------------------------------------------------------------------------
+   procedure Bit_Write
       (Bit : in Bits_1)
       is
    begin
-      PIR := (MMD => 1, MDC => 0, MDO => Bit, others => <>);
-      for Delay_Loop_Count in 1 .. MII_Delay loop CPU.NOP; end loop;
       PIR := (MMD => 1, MDC => 1, MDO => Bit, others => <>);
-      for Delay_Loop_Count in 1 .. MII_Delay loop CPU.NOP; end loop;
+      MII_Delay (1_000);
       PIR := (MMD => 1, MDC => 0, MDO => Bit, others => <>);
-      for Delay_Loop_Count in 1 .. MII_Delay loop CPU.NOP; end loop;
-   end Bit_Send;
+      MII_Delay (1_000);
+   end Bit_Write;
 
-   procedure Bus_Release
+   ----------------------------------------------------------------------------
+   -- Turn_Around
+   ----------------------------------------------------------------------------
+   procedure Turn_Around
       is
    begin
-      PIR := (MMD => 0, MDC => 0, others => <>);
-      for Delay_Loop_Count in 1 .. MII_Delay loop CPU.NOP; end loop;
       PIR := (MMD => 0, MDC => 1, others => <>);
-      for Delay_Loop_Count in 1 .. MII_Delay loop CPU.NOP; end loop;
+      MII_Delay (1_000);
       PIR := (MMD => 0, MDC => 0, others => <>);
-      for Delay_Loop_Count in 1 .. MII_Delay loop CPU.NOP; end loop;
-   end Bus_Release;
+      MII_Delay (1_000);
+      PIR := (MMD => 0, MDC => 1, others => <>);
+      MII_Delay (1_000);
+      PIR := (MMD => 0, MDC => 0, others => <>);
+      MII_Delay (1_000);
+   end Turn_Around;
+
+   ----------------------------------------------------------------------------
+   -- ReadOrWrite
+   ----------------------------------------------------------------------------
+   procedure ReadOrWrite
+      (ReadWrite : in Integer)
+      is
+   begin
+      for N in 1 .. 32 loop Bit_Write (1); end loop; -- preamble
+      Bit_Write (0); Bit_Write (1);                  -- start of frame
+      if ReadWrite = 0 then
+         Bit_Write (1); Bit_Write (0);               -- read operation
+      else
+         Bit_Write (0); Bit_Write (1);               -- write operation
+      end if;
+   end ReadOrWrite;
+
+   ----------------------------------------------------------------------------
+   -- AddressOrRegister
+   ----------------------------------------------------------------------------
+   procedure AddressOrRegister
+      (Value : in Unsigned_8)
+      is
+   begin
+      for Bitshift in reverse 0 .. 4 loop
+         Bit_Write (Bits_1 (Shift_Right (Value, Bitshift) and 1));
+      end loop;
+   end AddressOrRegister;
+
+   ----------------------------------------------------------------------------
+   -- RegisterRead
+   ----------------------------------------------------------------------------
+   function RegisterRead
+      return Unsigned_16
+      is
+      Data : Unsigned_16 := 0;
+   begin
+      for Bitshift in 0 .. 15 loop
+         Data := Shift_Left (@, 1) or Unsigned_16 (Bit_Read);
+      end loop;
+      return Data;
+   end RegisterRead;
+
+   ----------------------------------------------------------------------------
+   -- RegisterWrite
+   ----------------------------------------------------------------------------
+   procedure RegisterWrite
+      (Value : in Unsigned_16)
+      is
+   begin
+      for Bitshift in reverse 0 .. 15 loop
+         Bit_Write (Bits_1 (Shift_Right (Value, Bitshift) and 1));
+      end loop;
+   end RegisterWrite;
 
    ----------------------------------------------------------------------------
    -- Init
@@ -101,52 +185,35 @@ package body MII
       Data : Unsigned_16;
    begin
       -- ETH_RESET# active low
-      PFSR (P806).PMR := False;
-      PORT (8).PODR (6) := False;
-      PORT (8).PDR (6) := True;
---      loop
---         PORT (8).PODR (6) := False;
---         for Delay_Loop_Count in 1 .. 1_000_000 loop CPU.NOP; end loop;
---         PORT (8).PODR (6) := True;
---         for Delay_Loop_Count in 1 .. 1_000_000 loop CPU.NOP; end loop;
---      end loop;
+      PFSR (P806).PMR := False;   -- GPIO
+      PORT (8).PODR (6) := False; -- active low
+      PORT (8).PDR (6) := True;   -- output
       -- ETH (PHYAD[2])
-      PFSR (P701).PMR := False;
+      PFSR (P701).PMR := False;   -- GPIO
       PORT (7).PODR (1) := False; -- PHYAD[2] = 0
-      PORT (7).PDR (1) := True;
+      PORT (7).PDR (1) := True;   -- output
       -- ETH_CRS_DV (PHYAD[1:0])
-      PFSR (P705).PMR := False;
+      PFSR (P705).PMR := False;   -- GPIO
       PORT (7).PODR (5) := False; -- PHYAD[1:0] = 00
-      PORT (7).PDR (5) := True;
+      PORT (7).PDR (5) := True;   -- output
       -- ETH_MDC
       PFSR (P401) := (PMR => True, PSEL => PSEL_ETHERC_RMII, others => <>);
       -- ETH_MDIO
       PFSR (P402) := (PMR => True, PSEL => PSEL_ETHERC_RMII, others => <>);
       -- deactivate RESET#
       PORT (8).PODR (6) := True;
-
-      for Delay_Loop_Count in 1 .. 1_000_000 loop CPU.NOP; end loop;
-
-      -- preamble
-      for N in 1 .. 32 loop Bit_Send (1); end loop;
-      -- start of frame
-      Bit_Send (0); Bit_Send (1);
-      -- read operation
-      Bit_Send (1); Bit_Send (0);
-      -- PHY address
-      -- for N in 1 .. 5 loop Bit_Send (0); end loop;
-      Bit_Send (0); Bit_Send (0); Bit_Send (0); Bit_Send (0); Bit_Send (0);
-      -- register address
-      Bit_Send (0); Bit_Send (0); Bit_Send (0); Bit_Send (1); Bit_Send (0);
-      -- turn-around
-      Bus_Release; Bus_Release;
-      -- read
-      Data := 0;
-      for N in 0 .. 15 loop
-         Data := Shift_Left (@, 1) or Unsigned_16 (Bit_Read);
-      end loop;
-      Console.Print (Prefix => "MII: ", Value => Data, NL => True);
-
+      MII_Delay (1_000_000);
+      -- read ID registers
+      ReadOrWrite (READ);
+      AddressOrRegister (0);
+      AddressOrRegister (2);
+      Turn_Around;
+      Console.Print (Prefix => "MII reg2: ", Value => RegisterRead, NL => True);
+      ReadOrWrite (READ);
+      AddressOrRegister (0);
+      AddressOrRegister (3);
+      Turn_Around;
+      Console.Print (Prefix => "MII reg3: ", Value => RegisterRead, NL => True);
    end Init;
 
 end MII;
