9944f6fc64880e40fcee7a3aa60d1b8bb00c707e
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-04-30 23:35:15 +0200

    FRDM-KL46Z/application.adb: - put test inside conditional - write a welcome message on UART - a rotating hyphen on LCD

--- application/test-frdm-kl46z.adb
+++ application/test-frdm-kl46z.adb
@@ -1,5 +1,6 @@
 
 with Interfaces;
+with Bits;
 with CPU;
 with KL46Z;
 with Console;
@@ -16,6 +17,7 @@ package body Application
    --========================================================================--
 
    use Interfaces;
+   use Bits;
    use KL46Z;
 
    --========================================================================--
@@ -33,19 +35,35 @@ package body Application
       is
    begin
       -------------------------------------------------------------------------
-      -- blink on-board GREEN LED
-      declare
-         Delay_Count : constant := 10_000_000;
-      begin
-         -- LED1 (GREEN)
-         PORTD_MUXCTRL.PCR (5).MUX := MUX_GPIO;
-         GPIOD.PDDR (5) := True;
-         while True loop
-            GPIOD.PTOR (5) := True;
-            Console.Print (".");
-            for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
-         end loop;
-      end;
+      if True then
+         declare
+            Delay_Count : constant := 10_000_000;
+            DIGITON     : constant LCD_Waveform_Type := (C => True, others => False);
+            DIGITOFF    : constant LCD_Waveform_Type := (others => False);
+            Spin        : Bits_2;
+            Digit1 renames LCD_WF (9).WF (1);
+            Digit2 renames LCD_WF (1).WF (3);
+            Digit3 renames LCD_WF (13).WF (1);
+            Digit4 renames LCD_WF (2).WF (2);
+         begin
+            -- LED1 (GREEN)
+            PORTD_MUXCTRL.PCR (5).MUX := MUX_GPIO;
+            GPIOD.PDDR (5) := True;
+            Spin := 0;
+            while True loop
+               GPIOD.PTOR (5) := True;
+               Console.Print ("hello, SweetAda", NL => True);
+               case Spin is
+                  when 0 => Digit4 := DIGITOFF; Digit1 := DIGITON;
+                  when 1 => Digit1 := DIGITOFF; Digit2 := DIGITON;
+                  when 2 => Digit2 := DIGITOFF; Digit3 := DIGITON;
+                  when 3 => Digit3 := DIGITOFF; Digit4 := DIGITON;
+               end case;
+               Spin := @ + 1;
+               for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
+            end loop;
+         end;
+      end if;
       -------------------------------------------------------------------------
       loop null; end loop;
       -------------------------------------------------------------------------
