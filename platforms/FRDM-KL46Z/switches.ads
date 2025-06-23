6aca849fe19dca61fdd7ee498802a5d65c671047
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-06-23 23:14:54 +0200

    FRDM-KL46Z: new unit "switches" for SW1 & SW3 button inputs

---
+++ platforms/FRDM-KL46Z/switches.ads
@@ -0,0 +1,37 @@
+-----------------------------------------------------------------------------------------------------------------------
+--                                                     SweetAda                                                      --
+-----------------------------------------------------------------------------------------------------------------------
+-- __HDS__                                                                                                           --
+-- __FLN__ switches.ads                                                                                              --
+-- __DSC__                                                                                                           --
+-- __HSH__ e69de29bb2d1d6434b8b29ae775ad8c2e48c5391                                                                  --
+-- __HDE__                                                                                                           --
+-----------------------------------------------------------------------------------------------------------------------
+-- Copyright (C) 2020-2025 Gabriele Galeotti                                                                         --
+--                                                                                                                   --
+-- SweetAda web page: http://sweetada.org                                                                            --
+-- contact address: gabriele.galeotti@sweetada.org                                                                   --
+-- This work is licensed under the terms of the MIT License.                                                         --
+-- Please consult the LICENSE.txt file located in the top-level directory.                                           --
+-----------------------------------------------------------------------------------------------------------------------
+
+package Switches
+   is
+
+   --========================================================================--
+   --                                                                        --
+   --                                                                        --
+   --                               Public part                              --
+   --                                                                        --
+   --                                                                        --
+   --========================================================================--
+
+   function SW1_Read
+      return Boolean;
+
+   function SW3_Read
+      return Boolean;
+
+   procedure Init;
+
+end Switches;
