b8dafe4e0209c0939379359e95c2a97a965d53a2
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-08-10 15:52:35 +0200

    drivers/ili9341v.ads: - use symbolic names for commands as per manual - add constants for CABC command

--- drivers/ili9341v.ads
+++ drivers/ili9341v.ads
@@ -32,24 +32,58 @@ package ILI9341V
    use System;
    use Bits;
 
+   ----------------------------------------------------------------------------
    -- 8.1. Command List (Regulative Command Set)
+   ----------------------------------------------------------------------------
 
-   No_Operation                     : constant := 16#00#;
-   Software_Reset                   : constant := 16#01#;
-   Sleep_Out                        : constant := 16#11#;
-   Normal_Display_Mode_ON           : constant := 16#13#;
-   Display_OFF                      : constant := 16#28#;
-   Display_ON                       : constant := 16#29#;
-   Column_Address_Set               : constant := 16#2A#;
-   Page_Address_Set                 : constant := 16#2B#;
-   Memory_Write                     : constant := 16#2C#;
-   Color_Set                        : constant := 16#2D#;
-   Partial_Area                     : constant := 16#30#;
-   Memory_Access_Control            : constant := 16#36#;
-   Vertical_Scrolling_Start_Address : constant := 16#37#;
-   Pixel_Format_Set                 : constant := 16#3A#;
-   Write_Memory_Continue            : constant := 16#3C#;
-   Write_CTRL_Display               : constant := 16#53#;
+   NOP                     : constant := 16#00#; -- No Operation
+   SWRESET                 : constant := 16#01#; -- Software Reset
+   RDDIDIF                 : constant := 16#04#; -- Read Display Identification Information
+   RDDST                   : constant := 16#09#; -- Read Display Status
+   RDDPM                   : constant := 16#0A#; -- Read Display Power Mode
+   RDDMADCTL               : constant := 16#0B#; -- Read Display MADCTL
+   RDDCOLMOD               : constant := 16#0C#; -- Read Display Pixel Format
+   RDDIM                   : constant := 16#0D#; -- Read Display Image Mode
+   RDDSM                   : constant := 16#0E#; -- Read Display Signal Mode
+   RDDSDR                  : constant := 16#0F#; -- Read Display Self-Diagnostic Result
+   SPLIN                   : constant := 16#10#; -- Enter Sleep Mode
+   SLPOUT                  : constant := 16#11#; -- Sleep Out
+   PTLON                   : constant := 16#12#; -- Partial Mode On
+   NORON                   : constant := 16#13#; -- Normal Display Mode On
+   DINVOFF                 : constant := 16#20#; -- Display Inversion OFF
+   DINVON                  : constant := 16#21#; -- Display Inversion ON
+   GAMSET                  : constant := 16#26#; -- Gamma Set
+   DISPOFF                 : constant := 16#28#; -- Display OFF
+   DISPON                  : constant := 16#29#; -- Display ON
+   CASET                   : constant := 16#2A#; -- Column Address Set
+   PASET                   : constant := 16#2B#; -- Page Address Set
+   RAMWR                   : constant := 16#2C#; -- Memory Write
+   RGBSET                  : constant := 16#2D#; -- Color Set
+   RAMRD                   : constant := 16#2E#; -- Memory Read
+   PLTAR                   : constant := 16#30#; -- Partial Area
+   VSCRDEF                 : constant := 16#33#; -- Vertical Scrolling Definition
+   TEOFF                   : constant := 16#34#; -- Tearing Effect Line OFF
+   TEON                    : constant := 16#35#; -- Tearing Effect Line ON
+   MADCTL                  : constant := 16#36#; -- Memory Access Control
+   VSCRSADD                : constant := 16#37#; -- Vertical Scrolling Start Address
+   IDMOFF                  : constant := 16#38#; -- Idle Mode OFF
+   IDMON                   : constant := 16#39#; -- Idle Mode ON
+   PIXSET                  : constant := 16#3A#; -- Pixel Format Set
+   RAMWRC                  : constant := 16#3C#; -- Write_Memory_Continue
+   RAMRDC                  : constant := 16#3E#; -- Read_Memory_Continue
+   SETTSL                  : constant := 16#44#; -- Set_Tear_Scanline
+   GETSL                   : constant := 16#45#; -- Get_Scanline
+   WRDISBV                 : constant := 16#51#; -- Write Display Brightness
+   RDDISBV                 : constant := 16#52#; -- Read Display Brightness Value
+   WRCTRLD                 : constant := 16#53#; -- Write Control Display
+   RDCTRLD                 : constant := 16#54#; -- Read Control Display
+   WRCABC                  : constant := 16#55#; -- Write Content Adaptive Brightness Control
+   RDCABC                  : constant := 16#56#; -- Read Content Adaptive Brightness Control
+   WRCABCMB                : constant := 16#5E#; -- Backlight Control 1 Write CABC Minimum Brightness
+   RDCABCMB                : constant := 16#5F#; -- Backlight_Control_1 Read CABC Minimum Brightness
+   RDID1                   : constant := 16#DA#; -- Read ID1
+   RDID2                   : constant := 16#DB#; -- Read ID2
+   RDID3                   : constant := 16#DC#; -- Read ID3
 
    -- 8.2.29. Memory Access Control (36h)
 
@@ -112,5 +146,17 @@ package ILI9341V
       BCTRL   at 0 range 5 .. 5;
       Unused3 at 0 range 6 .. 7;
    end record;
+
+   -- 8.2.42. Write Content Adaptive Brightness Control (55h)
+   -- 8.2.43. Read Content Adaptive Brightness Control (56h)
+
+   CABC_OFF    : constant := 2#00#; -- Off
+   CABC_USER   : constant := 2#01#; -- User Interface Image
+   CABC_STILL  : constant := 2#10#; -- Still Picture
+   CABC_MOVING : constant := 2#11#; -- Moving Image
+
+   ----------------------------------------------------------------------------
+   -- 8.3. Description of Level 2 Command
+   ----------------------------------------------------------------------------
 
 end ILI9341V;
