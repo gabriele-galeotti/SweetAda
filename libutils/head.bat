9be1c7298d579fc015a150a4b1724d5cd4ae60b5
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-08-30 01:14:59 +0200

    libutils/head.[bat|ps1]: replacement utility, to gradually substitute sed.exe

---
+++ libutils/head.bat
@@ -0,0 +1,41 @@
+@ECHO OFF
+
+REM
+REM SweetAda cmd.exe PowerShell wrapper.
+REM
+REM Copyright (C) 2020-2025 Gabriele Galeotti
+REM
+REM This work is licensed under the terms of the MIT License.
+REM Please consult the LICENSE.txt file located in the top-level directory.
+REM
+
+REM
+REM Arguments:
+REM all arguments are passed to .ps1
+REM
+REM Environment variables:
+REM SWEETADA_PATH
+REM LIBUTILS_DIRECTORY
+REM
+
+REM ############################################################################
+REM # Script initialization.                                                   #
+REM #                                                                          #
+REM ############################################################################
+
+SETLOCAL ENABLEEXTENSIONS
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+REM ############################################################################
+REM # Main loop.                                                               #
+REM #                                                                          #
+REM ############################################################################
+
+powershell.exe                                          ^
+  -NoProfile                                            ^
+  -ExecutionPolicy Bypass                               ^
+  -File "%SWEETADA_PATH%"/%LIBUTILS_DIRECTORY%/head.ps1 ^
+  %*
+
+EXIT /B %ERRORLEVEL%
+
---
+++ libutils/head.ps1
@@ -0,0 +1,99 @@
+
+#
+# "head" mini-replacement for toolchain version information.
+#
+# Copyright (C) 2020-2025 Gabriele Galeotti
+#
+# This work is licensed under the terms of the MIT License.
+# Please consult the LICENSE.txt file located in the top-level directory.
+#
+
+#
+# Arguments:
+# optional "-n" (ignored)
+# $1 = number of lines to print (1 .. 9, default = 10)
+#
+# Environment variables:
+# none
+#
+
+################################################################################
+# Script initialization.                                                       #
+#                                                                              #
+################################################################################
+
+$scriptname = $MyInvocation.MyCommand.Name
+$nl = [Environment]::NewLine
+
+################################################################################
+# ExitWithCode()                                                               #
+#                                                                              #
+################################################################################
+function ExitWithCode
+{
+  param($exitcode)
+  $host.SetShouldExit($exitcode)
+  exit $exitcode
+}
+
+################################################################################
+# Write-Stderr()                                                               #
+#                                                                              #
+################################################################################
+function Write-Stderr
+{
+  param([PSObject]$inputobject)
+  $outf = if ($host.Name -eq "ConsoleHost")
+  {
+    [Console]::Error.WriteLine
+  }
+  else
+  {
+    $host.UI.WriteErrorLine
+  }
+  if ($inputobject)
+  {
+    [void]$outf.Invoke($inputobject.ToString())
+  }
+  else
+  {
+    [String[]]$lines = @()
+    $input | % { $lines += $_.ToString() }
+    [void]$outf.Invoke($lines -Join $nl)
+  }
+}
+
+################################################################################
+# Main loop.                                                                   #
+#                                                                              #
+################################################################################
+
+$linestoprint = 10
+
+$argsindex = 0
+while ($argsindex -lt $args.length)
+{
+  if ($args[$argsindex] -eq "-n")
+  {
+    # ignore
+  }
+  else
+  {
+    if ([Int]$args[$argsindex] -ge 1 -and [Int]$args[$argsindex] -le 9)
+    {
+      $linestoprint = [Int]$args[$argsindex]
+      break
+    }
+    else
+    {
+      Write-Stderr "$($scriptname): *** Error: unknown option `"$($args[$argsindex])`"."
+      ExitWithCode 1
+    }
+  }
+  $argsindex++
+}
+
+$Input | Select-Object -First $linestoprint
+
+ExitWithCode 0
+
