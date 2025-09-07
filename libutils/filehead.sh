895c179eeeb88027897309f86f37e51f46bfd73e
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-09-07 12:22:13 +0200

    libutils/[filehead|updatetm].sh: shell wrappers for head and touch (to be used later)

---
+++ libutils/filehead.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env sh
+
+#
+# "head" shell wrapper.
+#
+# Copyright (C) 2020-2025 Gabriele Galeotti
+#
+# This work is licensed under the terms of the MIT License.
+# Please consult the LICENSE.txt file located in the top-level directory.
+#
+
+#
+# Arguments:
+# arguments of native "head" utility
+#
+# Environment variables:
+# none
+#
+
+# shellcheck disable=SC2034,SC2317
+
+################################################################################
+# Script initialization.                                                       #
+#                                                                              #
+################################################################################
+
+SCRIPT_FILENAME=$(basename "$0")
+LOG_FILENAME=""
+if [ "x${LOG_FILENAME}" != "x" ] ; then
+  rm -f "${LOG_FILENAME}"
+  touch "${LOG_FILENAME}"
+fi
+
+################################################################################
+# log_print()                                                                  #
+#                                                                              #
+################################################################################
+log_print()
+{
+if [ "x${LOG_FILENAME}" != "x" ] ; then
+  printf "%s\n" "$1" | tee -a "${LOG_FILENAME}"
+else
+  printf "%s\n" "$1"
+fi
+return 0
+}
+
+################################################################################
+# log_print_error()                                                            #
+#                                                                              #
+################################################################################
+log_print_error()
+{
+if [ "x${LOG_FILENAME}" != "x" ] ; then
+  printf "%s\n" "$1" | tee -a "${LOG_FILENAME}" 1>&2
+else
+  printf "%s\n" "$1" 1>&2
+fi
+return 0
+}
+
+################################################################################
+# Main loop.                                                                   #
+#                                                                              #
+################################################################################
+
+head "$@"
+
+exit $?
+
