0daac0b6f49d6c679fd51145d188a0a0bd4d659a
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2025-12-21 18:38:04 +0100

    LPC-P2148/lpc2148.ads: definitions for CH6 Pin connect block

--- platforms/LPC-P2148/lpc2148.ads
+++ platforms/LPC-P2148/lpc2148.ads
@@ -344,6 +344,348 @@ pragma Style_Checks (Off);
            Convention           => Ada;
 
    ----------------------------------------------------------------------------
+   -- Chapter 6: LPC214x Pin connect block
+   ----------------------------------------------------------------------------
+
+   -- 6.4.1 Pin function Select register 0 (PINSEL0 - 0xE002 C000)
+
+   type P0_0_Type is new Bits_2;
+   P0_0_GPIO     : constant P0_0_Type := 2#00#; -- GPIO Port 0.0
+   P0_0_UART0TXD : constant P0_0_Type := 2#01#; -- TXD (UART0)
+   P0_0_PWM1     : constant P0_0_Type := 2#10#; -- PWM1
+   P0_0_RSVD     : constant P0_0_Type := 2#11#; -- Reserved
+
+   type P0_1_Type is new Bits_2;
+   P0_1_GPIO     : constant P0_1_Type := 2#00#; -- GPIO Port 0.1
+   P0_1_UART0RXD : constant P0_1_Type := 2#01#; -- RxD (UART0)
+   P0_1_PWM3     : constant P0_1_Type := 2#10#; -- PWM3
+   P0_1_EINT0    : constant P0_1_Type := 2#11#; -- EINT0
+
+   type P0_2_Type is new Bits_2;
+   P0_2_GPIO            : constant P0_2_Type := 2#00#; -- GPIO Port 0.2
+   P0_2_I2C0SCL0        : constant P0_2_Type := 2#01#; -- SCL0 (I2C0)
+   P0_2_Timer0Capture00 : constant P0_2_Type := 2#10#; -- Capture 0.0 (Timer 0)
+   P0_2_RSVD            : constant P0_2_Type := 2#11#; -- Reserved
+
+   type P0_3_Type is new Bits_2;
+   P0_3_GPIO          : constant P0_3_Type := 2#00#; -- GPIO Port 0.3
+   P0_3_I2C0SDA0      : constant P0_3_Type := 2#01#; -- SDA0 (I2 C0)
+   P0_3_Timer0Match00 : constant P0_3_Type := 2#10#; -- Match 0.0 (Timer 0)
+   P0_3_EINT1         : constant P0_3_Type := 2#11#; -- EINT1
+
+   type P0_4_Type is new Bits_2;
+   P0_4_GPIO            : constant P0_4_Type := 2#00#; -- GPIO Port 0.4
+   P0_4_SPI0SCK0        : constant P0_4_Type := 2#01#; -- SCK0 (SPI0)
+   P0_4_Timer0Capture01 : constant P0_4_Type := 2#10#; -- Capture 0.1 (Timer 0)
+   P0_4_AD06            : constant P0_4_Type := 2#11#; -- AD0.6
+
+   type P0_5_Type is new Bits_2;
+   P0_5_GPIO          : constant P0_5_Type := 2#00#; -- GPIO Port 0.5
+   P0_5_SPI0MISO0     : constant P0_5_Type := 2#01#; -- MISO0 (SPI0)
+   P0_5_Timer0Match01 : constant P0_5_Type := 2#10#; -- Match 0.1 (Timer 0)
+   P0_5_AD07          : constant P0_5_Type := 2#11#; -- AD0.7
+
+   type P0_6_Type is new Bits_2;
+   P0_6_GPIO            : constant P0_6_Type := 2#00#; -- GPIO Port 0.6
+   P0_6_SPI0MOSI0       : constant P0_6_Type := 2#01#; -- MOSI0 (SPI0)
+   P0_6_Timer0Capture02 : constant P0_6_Type := 2#10#; -- Capture 0.2 (Timer 0)
+   P0_6_RSVD            : constant P0_6_Type := 2#11#; -- Reserved
+   P0_6_AD10            : constant P0_6_Type := 2#11#; -- AD1.0
+
+   type P0_7_Type is new Bits_2;
+   P0_7_GPIO      : constant P0_7_Type := 2#00#; -- GPIO Port 0.7
+   P0_7_SPI0SSEL0 : constant P0_7_Type := 2#01#; -- SSEL0 (SPI0)
+   P0_7_PWM2      : constant P0_7_Type := 2#10#; -- PWM2
+   P0_7_EINT2     : constant P0_7_Type := 2#11#; -- EINT2
+
+   type P0_8_Type is new Bits_2;
+   P0_8_GPIO     : constant P0_8_Type := 2#00#; -- GPIO Port 0.8
+   P0_8_UART1TXD : constant P0_8_Type := 2#01#; -- TXD UART1
+   P0_8_PWM4     : constant P0_8_Type := 2#10#; -- PWM4
+   P0_8_RSVD     : constant P0_8_Type := 2#11#; -- Reserved
+   P0_8_AD11     : constant P0_8_Type := 2#11#; -- AD1.1
+
+   type P0_9_Type is new Bits_2;
+   P0_9_GPIO     : constant P0_9_Type := 2#00#; -- GPIO Port 0.9
+   P0_9_UART1RXD : constant P0_9_Type := 2#01#; -- RxD (UART1)
+   P0_9_PWM6     : constant P0_9_Type := 2#10#; -- PWM6
+   P0_9_EINT3    : constant P0_9_Type := 2#11#; -- EINT3
+
+   type P0_10_Type is new Bits_2;
+   P0_10_GPIO            : constant P0_10_Type := 2#00#; -- GPIO Port 0.10
+   P0_10_RSVD1           : constant P0_10_Type := 2#01#; -- Reserved
+   P0_10_UART1RTS        : constant P0_10_Type := 2#01#; -- RTS (UART1)
+   P0_10_Timer1Capture10 : constant P0_10_Type := 2#10#; -- Capture 1.0 (Timer 1)
+   P0_10_RSVD2           : constant P0_10_Type := 2#11#; -- Reserved
+   P0_10_AD12            : constant P0_10_Type := 2#11#; -- AD1.2
+
+   type P0_11_Type is new Bits_2;
+   P0_11_GPIO            : constant P0_11_Type := 2#00#; -- GPIO Port 0.11
+   P0_11_RSVD            : constant P0_11_Type := 2#01#; -- Reserved
+   P0_11_UART1CTS        : constant P0_11_Type := 2#01#; -- CTS (UART1)
+   P0_11_Timer1Capture11 : constant P0_11_Type := 2#10#; -- Capture 1.1 (Timer 1)
+   P0_11_I2C1SCL1        : constant P0_11_Type := 2#11#; -- SCL1 (I2C1)
+
+   type P0_12_Type is new Bits_2;
+   P0_12_GPIO          : constant P0_12_Type := 2#00#; -- GPIO Port 0.12
+   P0_12_RSVD1         : constant P0_12_Type := 2#01#; -- Reserved
+   P0_12_UART1DSR      : constant P0_12_Type := 2#01#; -- DSR (UART1)
+   P0_12_Timer1Match10 : constant P0_12_Type := 2#10#; -- Match 1.0 (Timer 1)
+   P0_12_RSVD2         : constant P0_12_Type := 2#11#; -- Reserved
+   P0_12_AD13          : constant P0_12_Type := 2#11#; -- AD1.3
+
+   type P0_13_Type is new Bits_2;
+   P0_13_GPIO          : constant P0_13_Type := 2#00#; -- GPIO Port 0.13
+   P0_13_RSVD1         : constant P0_13_Type := 2#01#; -- Reserved
+   P0_13_UART1DTR      : constant P0_13_Type := 2#01#; -- DTR (UART1)
+   P0_13_Timer1Match11 : constant P0_13_Type := 2#10#; -- Match 1.1 (Timer 1)
+   P0_13_RSVD2         : constant P0_13_Type := 2#11#; -- Reserved
+   P0_13_AD14          : constant P0_13_Type := 2#11#; -- AD1.4
+
+   type P0_14_Type is new Bits_2;
+   P0_14_GPIO     : constant P0_14_Type := 2#00#; -- GPIO Port 0.14
+   P0_14_RSVD     : constant P0_14_Type := 2#01#; -- Reserved
+   P0_14_UART1DCD : constant P0_14_Type := 2#01#; -- DCD (UART1)
+   P0_14_ENIT1    : constant P0_14_Type := 2#10#; -- EINT1
+   P0_14_I2C1SDA1 : constant P0_14_Type := 2#11#; -- SDA1 (I2 C1)
+
+   type P0_15_Type is new Bits_2;
+   P0_15_GPIO    : constant P0_15_Type := 2#00#; -- GPIO Port 0.15
+   P0_15_RSVD1   : constant P0_15_Type := 2#01#; -- Reserved
+   P0_15_UART1RI : constant P0_15_Type := 2#01#; -- RI (UART1)
+   P0_15_EINT2   : constant P0_15_Type := 2#10#; -- EINT2
+   P0_15_RSVD2   : constant P0_15_Type := 2#11#; -- Reserved
+   P0_15_AD15    : constant P0_15_Type := 2#11#; -- AD1.5
+
+   type PINSEL0_Type is record
+      P0  : P0_0_Type  := P0_0_GPIO;  -- Pin function Select
+      P1  : P0_1_Type  := P0_1_GPIO;  -- ''
+      P2  : P0_2_Type  := P0_2_GPIO;  -- ''
+      P3  : P0_3_Type  := P0_3_GPIO;  -- ''
+      P4  : P0_4_Type  := P0_4_GPIO;  -- ''
+      P5  : P0_5_Type  := P0_5_GPIO;  -- ''
+      P6  : P0_6_Type  := P0_6_GPIO;  -- ''
+      P7  : P0_7_Type  := P0_7_GPIO;  -- ''
+      P8  : P0_8_Type  := P0_8_GPIO;  -- ''
+      P9  : P0_9_Type  := P0_9_GPIO;  -- ''
+      P10 : P0_10_Type := P0_10_GPIO; -- ''
+      P11 : P0_11_Type := P0_11_GPIO; -- ''
+      P12 : P0_12_Type := P0_12_GPIO; -- ''
+      P13 : P0_13_Type := P0_13_GPIO; -- ''
+      P14 : P0_14_Type := P0_14_GPIO; -- ''
+      P15 : P0_15_Type := P0_15_GPIO; -- ''
+   end record
+      with Bit_Order => Low_Order_First,
+           Size      => 32;
+   for PINSEL0_Type use record
+      P0  at 0 range  0 ..  1;
+      P1  at 0 range  2 ..  3;
+      P2  at 0 range  4 ..  5;
+      P3  at 0 range  6 ..  7;
+      P4  at 0 range  8 ..  9;
+      P5  at 0 range 10 .. 11;
+      P6  at 0 range 12 .. 13;
+      P7  at 0 range 14 .. 15;
+      P8  at 0 range 16 .. 17;
+      P9  at 0 range 18 .. 19;
+      P10 at 0 range 20 .. 21;
+      P11 at 0 range 22 .. 23;
+      P12 at 0 range 24 .. 25;
+      P13 at 0 range 26 .. 27;
+      P14 at 0 range 28 .. 29;
+      P15 at 0 range 30 .. 31;
+   end record;
+
+   PINSEL0_ADDRESS : constant := 16#E002_C000#;
+
+   PINSEL0 : aliased PINSEL0_Type
+      with Address              => System'To_Address (PINSEL0_ADDRESS),
+           Volatile_Full_Access => True,
+           Import               => True,
+           Convention           => Ada;
+
+   -- 6.4.2 Pin function Select register 1 (PINSEL1 - 0xE002 C004)
+
+   type P0_16_Type is new Bits_2;
+   P0_16_GPIO            : constant P0_16_Type := 2#00#; -- GPIO Port 0.16
+   P0_16_EINT0           : constant P0_16_Type := 2#01#; -- EINT0
+   P0_16_Timer0Match02   : constant P0_16_Type := 2#10#; -- Match 0.2 (Timer 0)
+   P0_16_Timer0Capture02 : constant P0_16_Type := 2#11#; -- Capture 0.2 (Timer 0)
+
+   type P0_17_Type is new Bits_2;
+   P0_17_GPIO            : constant P0_17_Type := 2#00#; -- GPIO Port 0.17
+   P0_17_Timer1Capture12 : constant P0_17_Type := 2#01#; -- Capture 1.2 (Timer 1)
+   P0_17_SSPSCK1         : constant P0_17_Type := 2#10#; -- SCK1 (SSP)
+   P0_17_Timer1Match12   : constant P0_17_Type := 2#11#; -- Match 1.2 (Timer 1)
+
+   type P0_18_Type is new Bits_2;
+   P0_18_GPIO            : constant P0_18_Type := 2#00#; -- GPIO Port 0.18
+   P0_18_Timer1Capture13 : constant P0_18_Type := 2#01#; -- Capture 1.3 (Timer 1)
+   P0_18_SSPMISO1        : constant P0_18_Type := 2#10#; -- MISO1 (SSP)
+   P0_18_Timer1Match13   : constant P0_18_Type := 2#11#; -- Match 1.3 (Timer 1)
+
+   type P0_19_Type is new Bits_2;
+   P0_19_GPIO            : constant P0_19_Type := 2#00#; -- GPIO Port 0.19
+   P0_19_Timer1Match12   : constant P0_19_Type := 2#01#; -- Match 1.2 (Timer 1)
+   P0_19_SSPMOSI1        : constant P0_19_Type := 2#10#; -- MOSI1 (SSP)
+   P0_19_Timer1Capture12 : constant P0_19_Type := 2#11#; -- Capture 1.2 (Timer 1)
+
+   type P0_20_Type is new Bits_2;
+   P0_20_GPIO          : constant P0_20_Type := 2#00#; -- GPIO Port 0.20
+   P0_20_Timer1Match13 : constant P0_20_Type := 2#01#; -- Match 1.3 (Timer 1)
+   P0_20_SSPSSEL1      : constant P0_20_Type := 2#10#; -- SSEL1 (SSP)
+   P0_20_EINT3         : constant P0_20_Type := 2#11#; -- EINT3
+
+   type P0_21_Type is new Bits_2;
+   P0_21_GPIO            : constant P0_21_Type := 2#00#; -- GPIO Port 0.21
+   P0_21_PWM5            : constant P0_21_Type := 2#01#; -- PWM5
+   P0_21_RSVD            : constant P0_21_Type := 2#10#; -- Reserved
+   P0_21_AD16            : constant P0_21_Type := 2#10#; -- AD1.6
+   P0_21_Timer1Capture13 : constant P0_21_Type := 2#11#; -- Capture 1.3 (Timer 1)
+
+   type P0_22_Type is new Bits_2;
+   P0_22_GPIO            : constant P0_22_Type := 2#00#; -- GPIO Port 0.22
+   P0_22_RSVD            : constant P0_22_Type := 2#01#; -- Reserved
+   P0_22_AD17            : constant P0_22_Type := 2#01#; -- AD1.7
+   P0_22_Timer0Capture00 : constant P0_22_Type := 2#10#; -- Capture 0.0 (Timer 0)
+   P0_22_Timer0Match00   : constant P0_22_Type := 2#11#; -- Match 0.0 (Timer 0)
+
+   type P0_23_Type is new Bits_2;
+   P0_23_GPIO  : constant P0_23_Type := 2#00#; -- GPIO Port 0.23
+   P0_23_VBUS  : constant P0_23_Type := 2#01#; -- VBUS
+   P0_23_RSVD1 : constant P0_23_Type := 2#10#; -- Reserved
+   P0_23_RSVD2 : constant P0_23_Type := 2#11#; -- Reserved
+
+   type P0_24_Type is new Bits_2;
+   P0_24_RSVD1 : constant P0_24_Type := 2#00#; -- Reserved
+   P0_24_RSVD2 : constant P0_24_Type := 2#01#; -- Reserved
+   P0_24_RSVD3 : constant P0_24_Type := 2#10#; -- Reserved
+   P0_24_RSVD4 : constant P0_24_Type := 2#11#; -- Reserved
+
+   type P0_25_Type is new Bits_2;
+   P0_25_GPIO    : constant P0_25_Type := 2#00#; -- GPIO Port 0.25
+   P0_25_AD04    : constant P0_25_Type := 2#01#; -- AD0.4
+   P0_25_RSVD1   : constant P0_25_Type := 2#10#; -- Reserved
+   P0_25_DACAout : constant P0_25_Type := 2#10#; -- Aout(DAC)
+   P0_25_RSVD2   : constant P0_25_Type := 2#11#; -- Reserved
+
+   type P0_26_Type is new Bits_2;
+   P0_26_RSVD1 : constant P0_26_Type := 2#00#; -- Reserved
+   P0_26_RSVD2 : constant P0_26_Type := 2#01#; -- Reserved
+   P0_26_RSVD3 : constant P0_26_Type := 2#10#; -- Reserved
+   P0_26_RSVD4 : constant P0_26_Type := 2#11#; -- Reserved
+
+   type P0_27_Type is new Bits_2;
+   P0_27_RSVD1 : constant P0_27_Type := 2#00#; -- Reserved
+   P0_27_RSVD2 : constant P0_27_Type := 2#01#; -- Reserved
+   P0_27_RSVD3 : constant P0_27_Type := 2#10#; -- Reserved
+   P0_27_RSVD4 : constant P0_27_Type := 2#11#; -- Reserved
+
+   type P0_28_Type is new Bits_2;
+   P0_28_GPIO            : constant P0_28_Type := 2#00#; -- GPIO Port 0.28
+   P0_28_AD01            : constant P0_28_Type := 2#01#; -- AD0.1
+   P0_28_Timer0Capture02 : constant P0_28_Type := 2#10#; -- Capture 0.2 (Timer 0)
+   P0_28_Timer0Match02   : constant P0_28_Type := 2#11#; -- Match 0.2 (Timer 0)
+
+   type P0_29_Type is new Bits_2;
+   P0_29_GPIO            : constant P0_29_Type := 2#00#; -- GPIO Port 0.29
+   P0_29_AD02            : constant P0_29_Type := 2#01#; -- AD0.2
+   P0_29_Timer0Capture03 : constant P0_29_Type := 2#10#; -- Capture 0.3 (Timer 0)
+   P0_29_Timer0Match03   : constant P0_29_Type := 2#11#; -- Match 0.3 (Timer 0)
+
+   type P0_30_Type is new Bits_2;
+   P0_30_GPIO            : constant P0_30_Type := 2#00#; -- GPIO Port 0.30
+   P0_30_AD03            : constant P0_30_Type := 2#01#; -- AD0.3
+   P0_30_EINT3           : constant P0_30_Type := 2#10#; -- EINT3
+   P0_30_Timer0Capture00 : constant P0_30_Type := 2#11#; -- Capture 0.0 (Timer 0)
+
+   type P0_31_Type is new Bits_2;
+   P0_31_GPOPortonly : constant P0_31_Type := 2#00#; -- GPO Port only
+   P0_31_UPLED       : constant P0_31_Type := 2#01#; -- UP_LED
+   P0_31_CONNECT     : constant P0_31_Type := 2#10#; -- CONNECT
+   P0_31_RSVD        : constant P0_31_Type := 2#11#; -- Reserved
+
+   type PINSEL1_Type is record
+      P16 : P0_16_Type := P0_16_GPIO;        -- Pin function Select
+      P17 : P0_17_Type := P0_17_GPIO;        -- ''
+      P18 : P0_18_Type := P0_18_GPIO;        -- ''
+      P19 : P0_19_Type := P0_19_GPIO;        -- ''
+      P20 : P0_20_Type := P0_20_GPIO;        -- ''
+      P21 : P0_21_Type := P0_21_GPIO;        -- ''
+      P22 : P0_22_Type := P0_22_GPIO;        -- ''
+      P23 : P0_23_Type := P0_23_GPIO;        -- ''
+      P24 : P0_24_Type := P0_24_RSVD1;       -- ''
+      P25 : P0_25_Type := P0_25_GPIO;        -- ''
+      P26 : P0_26_Type := P0_26_RSVD1;       -- ''
+      P27 : P0_27_Type := P0_27_RSVD1;       -- ''
+      P28 : P0_28_Type := P0_28_GPIO;        -- ''
+      P29 : P0_29_Type := P0_29_GPIO;        -- ''
+      P30 : P0_30_Type := P0_30_GPIO;        -- ''
+      P31 : P0_31_Type := P0_31_GPOPortonly; -- ''
+   end record
+      with Bit_Order => Low_Order_First,
+           Size      => 32;
+   for PINSEL1_Type use record
+      P16 at 0 range  0 ..  1;
+      P17 at 0 range  2 ..  3;
+      P18 at 0 range  4 ..  5;
+      P19 at 0 range  6 ..  7;
+      P20 at 0 range  8 ..  9;
+      P21 at 0 range 10 .. 11;
+      P22 at 0 range 12 .. 13;
+      P23 at 0 range 14 .. 15;
+      P24 at 0 range 16 .. 17;
+      P25 at 0 range 18 .. 19;
+      P26 at 0 range 20 .. 21;
+      P27 at 0 range 22 .. 23;
+      P28 at 0 range 24 .. 25;
+      P29 at 0 range 26 .. 27;
+      P30 at 0 range 28 .. 29;
+      P31 at 0 range 30 .. 31;
+   end record;
+
+   PINSEL1_ADDRESS : constant := 16#E002_C004#;
+
+   PINSEL1 : aliased PINSEL1_Type
+      with Address              => System'To_Address (PINSEL1_ADDRESS),
+           Volatile_Full_Access => True,
+           Import               => True,
+           Convention           => Ada;
+
+   -- 6.4.3 Pin function Select register 2 (PINSEL2 - 0xE002 C014)
+
+   type GPIODEBUG_Type is new Bits_1;
+   GPIODEBUG_GPIO  : constant GPIODEBUG_Type := 0; -- Pins P1.36-26 are used as GPIO pins.
+   GPIODEBUG_DEBUG : constant GPIODEBUG_Type := 1; -- Pins P1.36-26 are used as a Debug port.
+
+   type GPIOTRACE_Type is new Bits_1;
+   GPIOTRACE_GPIO  : constant GPIOTRACE_Type := 0; -- Pins P1.25-16 are used as GPIO pins.
+   GPIOTRACE_TRACE : constant GPIOTRACE_Type := 1; -- Pins P1.25-16 are used as a Trace port.
+
+   type PINSEL2_Type is record
+      Reserved1 : Bits_2         := 0;
+      GPIODEBUG : GPIODEBUG_Type := GPIODEBUG_GPIO; -- Pin function Select
+      GPIOTRACE : GPIOTRACE_Type := GPIOTRACE_GPIO; -- ''
+      Reserved2 : Bits_28        := 0;
+   end record
+      with Bit_Order => Low_Order_First,
+           Size      => 32;
+   for PINSEL2_Type use record
+      Reserved1 at 0 range 0 ..  1;
+      GPIODEBUG at 0 range 2 ..  2;
+      GPIOTRACE at 0 range 3 ..  3;
+      Reserved2 at 0 range 4 .. 31;
+   end record;
+
+   PINSEL2_ADDRESS : constant := 16#E002_C014#;
+
+   PINSEL2 : aliased PINSEL2_Type
+      with Address              => System'To_Address (PINSEL2_ADDRESS),
+           Volatile_Full_Access => True,
+           Import               => True,
+           Convention           => Ada;
+
+   ----------------------------------------------------------------------------
    -- Chapter 8: LPC214x GPIO
    ----------------------------------------------------------------------------
 
