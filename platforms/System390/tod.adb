7f53bc4bbc35cd78796df73a98fca53b09c08f55
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2026-01-08 20:35:52 +0100

    System390/tod.ad[bs]: add useful constants, function to convert TOD to Epoch time

--- platforms/System390/tod.adb
+++ platforms/System390/tod.adb
@@ -15,10 +15,9 @@
 -- Please consult the LICENSE.txt file located in the top-level directory.                                           --
 -----------------------------------------------------------------------------------------------------------------------
 
-with System;
 with System.Machine_Code;
-with System.Storage_Elements;
 with Definitions;
+with Bits;
 
 package body TOD
    is
@@ -31,11 +30,17 @@ package body TOD
    --                                                                        --
    --========================================================================--
 
-   use System;
    use System.Machine_Code;
-   use System.Storage_Elements;
    use Interfaces;
    use Definitions;
+
+   -- 1 s in TOD units
+   TOD_S : constant := 4_096_000_000;
+
+   -- EPOCH_1900 is the duration starting 1900-01-01 and ending 1970-01-01,
+   -- in TOD units. Since 1 s (TOD) = 4096*E+6 and EPOCH_1900 = 25,567 days,
+   -- we find this result by computing 25_567 * 86_400 * 4_096_000_000
+   EPOCH_1900 : constant := 9_048_018_124_800_000_000;
 
    --========================================================================--
    --                                                                        --
@@ -64,5 +69,19 @@ package body TOD
           );
       return Value;
    end Read;
+
+   ----------------------------------------------------------------------------
+   -- To_Epoch
+   ----------------------------------------------------------------------------
+   procedure To_Epoch
+      (Value : in     Unsigned_64;
+       TM    :    out Time.TM_Time)
+      is
+      T : Unsigned_64 := Value;
+   begin
+      T := @ - EPOCH_1900; -- TOD offset 1900-01-01 -> 1970-01-01
+      T := @ / TOD_S;      -- scale to number of seconds
+      Time.Make_Time (Bits.LWord (T), TM);
+   end To_Epoch;
 
 end TOD;
