14a216ecaaabcddda0bee0738136dc4e4c967804
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2026-01-09 23:30:49 +0100

    System390/postbuild.sh: add a check

--- platforms/System390/postbuild.sh
+++ platforms/System390/postbuild.sh
@@ -11,6 +11,10 @@ case ${IPL_MODE} in
     #${TCLSH} S360obj.tcl -a 0x10000 -p ${KERNEL_PARENT_PATH}/${KERNEL_ROMFILE} ${KERNEL_IPLFILE}
     "${HERCULES_PREFIX}"/bin/dasdload -0 dasd_ctlfile.txt SYSRES.DSD 5
     ;;
+  *)
+    printf "%s\n" "*** Error: no valid IPL_MODE." 1>&2
+    exit 1
+    ;;
 esac
 
 exit $?
