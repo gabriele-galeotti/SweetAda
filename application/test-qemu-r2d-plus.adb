676addf36d34255435c277354c026c99dc9e7cab
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2026-01-18 16:09:47 +0100

    QEMU-R2D-PLUS/test-qemu-r2d-plus.adb: stretch delay and print a welcome message instead of a ".".

--- application/test-qemu-r2d-plus.adb
+++ application/test-qemu-r2d-plus.adb
@@ -35,11 +35,11 @@ package body Application
       -------------------------------------------------------------------------
       if True then
          declare
-            Delay_Count : constant := 100_000_000;
+            Delay_Count : constant := 300_000_000;
          begin
             loop
                for Delay_Loop_Count in 1 .. Delay_Count loop CPU.NOP; end loop;
-               Console.Print (Character'('.'));
+               Console.Print ("hello, SweetAda", NL => True);
             end loop;
          end;
       end if;
