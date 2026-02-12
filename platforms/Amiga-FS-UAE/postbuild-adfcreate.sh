b5616f43d18c566c57fed798420a25a72bfdde20
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2026-02-12 20:46:07 +0100

    Amiga-FS-UAE/postbuild-adfcreate.sh: use build system variable for kernel.rom

--- platforms/Amiga-FS-UAE/postbuild-adfcreate.sh
+++ platforms/Amiga-FS-UAE/postbuild-adfcreate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-"${PYTHON}" adfcreate.py adfboot ${KERNEL_PARENT_PATH}/kernel.rom 0x80000 0x80400 boot.adf
-#"${TCLSH}" adfcreate.tcl adfboot ${KERNEL_PARENT_PATH}/kernel.rom 0x80000 0x80400 boot.adf
+"${PYTHON}" adfcreate.py adfboot ${KERNEL_PARENT_PATH}/${KERNEL_ROMFILE} 0x80000 0x80400 boot.adf
+#"${TCLSH}" adfcreate.tcl adfboot ${KERNEL_PARENT_PATH}/${KERNEL_ROMFILE} 0x80000 0x80400 boot.adf
 
 exit $?
 
