a2590aac1a2c62df8880dba4fa86f676e85d4a6e
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2026-03-08 14:28:40 +0100

    M5235BCC/*: - do not use hardwired kernel point - protect paths with double-quotes - update copyright notice - cosmetics

--- platforms/M5235BCC/platform-dBUG/postbuild.sh
+++ platforms/M5235BCC/platform-dBUG/postbuild.sh
@@ -2,12 +2,12 @@
 
 TFTP_DIRECTORY=/home/tftp
 
-${OBJCOPY}                           \
-  --strip-all                        \
-  -j .vectors -j .text -j .data      \
-  -O elf32-m68k                      \
-  ${SWEETADA_PATH}/${KERNEL_OUTFILE} \
-  ${TFTP_DIRECTORY}/kernel.o
+${OBJCOPY}                             \
+  --strip-all                          \
+  -j .vectors -j .text -j .data        \
+  -O elf32-m68k                        \
+  "${SWEETADA_PATH}"/${KERNEL_OUTFILE} \
+  "${TFTP_DIRECTORY}"/kernel.o
 
 exit $?
 
