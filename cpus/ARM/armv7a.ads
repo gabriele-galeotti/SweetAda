eda1101cedb6719dfcc7c6f4d569b855a4436654
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2026-03-16 23:16:07 +0100

    cpus/ARM/armv7a.ads: remove unused with

--- cpus/ARM/armv7a.ads
+++ cpus/ARM/armv7a.ads
@@ -15,11 +15,6 @@
 -- Please consult the LICENSE.txt file located in the top-level directory.                                           --
 -----------------------------------------------------------------------------------------------------------------------
 
-with System;
-with System.Storage_Elements;
-with Interfaces;
-with Bits;
-
 package ARMv7A
    with Preelaborate => True
    is
