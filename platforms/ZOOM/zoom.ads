02783ec579941f837f6e33b32d36d19495052409
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2026-04-27 22:41:19 +0200

    ZOOM/zoom.ads: remove preelaboration

--- platforms/ZOOM/zoom.ads
+++ platforms/ZOOM/zoom.ads
@@ -19,7 +19,7 @@ with System;
 with Interfaces;
 
 package ZOOM
-   with Preelaborate => True
+--   with Preelaborate => True
    is
 
    --========================================================================--
