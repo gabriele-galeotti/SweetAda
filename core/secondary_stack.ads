eee1fcdca6dc299b450feef95d235988c05d818f
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2026-05-01 11:07:51 +0200

    core/secondary_stack.ad[b|s]: - remove preelaboration - apply No_Elaboration_Code

--- core/secondary_stack.ads
+++ core/secondary_stack.ads
@@ -15,6 +15,8 @@
 -- Please consult the LICENSE.txt file located in the top-level directory.                                           --
 -----------------------------------------------------------------------------------------------------------------------
 
+pragma Restrictions (No_Elaboration_Code);
+
 package Secondary_Stack
    with SPARK_Mode => On
    is
