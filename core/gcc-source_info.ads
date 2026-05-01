c4dd2799fee17694a02f1490d9d809ccff6eb414
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2026-05-01 11:08:25 +0200

    core/gcc-source_info.ads: - remove preelaboration - apply No_Elaboration_Code

--- core/gcc-source_info.ads
+++ core/gcc-source_info.ads
@@ -15,9 +15,10 @@
 -- Please consult the LICENSE.txt file located in the top-level directory.                                           --
 -----------------------------------------------------------------------------------------------------------------------
 
+pragma Restrictions (No_Elaboration_Code);
+
 package GCC.Source_Info
-   with Preelaborate => True,
-        SPARK_Mode   => On
+   with SPARK_Mode => On
    is
 
    ----------------------------------------------------------------------------
