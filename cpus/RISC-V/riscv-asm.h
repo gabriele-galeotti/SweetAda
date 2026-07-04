381d3e0d75954885f74db01c19a412889ee9a7c2
Gabriele Galeotti <gabriele.galeotti@sweetada.org>
2026-07-04 23:29:12 +0200

    cpus/RISC-V/div.S|riscv-asm.h: update to GCC-15.3.0

--- cpus/RISC-V/riscv-asm.h
+++ cpus/RISC-V/riscv-asm.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2017-2024 Free Software Foundation, Inc.
+/* Copyright (C) 2017-2025 Free Software Foundation, Inc.
 
 This file is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the
@@ -20,17 +20,20 @@ see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
 <http://www.gnu.org/licenses/>.  */
 
 /* SweetAda version, reformatted and cleaned                    */
-/* __INF__ original: gcc-14.2.0/libgcc/config/riscv/riscv-asm.h */
+/* __INF__ original: gcc-15.3.0/libgcc/config/riscv/riscv-asm.h */
 
 #define FUNC_TYPE(X) .type   X,@function
 #define FUNC_SIZE(X) .size   X,.-X
 
 #define FUNC_BEGIN(X)           \
+	        .align  2;	\
                 .global X     ; \
                 FUNC_TYPE (X) ; \
-X:
+X:                              \
+	        LPAD
 
-#define FUNC_END(X) FUNC_SIZE(X)
+#define FUNC_END(X) \
+                FUNC_SIZE(X)
 
 #define FUNC_ALIAS(X,Y)     \
                 .global X ; \
@@ -39,7 +42,70 @@ X:
 #define CONCAT1(a, b)        CONCAT2(a, b)
 #define CONCAT2(a, b)        a ## b
 #define HIDDEN_JUMPTARGET(X) CONCAT1(__hidden_, X)
-#define HIDDEN_DEF(X)                                 \
-                FUNC_ALIAS(HIDDEN_JUMPTARGET(X), X) ; \
-                .hidden HIDDEN_JUMPTARGET(X)
+#define HIDDEN_DEF(X)        FUNC_ALIAS(HIDDEN_JUMPTARGET(X), X); \
+                             .hidden HIDDEN_JUMPTARGET(X)
 
+/* GNU_PROPERTY_RISCV64_* macros from elf.h for use in asm code.  */
+#define FEATURE_1_AND 0xc0000000
+#define FEATURE_1_FCFI 1
+#define FEATURE_1_BCFI 2
+
+/* Add a NT_GNU_PROPERTY_TYPE_0 note.  */
+#if __riscv_xlen == 32
+#  define GNU_PROPERTY(type, value)	\
+    .section .note.gnu.property, "a";	\
+    .p2align 2;				\
+    .word 4;				\
+    .word 12;				\
+    .word 5;				\
+    .asciz "GNU";			\
+    .word type;				\
+    .word 4;				\
+    .word value;			\
+    .text
+#else
+#  define GNU_PROPERTY(type, value)	\
+    .section .note.gnu.property, "a";	\
+    .p2align 3;				\
+    .word 4;				\
+    .word 16;				\
+    .word 5;				\
+    .asciz "GNU";			\
+    .word type;				\
+    .word 4;				\
+    .word value;			\
+    .word 0;				\
+    .text
+#endif
+
+/* Add GNU property note with the supported features to all asm code
+   where sysdep.h is included.  */
+#undef __VALUE_FOR_FEATURE_1_AND
+#if defined (__riscv_zicfilp) || defined (__riscv_zicfiss)
+#  if defined (__riscv_zicfilp)
+#    if defined (__riscv_zicfiss)
+#      define __VALUE_FOR_FEATURE_1_AND 0x3
+#    else
+#      define __VALUE_FOR_FEATURE_1_AND 0x1
+#    endif
+#  else
+#    if defined (__riscv_zicfiss)
+#      define __VALUE_FOR_FEATURE_1_AND 0x2
+#    else
+#      error "What?"
+#    endif
+#  endif
+#endif
+
+#if defined (__VALUE_FOR_FEATURE_1_AND)
+GNU_PROPERTY (FEATURE_1_AND, __VALUE_FOR_FEATURE_1_AND)
+#endif
+#undef __VALUE_FOR_FEATURE_1_AND
+
+#ifdef __riscv_zicfilp
+# define SET_LPAD   lui  t2,0
+# define LPAD       lpad 0
+#else
+# define SET_LPAD
+# define LPAD
+#endif
